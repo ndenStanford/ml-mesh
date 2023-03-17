80e917d74a80fc5c727cf4795c9db00a434b5c44
EC2 Default User <ec2-user@ip-172-31-232-222.ec2.internal>
2023-03-17 19:39:20 +0000

    fixed the core test suite

--- docker/neuron-compile/conftest.py
+++ docker/neuron-compile/conftest.py
@@ -1,13 +1,25 @@
 import pytest
 import torch
+from typing import List
 
 
 @pytest.fixture
-def image_tracing_input():
-    
+def image_tracing_input() -> torch.Tensor:
+
     return torch.rand([1, 3, 224, 224])
 
+
 @pytest.fixture
-def text_tracing_input():
-    
-    return "This is a sentence sentence to generate sample inputs for the neuron compilation step"
+def text_tracing_input() -> List[str]:
+
+    return [
+        "This is a sentence record to generate sample inputs for the neuron compilation step",
+        "This is the second record to generate tracing compilation inputs.",
+        "This is the third text record. It has more than one sentence.",
+    ]
+
+
+@pytest.fixture
+def max_length() -> int:
+
+    return 25
