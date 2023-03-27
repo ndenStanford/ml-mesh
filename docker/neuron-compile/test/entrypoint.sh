22c70a2084e1c72a0e98dce45986df62c4b1c11a
EC2 Default User <ec2-user@ip-172-31-232-26.ec2.internal>
2023-03-27 18:52:35 +0000

    updated neuron-inference with agreed on changes from PR#7. also had to re-remote the neuron-compile entrypoint file for some reason

--- docker/neuron-compile/test/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-pytest -m compilation;
-# pytest -m inference; # this can be enabled once we have inferentia runners on github actions and the docker make command supports specifying devices at runtime
---
+++ docker/neuron-inference/test/test_torch_neuron.py
@@ -0,0 +1,34 @@
+import os
+import pytest
+import torch
+import torch.neuron
+
+
+@pytest.mark.inference
+def test_neuron_inference_torch_function(test_output_dir, torch_function_input):
+
+    traced_foo = torch.jit.load(os.path.join(test_output_dir, "traced_foo.pt"))
+    traced_foo(*torch_function_input)
+
+
+@pytest.mark.inference
+def test_neuron_inference_torch_graph(test_output_dir, torch_graph_input):
+
+    neuron_forward = torch.jit.load(os.path.join(test_output_dir, "neuron_forward.pt"))
+    neuron_forward(torch_graph_input)
+
+    neuron_net = torch.jit.load(os.path.join(test_output_dir, "neuron_net.pt"))
+    neuron_net.forward(torch_graph_input)
+
+
+@pytest.mark.inference
+def test_neuron_inference_transformer_nlp_model(test_output_dir, torch_model_input):
+
+    neuron_model_scripted = torch.jit.load(
+        os.path.join(
+            test_output_dir,
+            "transformer_nlp_model_neuron.pt",
+        )
+    )
+
+    neuron_model_scripted(*torch_model_input)
