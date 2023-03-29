fe5ad635685dc647a2e24162cbe5fa82d56ebd35
Ubuntu <ubuntu@ip-172-31-16-34.ec2.internal>
2023-03-29 12:42:07 +0000

    routes test

--- projects/summarization/serve/tests/unit/routes_test.py
+++ projects/summarization/serve/tests/unit/routes_test.py
@@ -1,1 +1,11 @@
-"""Routes test."""
+"""Test routes."""
+
+# 3rd party libraries
+from fastapi import status
+
+
+def test_health_route(test_client):
+    """Test health endpoint."""
+    response = test_client.get("/health")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == "OK"
