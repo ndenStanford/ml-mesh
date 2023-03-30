fd5f8d77e7b6209416a7cce3d051ec96d0c80158
Ubuntu <ubuntu@ip-172-31-16-34.ec2.internal>
2023-03-30 12:48:13 +0000

    predict test

--- projects/summarization/serve/tests/unit/predict_test.py
+++ projects/summarization/serve/tests/unit/predict_test.py
@@ -1,1 +1,47 @@
 """Predict test."""
+# Third party libs
+from fastapi.testclient import TestClient
+
+
+# pass fastAPI app into client
+client = TestClient(app)
+
+# Test input text
+input = """Elon Musk was the second person ever to amass a personal fortune of more than $200 billion, breaching that threshold in January 2021, months after Jeff Bezos.
+The Tesla Inc. chief executive officer has now achieved a first of his own: becoming the only person in history to erase $200 billion from their net worth.
+Musk, 51, has seen his wealth plummet to $137 billion after Tesla shares tumbled in recent weeks, including an 11% drop on Tuesday, according to the Bloomberg Billionaires Index. His fortune peaked at $340 billion on Nov. 4, 2021, and he remained the world's richest person until he was overtaken this month by Bernard Arnault, the French tycoon behind luxury-goods powerhouse LVMH.
+The round-number milestone reflects just how high Musk soared during the run-up in asset prices during the easy-money pandemic era. Tesla exceeded a $1 trillion market capitalization for the first time in October 2021, joining the likes of ubiquitous technology companies Apple Inc., Microsoft Corp., Amazon.com Inc. and Google parent Alphabet Inc., even though its electric vehicles represented only a sliver of the overall auto market."""  # noqa: E501
+
+
+def test_non_empty_summarization():
+    response = client.post(
+        "/v1/summarization/gpt3/predict",
+        json={
+            "content": input,
+        },
+    )
+    assert len(response.json()["summary"]) != 0
+
+
+# Test number of tokens is at most 50
+def test_max_token_length_50():
+    response = client.post(
+        "/v1/summarization/gpt3/predict",
+        json={
+            "content": input,
+            "max_tokens": 50,
+        },
+    )
+    assert len(response.json()["summary"]) <= 50
+
+
+# test if davinci model used
+def test_max_token_length_davinci():
+    response = client.post(
+        "/v1/summarization/gpt3/predict",
+        json={
+            "content": input,
+            "model": "text-davinci-003",
+        },
+    )
+    assert response.json()["model"] == "text-davinci-003"
