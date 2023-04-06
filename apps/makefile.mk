0b1340db9cfc247cd6909ece83adffb0b17b6bc6
Rene Jean Corneille <rene-jean.corneille@onclusive.com>
2023-04-06 15:55:26 +0100

    update make targets

--- apps/makefile.mk
+++ apps/makefile.mk
@@ -2,37 +2,29 @@
 
 apps.build/%: apps.set ## Build app
 	@echo "::group::Build $(OWNER)/$(notdir apps/$@)-$(COMPONENT) (system architecture)"
-	docker-compose -f ./apps/$(notdir apps/$@)/docker-compose.dev.yaml build --no-cache
+	docker compose -f ./apps/$(notdir apps/$@)/docker-compose.$(ENVIRONMENT).yaml build $(COMPONENT) --no-cache
 	@echo "::endgroup::"
 
 apps.install/%:
 	poetry install --directory=apps/$(notdir $@)/$(COMPONENT)
 
 apps.deploy/%: ## Deploy project component docker image to ECR.
-	docker push  $(OWNER)/$(notdir $@)-$(COMPONENT):${IMAGE_TAG}
-
-apps.tag/%:
-	docker tag $(OWNER)/$(notdir $@)-$(COMPONENT):${IMAGE_TAG} $(OWNER)/$(notdir $@)-$(COMPONENT):latest
-
-apps.untag/%:
-	aws ecr batch-delete-image --repository-name $(notdir $@)-$(COMPONENT) --image-ids imageTag="latest"
+	docker compose -f ./apps/$(notdir apps/$@)/docker-compose.$(ENVIRONMENT).yaml push $(COMPONENT)
 
 apps.start/%: # Start development container of component
-	docker compose -f apps/$(notdir $@)/docker-compose.dev.yaml up
+	docker compose -f apps/$(notdir $@)/docker-compose.$(ENVIRONMENT).yaml up
 
 apps.stop/%: # Start development container of component
-	docker compose -f apps/$(notdir $@)/docker-compose.dev.yaml down
+	docker compose -f apps/$(notdir $@)/docker-compose.$(ENVIRONMENT).yaml down
 
 apps.test/%: apps.unit/% apps.integration/% ## Run all tests for project component
 	echo "Running all tests."
 
 apps.unit/%: apps.set ## Run unit tests for project component
-	docker compose -f apps/$(notdir $@)/docker-compose.dev.yaml up -d
-	docker compose -f apps/$(notdir $@)/docker-compose.dev.yaml exec prompt pytest tests/unit -ra -vvv --tb=long --capture=no
+	docker compose -f apps/$(notdir $@)/docker-compose.$(ENVIRONMENT).yaml run $(COMPONENT)-unit
 
 apps.integration/%: ## Run integration tests for project component
-	docker compose -f apps/$(notdir $@)/docker-compose.dev.yaml up -d
-	docker compose -f apps/$(notdir $@)/docker-compose.dev.yaml exec prompt pytest tests/unit -ra -vvv --tb=long --capture=no
+	docker compose -f apps/$(notdir $@)/docker-compose.$(ENVIRONMENT).yaml --profile integration up --exit-code-from $(COMPONENT)-integration
 
 apps.lock/%:
 	poetry lock --directory=apps/$(notdir $@)/$(COMPONENT)
@@ -40,3 +32,5 @@ apps.lock/%:
 apps.set:
 	export OWNER=$(OWNER)
 	export IMAGE_TAG=$(IMAGE_TAG)
+	export TARGET_BUILD_STAGE=$(TARGET_BUILD_STAGE)
+	export AWS_ACCOUNT_ID=$(AWS_ACCOUNT_ID)
