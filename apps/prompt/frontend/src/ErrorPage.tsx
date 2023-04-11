483a182dc14f2f4997946dc95d39e2c45fa7c1e6
Rene Jean Corneille <rene-jean.corneille@onclusive.com>
2023-04-11 12:14:54 +0100

    intermediary commit

---
+++ apps/prompt/frontend/src/ErrorPage.tsx
@@ -0,0 +1,10 @@
+import { useRouterError } from "react-router-dom";
+
+// export default function ErrorPage() {
+//     const error = useRouterError();
+//     console.error(error);
+
+//     return (
+
+//     )
+// }
