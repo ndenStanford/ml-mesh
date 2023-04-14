04cf0f7a41476528b913e5d7e95ab54d349d214f
Ubuntu <ubuntu@ip-172-31-232-49.ec2.internal>
2023-04-14 16:37:54 +0000

    pre-commit on neuron compile build test suite

--- docker/neuron-compile/tests/build/test_ml_compile_neuron.py
+++ docker/neuron-compile/tests/build/test_ml_compile_neuron.py
@@ -1,4 +1,3 @@
-
 # Standard Library
 import os
 
@@ -18,15 +17,23 @@ import pytest
 @pytest.mark.build
 @pytest.mark.compilation
 @pytest.mark.parametrize(
-    'huggingface_model_reference,huggingface_model_type,export_handle',
+    "huggingface_model_reference,huggingface_model_type,export_handle",
     [
-        ('prajjwal1/bert-tiny',AutoModelForSequenceClassification,'text-classification-model'),
-        ('prajjwal1/bert-tiny',AutoModel,'feature-extraction-model')
-    ]
+        (
+            "prajjwal1/bert-tiny",
+            AutoModelForSequenceClassification,
+            "text-classification-model",
+        ),
+        ("prajjwal1/bert-tiny", AutoModel, "feature-extraction-model"),
+    ],
 )
-def test_create_and_save_neuron_compiled_model(huggingface_model_reference, huggingface_model_type, test_output_dir, export_handle):
+def test_create_and_save_neuron_compiled_model(
+    huggingface_model_reference, huggingface_model_type, test_output_dir, export_handle
+):
 
-    huggingface_model = huggingface_model_type.from_pretrained(huggingface_model_reference)
+    huggingface_model = huggingface_model_type.from_pretrained(
+        huggingface_model_reference
+    )
     huggingface_model.eval()
 
     neuron_compiled_model = CompiledModel.from_model(
@@ -34,93 +41,85 @@ def test_create_and_save_neuron_compiled_model(huggingface_model_reference, hugg
         max_length=5,
         batch_size=2,
         neuron=True,
-        validate_compilation=False # this would require the ability to score a neuron model   
+        validate_compilation=False,  # this would require the ability to score a neuron model
     )
 
-    neuron_compiled_model.save_pretrained(
-        os.path.join(
-            test_output_dir,
-            export_handle
-        )
-    )
+    neuron_compiled_model.save_pretrained(os.path.join(test_output_dir, export_handle))
 
 
 @pytest.mark.inference
 @pytest.mark.parametrize(
-    'export_handle',
+    "export_handle",
     [
-        ('text-classification-model'), # task: text-classification
-        ('feature-extraction-model') # task: feature-extraction
-    ]
+        ("text-classification-model"),  # task: text-classification
+        ("feature-extraction-model"),  # task: feature-extraction
+    ],
 )
 def test_load_and_score_neuron_compiled_model(export_handle, test_output_dir):
 
     neuron_compiled_model = CompiledModel.from_pretrained(
-        os.path.join(
-            test_output_dir,
-            export_handle
-        )
+        os.path.join(test_output_dir, export_handle)
     )
 
     # demonstrate retrieving neuron model meta data
     # note that retrieving batch size is not required as dynamic batching was enabled
     # by default during compilation
-    model_batch_size = neuron_compiled_model.compilation_specs['tracing__batch_size']
-    model_max_length = neuron_compiled_model.compilation_specs['tracing__max_length']
-    
+    model_batch_size = neuron_compiled_model.compilation_specs["tracing__batch_size"]
+    model_max_length = neuron_compiled_model.compilation_specs["tracing__max_length"]
+
     # force the dynamic batching chunking by adding one row to the batch size
     model_inputs = {
-        'input_ids':torch.ones((model_batch_size+1,model_max_length), dtype=torch.long),
-        'attention_mask':torch.ones((model_batch_size+1,model_max_length), dtype=torch.long)
+        "input_ids": torch.ones(
+            (model_batch_size + 1, model_max_length), dtype=torch.long
+        ),
+        "attention_mask": torch.ones(
+            (model_batch_size + 1, model_max_length), dtype=torch.long
+        ),
     }
-    
+
     neuron_compiled_model(**model_inputs)
-    
-    
+
+
 @pytest.mark.build
 @pytest.mark.compilation
 @pytest.mark.parametrize(
-    'huggingface_model_reference,huggingface_pipeline_task',
+    "huggingface_model_reference,huggingface_pipeline_task",
     [
-        ('prajjwal1/bert-tiny','text-classification'),
-        ('prajjwal1/bert-tiny','feature-extraction')
-    ]
+        ("prajjwal1/bert-tiny", "text-classification"),
+        ("prajjwal1/bert-tiny", "feature-extraction"),
+    ],
 )
-def test_create_and_save_neuron_compiled_pipeline(huggingface_model_reference, huggingface_pipeline_task, test_output_dir):
+def test_create_and_save_neuron_compiled_pipeline(
+    huggingface_model_reference, huggingface_pipeline_task, test_output_dir
+):
 
-    huggingface_pipeline = pipeline(task=huggingface_pipeline_task, model=huggingface_model_reference)
+    huggingface_pipeline = pipeline(
+        task=huggingface_pipeline_task, model=huggingface_model_reference
+    )
 
     neuron_compiled_pipeline = CompiledPipeline.from_pipeline(
         pipeline=huggingface_pipeline,
         max_length=5,
         batch_size=2,
         neuron=True,
-        validate_compilation=False        
+        validate_compilation=False,
     )
 
     neuron_compiled_pipeline.save_pretrained(
-        os.path.join(
-            test_output_dir,
-            huggingface_pipeline_task
-        )
+        os.path.join(test_output_dir, huggingface_pipeline_task)
     )
 
 
 @pytest.mark.inference
 @pytest.mark.parametrize(
-    'huggingface_pipeline_task',
-    [
-        ('text-classification'),
-        ('feature-extraction')
-    ]
+    "huggingface_pipeline_task", [("text-classification"), ("feature-extraction")]
 )
-def test_load_and_score_neuron_compiled_pipeline(huggingface_pipeline_task, test_output_dir, torch_model_text_input):
+def test_load_and_score_neuron_compiled_pipeline(
+    huggingface_pipeline_task, test_output_dir, torch_model_text_input
+):
 
     neuron_compiled_pipeline = CompiledPipeline.from_pretrained(
-        os.path.join(
-            test_output_dir,
-            huggingface_pipeline_task
-        )
+        os.path.join(test_output_dir, huggingface_pipeline_task)
     )
-    
+
     neuron_compiled_pipeline(torch_model_text_input)
