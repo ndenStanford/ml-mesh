a5d44d0be5cadd7ff229d0c36ccca16e10660bb2
sreza1 <shrezash98@gmail.com>
2023-04-17 12:44:49 +0000

    enum

---
+++ apps/prompt/backend/src/model/model_enum.py
@@ -0,0 +1,10 @@
+"""Model Enum"""
+
+# Standard Library
+from enum import Enum
+
+
+class ModelEnum(Enum):
+    GPT3_5 = "gpt-3.5-turbo"
+    DAVINCI = "text-davinci-003"
+    CURIE = "text-curie-001"
