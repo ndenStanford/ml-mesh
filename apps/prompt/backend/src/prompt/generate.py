997431d91d1e013d46d86aecc7961b01b0228ea3
sreza1 <shrezash98@gmail.com>
2023-04-17 12:44:49 +0000

    enum

--- apps/prompt/backend/src/prompt/generate.py
+++ apps/prompt/backend/src/prompt/generate.py
@@ -1,11 +1,10 @@
 """Text generation from prompt."""
 
-# TODO: [DS-426] Add model module to manage models and call parameters.
-
 # 3rd party libraries
 import openai
 
 # Source
+from src.model.model_enum import ModelEnum
 from src.settings import get_settings
 
 
@@ -16,11 +15,19 @@ def generate_text(prompt: str, model_name: str) -> str:
     """Sends request to generate text."""
 
     openai.api_key = settings.OPENAI_API_KEY
+    # Response based on what model we use
+    if (
+        model_name == ModelEnum.GPT3_5.value
+        or model_name == ModelEnum.DAVINCI.value  # noqa: W503
+        or model_name == ModelEnum.CURIE.value  # noqa: W503
+    ):
 
-    response = openai.ChatCompletion.create(
-        model=model_name,
-        messages=[{"role": "user", "content": prompt}],
-        max_tokens=settings.OPENAI_MAX_TOKENS,
-        temperature=settings.OPENAI_TEMPERATURE,
-    )
-    return response["choices"][0]["message"]["content"]
+        response = openai.ChatCompletion.create(
+            model=model_name,
+            messages=[{"role": "user", "content": prompt}],
+            max_tokens=settings.OPENAI_MAX_TOKENS,
+            temperature=settings.OPENAI_TEMPERATURE,
+        )
+        return response["choices"][0]["message"]["content"]
+    else:
+        return "Sorry, the backend for this model is in development"
