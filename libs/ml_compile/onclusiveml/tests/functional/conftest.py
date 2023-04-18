7731ced238991a17134fadcaaf94da26cc6769b3
Sebastian Scherer <scherersebastian@yahoo.de>
2023-04-18 15:14:51 +0100

    combined module level functional test conftests into one conftest as per internal code guidelines

--- libs/ml_compile/onclusiveml/tests/functional/conftest.py
+++ libs/ml_compile/onclusiveml/tests/functional/conftest.py
@@ -1,11 +1,29 @@
+# Standard Library
+from typing import List
+
 # ML libs
-from transformers import AutoTokenizer
+from transformers import AutoModel, AutoTokenizer
+from transformers.pipelines import pipeline
 
 # 3rd party libraries
 import pytest
 
 
 MODEL_MAX_LENGTH = 50
+
+
+@pytest.fixture
+def sample_inputs() -> List[str]:
+
+    return (
+        [
+            "This is an extremely bad and short sample input.",
+            "This is a pretty neutral sentence."
+            """This is another, much, much better sample input. It is amazing! This is to test how
+        the compiled model handles more than one tokenized sample at a time.""",
+        ]
+        * 2  # noqa: W503
+    )
 
 
 @pytest.fixture
@@ -20,6 +38,17 @@ def huggingface_model_max_length(huggingface_tokenizer):
     return huggingface_tokenizer.model_max_length
 
 
+@pytest.fixture
+def regression_test_atol():
+    return 2e-02
+
+
+@pytest.fixture
+def regression_test_rtol():
+    return 1e-02
+
+
+# ---------- compiled tokenizer
 @pytest.fixture
 def custom_tokenization_settings_1():
 
@@ -72,3 +101,19 @@ def all_delegated_method_references_with_sample_inputs():
         # ('convert_tokens_to_string',[0,1,2]),
         ("clean_up_tokenization", "some ,example text ."),
     )
+
+
+# ---------- compiled model
+@pytest.fixture
+def huggingface_model(huggingface_model_reference: str):
+
+    return AutoModel.from_pretrained(huggingface_model_reference)
+
+
+# -------------- compiled pipeline
+@pytest.fixture
+def huggingface_pipeline(
+    huggingface_pipeline_task: str, huggingface_model_reference: str
+):
+
+    return pipeline(task=huggingface_pipeline_task, model=huggingface_model_reference)
