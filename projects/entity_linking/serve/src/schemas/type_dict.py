dd387b62a0eeff1467d80b4697dddbc2b0947ffa
Nutch Den <nden@alumni.stanford.edu>
2023-04-20 11:00:10 +0700

    entity dict type

--- projects/entity_linking/serve/src/schemas/type_dict.py
+++ projects/entity_linking/serve/src/schemas/type_dict.py
@@ -1,11 +1,4 @@
 from typing_extensions import TypedDict
 
-EntityDictInput = TypedDict('EntityDictInput', {'entity_type': str, 'entity_text': str, 'score': str, 'sentence_index': 1})
- 
-
-entity_dict_input_test: EntityDictInput = {
-      "entity_type": "PER",
-      "entity_text": "Tim Cook",
-      "score": "0.99980134",
-      "sentence_index": 1
-    }
+EntityDictInput = TypedDict('EntityDictInput', {'entity_type': str, 'entity_text': str, 'score': str, 'sentence_index': int})
+EntityDictOutput = TypedDict('EntityDictInput', {'entity_type': str, 'entity_text': str, 'score': str, 'sentence_index': int, 'wiki_link': str})
