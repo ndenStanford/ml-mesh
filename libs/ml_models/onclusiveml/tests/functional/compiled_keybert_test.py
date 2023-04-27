38df53db9f212d047e935b6020a266f348f29b60
EC2 Default User <ec2-user@ip-172-31-232-26.ec2.internal>
2023-04-27 10:04:11 +0000

    cleaned up the benchmark notebooks to give more accurate picture of uncompiled vs compiled pipelines, and more importantly the uncompiled vs hybrid / compiled keyberts. updated functional tests which are now passing against pretty accurate latency improvement thresholds. the key to freeing up the neuron cores is to restart the notebook kernels - this was causing the testing issues earlier

--- libs/ml_models/onclusiveml/tests/functional/compiled_keybert_test.py
+++ libs/ml_models/onclusiveml/tests/functional/compiled_keybert_test.py
@@ -21,89 +21,99 @@ logger = get_default_logger(__name__, level=20, fmt=LogFormat.DETAILED.value)
 @pytest.mark.regression
 @pytest.mark.parametrize("document_index", [0, 1, 2])
 @pytest.mark.parametrize(
-    "compiled_word_pipeline, compiled_document_pipeline",
+    "document_pipeline, compiled_word_pipeline",
     [
         (
-            lazy_fixture("test_compiled_word_pipeline"),
-            lazy_fixture("test_compiled_document_pipeline"),
+            lazy_fixture("test_hf_pipeline"),
+            lazy_fixture("test_neuron_compiled_word_pipeline"),
         ),
         (
-            lazy_fixture("test_neuron_compiled_word_pipeline"),
+            lazy_fixture("test_compiled_document_pipeline"),
+            lazy_fixture("test_compiled_word_pipeline"),
+        ),
+        (
             lazy_fixture("test_neuron_compiled_document_pipeline"),
+            lazy_fixture("test_neuron_compiled_word_pipeline"),
         ),
     ],
 )
 def compiled_keybert_extract_keywords_regression_test(
+    document_pipeline,
     compiled_word_pipeline,
-    compiled_document_pipeline,
     test_hf_pipeline,
     test_document,
     document_index,
 ):
     # compiled keybert
     compiled_keybert = CompiledKeyBERT(
+        document_pipeline=document_pipeline,
         compiled_word_pipeline=compiled_word_pipeline,
-        compiled_document_pipeline=compiled_document_pipeline,
     )
 
     keywords_actual = compiled_keybert.extract_keywords(docs=test_document)
     keywords_actual_df = pd.DataFrame(keywords_actual, columns=["keyword", "score"])
-    keywords_actual_df["score"] = keywords_actual_df["score"].round(1)
+
     # generic keybert - ground truth behaviour
     keybert = KeyBERT(model=test_hf_pipeline)
 
     keywords_expected = keybert.extract_keywords(docs=test_document)
     keywords_expected_df = pd.DataFrame(keywords_expected, columns=["keyword", "score"])
-    keywords_expected_df["score"] = keywords_expected_df["score"].round(1)
+
     # assert keywords are identical and scores are within 0.01 absolute deviation
-    pd.testing.assert_frame_equal(keywords_actual_df, keywords_expected_df)
+    pd.testing.assert_frame_equal(keywords_actual_df, keywords_expected_df, atol=0.01)
 
 
 @pytest.mark.order(2)
 @pytest.mark.latency
 @pytest.mark.parametrize(
-    "compiled_word_pipeline, compiled_document_pipeline, n_runs, expected_speedup_factor",
+    "keybert_scenario, document_pipeline, compiled_word_pipeline, n_runs, expected_speedup_factor",
     [
         (
-            lazy_fixture("test_compiled_word_pipeline"),
-            lazy_fixture("test_compiled_document_pipeline"),
-            10,
-            1.03,
+            "hybrid",
+            lazy_fixture("test_hf_pipeline"),
+            lazy_fixture("test_neuron_compiled_word_pipeline"),
+            50,
+            1.9,
         ),
         (
-            lazy_fixture("test_neuron_compiled_word_pipeline"),
+            "full-compiled",
             lazy_fixture("test_neuron_compiled_document_pipeline"),
-            10,
-            1.9,
+            lazy_fixture("test_neuron_compiled_word_pipeline"),
+            50,
+            2.2,
         ),
     ],
 )
 def compiled_keybert_extract_keywords_latency_test(
+    keybert_scenario,
+    document_pipeline,
     compiled_word_pipeline,
-    compiled_document_pipeline,
     test_hf_pipeline,
     test_documents,
     n_runs,
     expected_speedup_factor,
 ):
 
+    # time compiled/hybrid keybert
     compiled_keybert = CompiledKeyBERT(
+        document_pipeline=document_pipeline,
         compiled_word_pipeline=compiled_word_pipeline,
-        compiled_document_pipeline=compiled_document_pipeline,
     )
 
     compiled_start = time.time()
     for i in range(n_runs):
         compiled_keybert.extract_keywords(test_documents)
     average_compiled_duration = (time.time() - compiled_start) / n_runs
-    logger.info(f"Average compiled inference duration: {average_compiled_duration}s")
+    logger.info(f"Average {keybert_scenario} compiled keybert inference duration: ")
+    logger.info(f"{average_compiled_duration}s")
 
+    # time generic keybert
     keybert = KeyBERT(model=test_hf_pipeline)
 
     start = time.time()
     for i in range(n_runs):
         keybert.extract_keywords(test_documents)
     average_duration = (time.time() - start) / n_runs
-    logger.info(f"Average inference duration: {average_duration}s")
+    logger.info(f"Average generic keybert inference duration: {average_duration}s")
 
-    # assert average_duration >= (expected_speedup_factor * average_compiled_duration)
+    assert average_duration >= (expected_speedup_factor * average_compiled_duration)
--- libs/ml_models/onclusiveml/tests/functional/conftest.py
+++ libs/ml_models/onclusiveml/tests/functional/conftest.py
@@ -35,7 +35,7 @@ def test_compiled_document_pipeline(test_hf_pipeline):
 
     return CompiledPipeline.from_pipeline(
         test_hf_pipeline,
-        max_length=512,  # test docs are 100 words, so should be <= 300 tokens
+        max_length=150,  # test docs are <=120 words
         batch_size=1,
         neuron=False,
         validate_compilation=False,
@@ -46,15 +46,10 @@ def test_compiled_document_pipeline(test_hf_pipeline):
 @pytest.fixture(scope="session")
 def test_neuron_compiled_word_pipeline(test_hf_pipeline):
 
-    # try:
-    #     neuron_compiled_word_pipeline = CompiledPipeline.from_pretrained(
-    #         "neuron_compiled_word_pipeline"
-    #     )
-    # except:
     neuron_compiled_word_pipeline = CompiledPipeline.from_pipeline(
         test_hf_pipeline,
-        max_length=25,
-        batch_size=3,
+        max_length=15,
+        batch_size=1,
         neuron=True,
         validate_compilation=False,
         tokenizer_settings={"add_special_tokens": True},
@@ -68,15 +63,10 @@ def test_neuron_compiled_word_pipeline(test_hf_pipeline):
 @pytest.fixture(scope="session")
 def test_neuron_compiled_document_pipeline(test_hf_pipeline):
 
-    # try:
-    #     neuron_compiled_document_pipeline = CompiledPipeline.from_pretrained(
-    #         "neuron_compiled_document_pipeline"
-    #     )
-    # except:
     neuron_compiled_document_pipeline = CompiledPipeline.from_pipeline(
         test_hf_pipeline,
-        max_length=512,
-        batch_size=3,
+        max_length=150,  # test docs are <=120 words
+        batch_size=1,
         neuron=True,
         validate_compilation=False,
         tokenizer_settings={"add_special_tokens": True},
@@ -93,24 +83,21 @@ def test_neuron_compiled_document_pipeline(test_hf_pipeline):
 def test_documents():
 
     return [
+        # ~ 117 tokens
         """Machine learning is a subfield of artificial intelligence that involves training
         algorithms to make predictions or decisions based on data. It is a powerful tool for
         solving complex problems and has numerous applications in fields such as finance,
         healthcare, and transportation. In machine learning, the algorithm learns patterns and
         relationships from the data, rather than being explicitly programmed. This allows it to
         adapt and improve over time as more data becomes available. Machine learning models can be
-        supervised, unsupervised, or semi-supervised, depending on the type of data and the problem
-        being solved. As more data is generated and computing power increases, the possibilities
-        for machine learning are only growing.""",
+        supervised, unsupervised, or semi-supervised.""",
+        # ~ 115 tokens
         """ML Ops, also known as Machine Learning Operations, is a practice that focuses on the
         management and deployment of machine learning models in production environments. It
         combines the principles of DevOps, data engineering, and machine learning to ensure that ML
         models are deployed efficiently and reliably. ML Ops involves automating the entire machine
         learning lifecycle, from data preparation and model training to deployment and monitoring.
-        This helps to improve the scalability and performance of ML models, reduce downtime, and
-        enable faster iteration and experimentation. As the demand for AI and ML continues to grow,
-        ML Ops is becoming an essential practice for organizations that want to achieve success
-        with their machine learning initiatives.""",
+        This helps to enable faster iteration and experimentation.""",
         # ~ 110 tokens
         """Flowers come in different shapes, sizes, and colors, and their beauty has captivated
         humans for centuries. Apart from their aesthetic appeal, flowers are also essential to the
@@ -122,34 +109,7 @@ def test_documents():
         religious ceremonies, weddings, and funerals. Different flowers have symbolic meanings,
         such as lilies representing purity and innocence. Flowers are also a significant part of
         home decor, adding charm and personality to homes and gardens. In conclusion, flowers have
-        multiple dimensions of significance."""
-        # ~ 500 tokens
-        """Flowers are nature's way of displaying beauty and elegance. They come in various colors,
-        shapes, and sizes, and their attractiveness has fascinated humans for centuries. However,
-        flowers have more to offer than their aesthetic value, and they play a crucial role in the
-        ecosystem. Flowers are the reproductive organs of plants, and they play a vital role in
-        pollination. They produce nectar and pollen that attract pollinators such as bees,
-        butterflies, and hummingbirds, who transfer pollen from the stamen to the stigma of the
-        flowers. This process helps in the fertilization of plants and is essential for the growth
-        of crops and other vegetation. Flowers also ensure the genetic diversity of plant species.
-        Apart from their ecological significance, flowers have numerous medicinal benefits. Many
-        flowers have therapeutic properties that have been used in traditional medicine for
-        centuries. For instance, chamomile is known for its calming effects and is used to treat
-        anxiety and insomnia. Lavender is also used to promote relaxation, relieve pain, and
-        improve sleep quality. Flowers like calendula have anti-inflammatory and antiseptic
-        properties and can be used to treat skin infections and wounds. Moreover, flowers have an
-        essential aesthetic value. They add color, charm, and personality to homes and gardens.
-        The beauty of flowers is mesmerizing and captivating, and it has a calming effect on the
-        mind and soul. Flowers arrangements, whether simple or elaborate, have a significant impact
-        on the overall aesthetic appeal of a space. In conclusion, flowers are not just pretty
-        things to look at. They have significant ecological, medicinal, cultural, and aesthetic
-        values. They play a vital role in the ecosystem, ensuring the growth of crops and
-        maintaining genetic diversity of plant species. Flowers also have therapeutic properties
-        and are used in traditional medicine worldwide. They are an essential part of cultural
-        traditions and are used to express emotions. The beauty and charm of flowers are
-        mesmerizing and captivating, and it adds personality and charm to homes and gardens.
-        Flowers are more than just plants; they are essential to human life and bring happiness and
-        joy to those who admire them..""",
+        multiple dimensions of significance.""",
     ]
 
 
