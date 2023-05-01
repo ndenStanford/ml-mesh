74da31e66feea61642a963e92425f1b29aa14d9e
Rene Jean Corneille <rene-jean.corneille@onclusive.com>
2023-05-01 11:51:45 +0100

    migrate targets to use docker-compose

--- projects/makefile.mk
+++ projects/makefile.mk
@@ -1,44 +1,36 @@
 ## PROJECTS TARGETS
 
-projects.build/%: ## Build project
-	@echo "::group::Build $(OWNER)/$(notdir projects/$@)-$(COMPONENT) (system architecture)"
-	docker build $(DOCKER_BUILD_ARGS) . \
-			-t $(OWNER)/$(notdir $@)-$(COMPONENT):${IMAGE_TAG} \
-			-f ./projects/$(notdir $@)/$(COMPONENT)/Dockerfile	\
-			--build-arg OWNER="$(OWNER)" \
-			--build-arg PROJECT=$(notdir $@) \
-			--build-arg COMPONENT=$(COMPONENT) \
-			--build-arg IMAGE_TAG="$(IMAGE_TAG)" \
-			--platform=$(PLATFORM) \
-			$(DOCKER_EXTRA_FLAGS) \
-			--rm --force-rm
-	@echo -n "built image size:"
-	@docker images $(OWNER)/$(notdir $@)-$(COMPONENT):latest --format "{{.Size}}"
+projects.build/%: projects.set ## Build app
+	@echo "::group::Build $(notdir projects/$@)-$(COMPONENT) (system architecture)"
+	docker compose -f ./projects/$(notdir projects/$@)/docker-compose.$(ENVIRONMENT).yaml build $(COMPONENT) --no-cache
 	@echo "::endgroup::"
 
 projects.install/%:
 	poetry install --directory=projects/$(notdir $@)/$(COMPONENT)
 
 projects.deploy/%: ## Deploy project component docker image to ECR.
-	docker push  $(OWNER)/$(notdir $@)-$(COMPONENT):${IMAGE_TAG}
-
-projects.tag/%:
-	docker tag $(OWNER)/$(notdir $@)-$(COMPONENT):${IMAGE_TAG} $(OWNER)/$(notdir $@)-$(COMPONENT):latest
-
-projects.untag/%:
-	aws ecr batch-delete-image --repository-name $(notdir $@)-$(COMPONENT) --image-ids imageTag="latest"
+	docker compose -f ./projects/$(notdir projects/$@)/docker-compose.$(ENVIRONMENT).yaml push $(COMPONENT)
 
 projects.start/%: # Start development container of component
-	cd projects/$(notdir $@)/$(COMPONENT) && $(DOCKER_COMPOSE_RUN_CMD) uvicorn src.app:app --host 0.0.0.0 --port $(PORT) --reload --log-level "debug"
+	docker compose -f projects/$(notdir $@)/docker-compose.$(ENVIRONMENT).yaml --profile $(COMPONENT) up
+
+projects.stop/%: # Start development container of component
+	docker compose -f projects/$(notdir $@)/docker-compose.$(ENVIRONMENT).yaml --profile $(COMPONENT) down
 
 projects.test/%: projects.unit/% projects.integration/% ## Run all tests for project component
 	echo "Running all tests."
 
-projects.unit/%: ## Run unit tests for project component
-	cd projects/$(notdir $@)/$(COMPONENT) && $(DOCKER_COMPOSE_RUN_CMD) pytest tests/unit -ra -vvv --tb=long --capture=no
+projects.unit/%: projects.set ## Run unit tests for project component
+	docker compose -f projects/$(notdir $@)/docker-compose.$(ENVIRONMENT).yaml run $(COMPONENT)-unit
 
 projects.integration/%: ## Run integration tests for project component
-	cd projects/$(notdir $@)/$(COMPONENT) && $(DOCKER_COMPOSE_RUN_CMD) pytest tests/integration -ra -vvv --tb=long --capture=no
+	docker compose -f projects/$(notdir $@)/docker-compose.$(ENVIRONMENT).yaml --profile integration up --exit-code-from $(COMPONENT)-integration
 
 projects.lock/%:
 	poetry lock --directory=projects/$(notdir $@)/$(COMPONENT)
+
+projects.set:
+	export OWNER=$(OWNER)
+	export IMAGE_TAG=$(IMAGE_TAG)
+	export TARGET_BUILD_STAGE=$(TARGET_BUILD_STAGE)
+	export AWS_ACCOUNT_ID=$(AWS_ACCOUNT_ID)
