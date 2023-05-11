9d4ba01e842fa23cd67eb0c2e04cea1ef6d0fc99
sreza1 <shrezash98@gmail.com>
2023-05-11 11:59:02 +0000

    initialise summarization prompts

--- apps/prompt/backend/src/_init.py
+++ apps/prompt/backend/src/_init.py
@@ -10,6 +10,7 @@ from onclusiveml.core.logging import get_default_logger
 from src.db import BaseTable
 from src.model.schemas import ModelSchema
 from src.model.tables import ModelTable
+from src.prompt.schemas import PromptTemplateSchema
 from src.prompt.tables import PromptTemplateTable
 from src.settings import get_settings
 
@@ -24,10 +25,11 @@ def init() -> None:
     """App initialization."""
     logger.info("Creating tables...")
     _create_tables([PromptTemplateTable, ModelTable])
-    fill_table()
+    fill_model_table()
+    fill_prompt_table()
 
 
-def fill_table() -> None:
+def fill_model_table() -> None:
     """Model filling."""
     logger.info("Adding models to model table...")
     list_of_models = settings.LIST_OF_MODELS
@@ -41,6 +43,22 @@ def fill_table() -> None:
             logger.debug("Adding model: {}".format(model[0]))
             model = ModelSchema(id=id, model_name=model[0], parameters=model[1])
             model.save()
+
+
+def fill_prompt_table() -> None:
+    """Prompts filling."""
+    logger.info("Adding prompts to prompts table...")
+    list_of_prompts = settings.LIST_OF_PROMPTS
+    # Saving predifined prompts into database
+    # If prompt id doesn't exist, then add model to table
+    for id, prompt in list_of_prompts.items():
+        try:
+            _ = PromptTemplateSchema.get(id)
+        except Exception as e:
+            logger.debug(e)
+            logger.debug("Adding prompt: {}".format(prompt[0]))
+            prompt = PromptTemplateSchema(id=id, template=prompt[0], alias=prompt[1])
+            prompt.save()
 
 
 def _create_tables(tables: List[BaseTable]) -> None:
