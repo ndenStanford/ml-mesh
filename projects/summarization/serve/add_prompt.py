f4c114c75505e1510e4fc410e30a148d01a27d4e
ndenStanford <nden@alumni.stanford.edu>
2023-05-12 19:15:02 +0000

    add prompt

---
+++ projects/summarization/serve/add_prompt.py
@@ -0,0 +1,24 @@
+import requests
+from article import ARTICLE
+
+#curl -X 'GET' 'http://0.0.0.0:4000/api/v1/prompts' -H 'accept: application/json' -H 'x-api-key: 1234'
+
+url = 'http://0.0.0.0:4000'
+headers = {'x-api-key': "1234"}
+
+q = requests.get("{}/api/v1/prompts".format(url), headers = headers)
+
+english_summarization_template = "{content} Summarize the article in 20 words"
+english_summarization_alias = "english_summarization_prompt"
+
+#q = requests.post(f"{url}/api/v1/prompts?template={english_summarization_template}&alias={english_summarization_alias}", headers = headers)
+#q
+
+#q = requests.post("{}/api/v1/prompts".format(url), json = {"template": english_summarization_template, "alias": "english_summarization_prompt"}, headers = headers)
+q = requests.get("{}/api/v1/prompts".format(url), headers = headers)
+q
+
+prompt_id = "c454b675-91fd-4499-9c37-ff0a5487bf02"
+values = {"content": ARTICLE}
+q = requests.post(f"{url}/api/v1/prompts/{prompt_id}/generate", headers = headers, json=values)
+q
---
+++ projects/summarization/serve/article.py
@@ -0,0 +1,38 @@
+ARTICLE = """Liverpool legend Jamie Carragher has backed Chelsea’s deadline-day move for ‘brilliant player’ Pierre-Emerick Aubameyang.
+Blues owner Todd Boehly and Stamford Bridge boss Thomas Tuchel secured a late deal to sign the former Arsenal captain from Barcelona.
+While Aubameyang only spent six months at Barca – scoring 13 goals in 24 appearances – he was one of the best players in the Premier League during his Arsenal career.
+The Gabon frontman scored 92 goals in 163 games for the Gunners and helped the club win the FA Cup before he was stripped of the captaincy and exiled by Mikel Arteta.
+Aubameyang has said he has ‘unfinished business’ in the Premier League and Carragher believes he will turn out to be a shrewd signing for Chelsea.
+The Blues have endured an underwhelming start to the new season and were desperate to sign a new striker after letting club-record signing Romelu Lukaku return to Italy.
+‘He’s a quality player, there is no doubt about that,’ Carragher said of Aubameyang on Sky Sports. ‘I think it’s a good signing.
+‘People may question Arsenal’s role in this and the fact they let him go for free, and now Chelsea are paying money for him 12 months later.
+‘I think it’s a good signing for Thomas Tuchel and Chelsea, and also the right decision from Mikel Arteta – look at where Arsenal are right now.
+‘We know Aubameyang brings goals and quality but at times there are maybe situations and misdemeanours off the pitch or within the dressing room and Arteta couldn’t stand for that.
+‘But Tuchel knows him well from their time at Borussia Dortmund and, as a neutral, I’m delighted he’s back in the Premier League because he’s a brilliant player.’
+Chelsea’s £10m deal for Aubameyang capped an extraordinary summer of spending at Stamford Bridge, with new owner Boehly spending more than £250m in his first transfer window in charge.
+Aubameyang joins the likes of Wesley Fofana, Raheem Sterling and Kalidou Koulibaly to join the Blues this summer.
+Asked whether Tuchel faces greater pressure following Chelsea’s record-breaking spending spree, Carragher added: ‘Yes, that is the name of the game for any top club, and that has always been the situation at Chelsea.
+‘It will be interesting to see if the change in ownership changes how ruthless the club was with managers under Roman Abramovich.
+‘But, for me, Thomas Tuchel is one of the best managers in Europe, he’s a Champions League winner, and right now in the Premier League it’s been tough competing with Manchester City and Liverpool.
+‘Chelsea did need a rejig at centre-back given the players they lost, so you can understand why they have had to go into the market. But there is no doubt they have paid big money for some of the players.’
+After five games of the 2022-23 campaign, Chelsea are only 10th in the Premier League. Tuchel’s side face London rivals West Ham tomorrow.
+"""
+
+ARTICLE_FRENCH = """La légende de Liverpool, Jamie Carragher, a soutenu la décision de Chelsea pour le "brillant joueur" Pierre-Emerick Aubameyang.
+Le propriétaire des Blues Todd Boehly et le patron de Stamford Bridge Thomas Tuchel ont conclu un accord tardif pour signer l'ancien capitaine d'Arsenal de Barcelone.
+Alors qu'Aubameyang n'a passé que six mois au Barca - marquant 13 buts en 24 apparitions - il a été l'un des meilleurs joueurs de la Premier League au cours de sa carrière à Arsenal.
+Le leader gabonais a marqué 92 buts en 163 matchs pour les Gunners et a aidé le club à remporter la FA Cup avant d'être démis de ses fonctions de capitaine et exilé par Mikel Arteta.
+Aubameyang a déclaré qu'il avait des "affaires inachevées" en Premier League et Carragher pense qu'il se révélera être une signature astucieuse pour Chelsea.
+Les Bleus ont connu un début de saison décevant et cherchaient désespérément à signer un nouvel attaquant après avoir laissé Romelu Lukaku, le record du club, revenir en Italie.
+"C'est un joueur de qualité, cela ne fait aucun doute", a déclaré Carragher à propos d'Aubameyang sur Sky Sports. «Je pense que c'est une bonne signature.
+«Les gens peuvent remettre en question le rôle d'Arsenal dans cela et le fait qu'ils l'ont laissé partir gratuitement, et maintenant Chelsea paie de l'argent pour lui 12 mois plus tard.
+«Je pense que c'est une bonne signature pour Thomas Tuchel et Chelsea, et aussi la bonne décision de Mikel Arteta – regardez où en est Arsenal en ce moment.
+Nous savons qu'Aubameyang apporte des buts et de la qualité, mais parfois il y a peut-être des situations et des délits en dehors du terrain ou dans le vestiaire et Arteta ne pouvait pas supporter cela.
+"Mais Tuchel le connaît bien depuis son passage au Borussia Dortmund et, en tant que neutre, je suis ravi qu'il soit de retour en Premier League car c'est un joueur brillant."
+L'accord de 10 millions de livres sterling de Chelsea pour Aubameyang a couronné un été extraordinaire de dépenses à Stamford Bridge, le nouveau propriétaire Boehly dépensant plus de 250 millions de livres sterling lors de sa première fenêtre de transfert en charge.
+Aubameyang rejoint Wesley Fofana, Raheem Sterling et Kalidou Koulibaly pour rejoindre les Blues cet été.
+Lorsqu'on lui a demandé si Tuchel faisait face à une plus grande pression après la frénésie de dépenses record de Chelsea, Carragher a ajouté: "Oui, c'est le nom du jeu pour n'importe quel club de haut niveau, et cela a toujours été la situation à Chelsea.
+« Il sera intéressant de voir si le changement de propriétaire change à quel point le club était impitoyable avec les managers sous Roman Abramovich.
+"Mais, pour moi, Thomas Tuchel est l'un des meilleurs managers d'Europe, il est vainqueur de la Ligue des champions, et en ce moment en Premier League, il a été difficile de rivaliser avec Manchester City et Liverpool.
+«Chelsea avait besoin d'un rejig à l'arrière central étant donné les joueurs qu'ils ont perdus, vous pouvez donc comprendre pourquoi ils ont dû entrer sur le marché. Mais il ne fait aucun doute qu'ils ont payé beaucoup d'argent pour certains des joueurs.
+Après cinq matchs de la campagne 2022-23, Chelsea n'est que 10e de Premier League. L'équipe de Tuchel affrontera demain son rival londonien West Ham."""
