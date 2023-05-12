b6fc03165fba3c2c6cbc5071fba6df5ba7026ed1
sreza1 <shrezash98@gmail.com>
2023-05-12 16:33:26 +0000

    don't add repeated rows in database - search by id doesnt work because id is always random

--- apps/prompt/backend/src/_init.py
+++ apps/prompt/backend/src/_init.py
@@ -30,35 +30,45 @@ def init() -> None:
 
 
 def fill_model_table() -> None:
+
     """Model filling."""
     logger.info("Adding models to model table...")
-    list_of_models = settings.LIST_OF_MODELS
+    list_of_models = []
+    models = ModelSchema.get()
+
+    # make list of predefined model names from database
+    for x in models:
+        list_of_models.append(x.model_name)
+
     # Saving predifined models into database
     # If model id doesn't exist, then add model to table
-    for id, model in list_of_models.items():
-        try:
-            _ = ModelSchema.get(id)
-        except Exception as e:
-            logger.debug(e)
-            logger.debug("Adding model: {}".format(model[0]))
-            model = ModelSchema(id=id, model_name=model[0], parameters=model[1])
-            model.save()
+    for _, model in settings.LIST_OF_MODELS.items():
+        if model[0] in list_of_models:
+            continue
+        logger.debug("Adding model: {}".format(model[0]))
+        model = ModelSchema(model_name=model[0], parameters=model[1])
+        model.save()
 
 
 def fill_prompt_table() -> None:
     """Prompts filling."""
     logger.info("Adding prompts to prompts table...")
     list_of_prompts = settings.LIST_OF_PROMPTS
+    prompts = PromptTemplateSchema.get()
+    list_of_aliases = []
+
+    # make list of predefined aliases from database
+    for x in prompts:
+        list_of_aliases.append(x.alias)
+
     # Saving predifined prompts into database
-    # If prompt id doesn't exist, then add model to table
-    for id, prompt in list_of_prompts.items():
-        try:
-            _ = PromptTemplateSchema.get(id)
-        except Exception as e:
-            logger.debug(e)
-            logger.debug("Adding prompt: {}".format(prompt[0]))
-            prompt = PromptTemplateSchema(id=id, template=prompt[0], alias=prompt[1])
-            prompt.save()
+    # If prompt doesn't exist, then add prompt to table
+    for _, prompt in list_of_prompts.items():
+        if prompt[1] in list_of_aliases:
+            continue
+        logger.debug("Adding prompt: {}".format(prompt[0]))
+        prompt = PromptTemplateSchema(template=prompt[0], alias=prompt[1])
+        prompt.save()
 
 
 def _create_tables(tables: List[BaseTable]) -> None:
