ce90d0bf3be459f9067c74d807df70a926cdacaf
Rene Jean Corneille <rene-jean.corneille@onclusive.com>
2023-05-19 15:14:43 +0100

    add method decorator

---
+++ libs/core/onclusiveml/core/decorator.py
@@ -0,0 +1,23 @@
+"""Decorator."""
+
+# Standard Library
+import functools
+from typing import Any, Callable
+
+
+def decorator(caller: Callable) -> Callable:
+    """Turns caller into a decorator.
+    Unlike decorator module, function signature is not preserved.
+
+    Args:
+        caller: Callable
+    """
+
+    def decor(f: Callable) -> Callable:
+        @functools.wraps(f)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            return caller(f, *args, **kwargs)
+
+        return wrapper
+
+    return decor
