172a018d3b207b966913aec874eb6ac9fd7f79e9
Rene-Jean Corneille <rene-jean.corneille@onclusive.com>
2023-05-22 08:05:43 +0000

    add base exception

---
+++ apps/prompt/backend/src/exceptions.py
@@ -0,0 +1,8 @@
+"""Base Exception."""
+
+# Internal libraries
+from onclusiveml.core.base.exception import OnclusiveException
+
+
+class BasePromptException(OnclusiveException):
+    """Base exception."""
