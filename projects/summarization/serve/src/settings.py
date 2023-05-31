b7105779db130aeea2d5e219152280030e5cc61a
Nutch Den <nden@alumni.stanford.edu>
2023-05-31 09:55:30 +0700

    update alias

--- projects/summarization/serve/src/settings.py
+++ projects/summarization/serve/src/settings.py
@@ -42,36 +42,36 @@ class Settings(BaseSettings):
 
     PROMPT_DICT = {
         "en": {
-            "alias": "english-summarization-prompt",
+            "alias": "ml-summarization-english",
             "template": "Give an abstractive summary while retaining important \
                         quotes of speech in less than {desired_length} words: \n {content} \n",
         },
         "fr": {
-            "alias": "french-summarization-prompt",
+            "alias": "ml-summarization-french",
             "template": "Donner un résumé abstrait tout en gardant les importantes \
                         citations du discours en moins de {desired_length} mots: \n {content} \n",
         },
         "de": {
-            "alias": "german-summarization-prompt",
+            "alias": "ml-summarization-german",
             "template": "Geben Sie eine abstrakte Zusammenfassung mit weniger als {desired_length} Wörtern und behalten \
                         Sie dabei wichtige Zitate: \n {content} \n",
         },
         "zh": {
-            "alias": "chinese-summarization-prompt",
+            "alias": "ml-summarization-chinese",
             "template": "给出一个抽象的总结，同时保留重要的\
                         少于 {desired_length} 个单词的演讲引述：\n {content} \n",
         },
         "ko": {
-            "alias": "korean-summarization-prompt",
+            "alias": "ml-summarization-korean",
             "template": "다음 내용에 대해 중요한 인용구를 유지하면서 {desired_length} 단어 미만으로 요약하세요: \n {content} \n",
         },
         "fr": {
-            "alias": "french-summarization-prompt",
+            "alias": "ml-summarization-korean",
             "template": "Donner un résumé abstrait tout en gardant l'importance \
                         citations de discours en moins de {desired_length} mots: \n {content} \n",
         },
         "fr": {
-            "alias": "french-summarization-prompt",
+            "alias": "ml-summarization-korean",
             "template": "Donner un résumé abstrait tout en gardant l'importance \
                         citations de discours en moins de {desired_length} mots: \n {content} \n",
         },
