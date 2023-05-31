c313e10dd05197862f21a4ba721fc0b56ca88e29
Nutch Den <nden@alumni.stanford.edu>
2023-05-31 10:10:15 +0700

    secondary prompt added

--- projects/summarization/serve/src/settings.py
+++ projects/summarization/serve/src/settings.py
@@ -56,25 +56,41 @@ class Settings(BaseSettings):
             "template": "Geben Sie eine abstrakte Zusammenfassung mit weniger als {desired_length} Wörtern und behalten \
                         Sie dabei wichtige Zitate: \n {content} \n",
         },
+        "it": {
+            "alias": "ml-summarization-italian",
+            "template": "Fornisci un riassunto astratto pur mantenendo importanti \
+                        virgolette del discorso in meno di {lunghezza_desiderata} parole: \n {contenuto} \n",
+        },
+        "es": {
+            "alias": "ml-summarization-spanish",
+            "template": "Proporcione un resumen abstracto manteniendo importantes \
+                        citas de discurso en menos de {desired_length} palabras: \n {content} \n",
+        },
+        "ca": {
+            "alias": "ml-summarization-catalan",
+            "template": "Feu un resum abstractiu tot conservant la importància \
+                        cites del discurs en menys de {desired_length} paraules: \n {contingut} \n",
+        },
+        "pt": {
+            "alias": "ml-summarization-portuguese",
+            "template": "Forneça um resumo abstrato, mantendo importantes \
+                        citações do discurso em menos de {desired_length} palavras: \n {conteúdo} \n",
+        },
         "zh": {
             "alias": "ml-summarization-chinese",
             "template": "给出一个抽象的总结，同时保留重要的\
                         少于 {desired_length} 个单词的演讲引述：\n {content} \n",
         },
+        "ja": {
+            "alias": "ml-summarization-japanese",
+            "template": "重要な部分を保持しながら、抽象的な要約を提供します \
+                        {desired_length} 単語未満のスピーチの引用: \n {content} \n",
+        },
         "ko": {
             "alias": "ml-summarization-korean",
             "template": "다음 내용에 대해 중요한 인용구를 유지하면서 {desired_length} 단어 미만으로 요약하세요: \n {content} \n",
         },
-        "fr": {
-            "alias": "ml-summarization-korean",
-            "template": "Donner un résumé abstrait tout en gardant l'importance \
-                        citations de discours en moins de {desired_length} mots: \n {content} \n",
-        },
-        "fr": {
-            "alias": "ml-summarization-korean",
-            "template": "Donner un résumé abstrait tout en gardant l'importance \
-                        citations de discours en moins de {desired_length} mots: \n {content} \n",
-        },
+        
     }
 
 
