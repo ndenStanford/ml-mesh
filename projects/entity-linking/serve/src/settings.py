3f4b21a6fa6e53e5f701013c9dae629ae00bc24e
ndenStanford <nden@alumni.stanford.edu>
2023-05-31 12:31:28 +0700

    endpoint update

--- projects/entity-linking/serve/src/settings.py
+++ projects/entity-linking/serve/src/settings.py
@@ -36,7 +36,7 @@ class Settings(BaseSettings):
     # entity fishing endpoint
     # our endpoint does not have load balancer for now
     ENTITY_FISHING_ENDPOINT: str = (
-        "https://cloud.science-miner.com/nerd/service/disambiguate"
+        "https://internal.api.ml.stage.onclusive.com/service/disambiguate"
     )
 
     # entity recognition endpoint
@@ -44,5 +44,7 @@ class Settings(BaseSettings):
         "https://eks-data-prod.onclusive.com/predictions/ner_neuron_v2"
     )
 
+    API_KEY_NAME: str = "x-api-key"
+    API_KEY: str
 
 settings = Settings()
