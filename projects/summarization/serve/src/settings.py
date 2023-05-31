8876f999487815be53e4a02ae8e7b72f21a50145
denStanford <nden@alumni.stanford.edu>
2023-05-31 01:01:22 +0000

    prompt added

--- projects/summarization/serve/src/settings.py
+++ projects/summarization/serve/src/settings.py
@@ -48,6 +48,30 @@ class Settings(BaseSettings):
         },
         "fr": {
             "alias": "french-summarization-prompt",
+            "template": "Donner un résumé abstrait tout en gardant les importantes \
+                        citations du discours en moins de {desired_length} mots: \n {content} \n",
+        },
+        "de": {
+            "alias": "german-summarization-prompt",
+            "template": "Geben Sie eine abstrakte Zusammenfassung mit weniger als {desired_length} Wörtern und behalten \
+                        Sie dabei wichtige Zitate: \n {content} \n",
+        },
+        "zh": {
+            "alias": "chinese-summarization-prompt",
+            "template": "给出一个抽象的总结，同时保留重要的\
+                        少于 {desired_length} 个单词的演讲引述：\n {content} \n",
+        },
+        "ko": {
+            "alias": "korean-summarization-prompt",
+            "template": "다음 내용에 대해 중요한 인용구를 유지하면서 {desired_length} 단어 미만으로 요약하세요: \n {content} \n",
+        },
+        "fr": {
+            "alias": "french-summarization-prompt",
+            "template": "Donner un résumé abstrait tout en gardant l'importance \
+                        citations de discours en moins de {desired_length} mots: \n {content} \n",
+        },
+        "fr": {
+            "alias": "french-summarization-prompt",
             "template": "Donner un résumé abstrait tout en gardant l'importance \
                         citations de discours en moins de {desired_length} mots: \n {content} \n",
         },
