7ad27d10fe6dfde912ed1b343c51ed072a5fb45d
Sebastian Scherer <scherersebastian@yahoo.de>
2023-06-05 12:54:15 +0000

    converted unit test depending on env vars into integration test

---
+++ libs/tracking/onclusiveml/tests/integration/tracked_model_utils/tracked_model_specs_test.py
@@ -0,0 +1,22 @@
+# Standard Library
+import os
+
+# Internal libraries
+from onclusiveml.tracking.tracked_model_utils import TrackedModelSpecs
+
+
+def test_from_env():
+    """This test relies on the following environment variables being set:
+    - NEPTUNE_PROJECT
+    - NEPTUNE_MODEL_ID
+    - NEPTUNE_API_TOKEN
+    """
+
+    tracked_model_specs = TrackedModelSpecs()
+
+    assert tracked_model_specs.project == os.environ["NEPTUNE_PROJECT"]
+    assert tracked_model_specs.model == os.environ["NEPTUNE_MODEL_ID"]
+    # api token needs to be accessed via the SecretString's  `_secret_value` field attribute
+    assert (
+        tracked_model_specs.api_token._secret_value == os.environ["NEPTUNE_API_TOKEN"]
+    )
