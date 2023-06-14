150f757e31e07853c92fad33aa7804937cec7612
sreza1 <shrezash98@gmail.com>
2023-06-14 10:56:18 +0000

    additional tests and documentation

--- libs/nlp/onclusiveml/nlp/sentence_tokenize/sentence_tokenize.py
+++ libs/nlp/onclusiveml/nlp/sentence_tokenize/sentence_tokenize.py
@@ -15,10 +15,22 @@ from onclusiveml.nlp.sentence_tokenize.consts import TOKENS
 
 
 class SentenceTokenize:
+    """Tokenizing sentences in a given text."""
 
     regex = re.compile(r"|".join(TOKENS))
 
     def tokenize(self, content: str, language: str = "english") -> Dict[str, List[Any]]:
+        """
+        Tokenizes the input content into sentences.
+        Uses both nltk sentence tokenize and regex using list of unique characters
+
+        Args:
+            content (str): Text to be tokenized into sentences
+            language (str, optional): Language of the text (default English)
+
+        Returns:
+            dict: Dictionary containing tokenized setences
+        """
 
         sentences_first = nltk.sent_tokenize(content, language)
         sentences = []
--- libs/nlp/onclusiveml/tests/unit/sentence_tokenize/sentence_tokenize_test.py
+++ libs/nlp/onclusiveml/tests/unit/sentence_tokenize/sentence_tokenize_test.py
@@ -4,7 +4,7 @@
 from onclusiveml.nlp import SentenceTokenize
 
 
-def sentence_tokenize_test():
+def tokenize_test():
     text = """Elon Musk was the second person ever to amass a personal fortune of more than $200 billion, breaching that threshold in January 2021, months after Jeff Bezos.
     The Tesla Inc. chief executive officer has now achieved a first of his own: becoming the only person in history to erase $200 billion from their net worth."""  # noqa: E501
     tokenizer = SentenceTokenize()
@@ -13,3 +13,24 @@ def sentence_tokenize_test():
         "Elon Musk was the second person ever to amass a personal fortune of more than $200 billion, breaching that threshold in January 2021, months after Jeff Bezos.",  # noqa: E501
         "The Tesla Inc. chief executive officer has now achieved a first of his own: becoming the only person in history to erase $200 billion from their net worth.",  # noqa: E501
     ]
+
+
+def tokenize_fr_test():
+    text = """Elon Reeve Musk naît le 28 juin 1971 à Pretoria, en Afrique du Sud.
+    Il est le fils d'Errol Musk, riche ingénieur et promoteur immobilier sud-africain aux origines afrikaner et anglo-sud-africaine, ayant eu des parts d’une mine d'émeraudes en Zambie, et de Maye Haldeman, une nutritionniste et mannequin canadienne.
+    Après le divorce de ses parents en 1979, il continue de vivre avec son père. À l'âge de 12 ans, il vend son premier programme de jeu vidéo pour l'équivalent de 500 dollars"""  # noqa: E501
+    tokenizer = SentenceTokenize()
+    res = tokenizer.tokenize(content=text, language="french")
+    assert res["sentences"] == [
+        "Elon Reeve Musk naît le 28 juin 1971 à Pretoria, en Afrique du Sud.",
+        "Il est le fils d'Errol Musk, riche ingénieur et promoteur immobilier sud-africain aux origines afrikaner et anglo-sud-africaine, ayant eu des parts d’une mine d'émeraudes en Zambie, et de Maye Haldeman, une nutritionniste et mannequin canadienne.",  # noqa: E501
+        "Après le divorce de ses parents en 1979, il continue de vivre avec son père.",
+        "À l'âge de 12 ans, il vend son premier programme de jeu vidéo pour l'équivalent de 500 dollars",  # noqa: E501
+    ]
+
+
+def tokenize_unique_chars_test():
+    text = """This is sentence one！ This is sentence two."""
+    tokenizer = SentenceTokenize()
+    res = tokenizer.tokenize(content=text)
+    assert res["sentences"] == ["This is sentence one", " This is sentence two."]
