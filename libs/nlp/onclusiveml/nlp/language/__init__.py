bedb9bc8d86fa53d2792acb64cd69ec43225a0f4
sreza1 <shrezash98@gmail.com>
2023-06-16 11:46:07 +0000

    remove comment

--- libs/nlp/onclusiveml/nlp/language/__init__.py
+++ libs/nlp/onclusiveml/nlp/language/__init__.py
@@ -3,7 +3,3 @@ from onclusiveml.nlp.language.lang_detect import (  # noqa: F401
     detect_language,
     filter_language,
 )
-
-# from onclusiveml.nlp.sentence_tokenize.consts import (  # noqa: F401
-#     SPECIAL_CHARACTERS,
-# )
