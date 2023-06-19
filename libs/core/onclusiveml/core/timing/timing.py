ab86b7434e79e719798f55b1d56e87bf9bdacf54
sreza1 <shrezash98@gmail.com>
2023-06-19 20:56:12 +0000

    docstring

--- libs/core/onclusiveml/core/timing/timing.py
+++ libs/core/onclusiveml/core/timing/timing.py
@@ -1,8 +1,5 @@
 """Timing decorator"""
 
-# Wrapper that checks time to run function, then log it
-# make so can deal with unkonwn amount of parameters
-
 # Standard Library
 import datetime
 from typing import Any, Callable
@@ -15,6 +12,19 @@ logger = get_default_logger(__name__)
 
 
 def timing_decorator(func: Callable) -> Callable:
+    """
+    A decorator function that measures execution time of a given function
+
+    args:
+        func (Callable): The function to be timed
+
+    Example usage:
+        @timing_decorator
+        def my_function():
+            ...
+
+        my_function() # execution time will be logged    
+    """
     def wrapper(*args: Any, **kwargs: Any) -> None:
         start_time = datetime.datetime.utcnow()
         result = func(*args, **kwargs)
