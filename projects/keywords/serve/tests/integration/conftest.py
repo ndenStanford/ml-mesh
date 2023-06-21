d6e3c58bf55648b893e56321aa595ea287c1dc9f
Sebastian Scherer <scherersebastian@yahoo.de>
2023-06-21 16:35:24 +0000

    added served model predict with regression validation to integration test

--- projects/keywords/serve/tests/integration/conftest.py
+++ projects/keywords/serve/tests/integration/conftest.py
@@ -1,8 +1,47 @@
+# Standard Library
+import json
+
 # 3rd party libraries
 import pytest
+
+# Source
+from src.serving_params import ServedModelParams
 
 
 @pytest.fixture
 def test_model_name():
 
     return "test-model"
+
+
+@pytest.fixture
+def test_served_model_params(test_model_name):
+
+    return ServedModelParams(model_name=test_model_name)
+
+
+@pytest.fixture
+def test_inputs(test_served_model_params):
+
+    with open(test_served_model_params.inputs_test_file, "r") as json_file:
+        test_inputs = json.load(json_file)
+
+    return test_inputs
+
+
+@pytest.fixture
+def test_inference_params(test_served_model_params):
+
+    with open(test_served_model_params.inference_params_test_file, "r") as json_file:
+        test_inference_params = json.load(json_file)
+
+    return test_inference_params
+
+
+@pytest.fixture
+def test_predictions(test_served_model_params):
+
+    with open(test_served_model_params.predictions_test_file, "r") as json_file:
+        test_predictions = json.load(json_file)
+
+    return test_predictions
--- projects/keywords/serve/tests/integration/served_model/served_keywords_model_test.py
+++ projects/keywords/serve/tests/integration/served_model/served_keywords_model_test.py
@@ -1,21 +1,64 @@
+# 3rd party libraries
+import pytest
+
 # Source
 from src.served_model import ServedKeywordsModel
-from src.serving_params import ServedModelParams
+from src.server_models import (
+    PredictConfiguration,
+    PredictInputDocumentModel,
+    PredictionExtractedKeyword,
+    PredictionOutputDocument,
+    PredictRequestModel,
+    PredictResponseModel,
+)
 
 
-def test_served_keywords_model__init__(test_model_name):
+# def test_served_keywords_model__init__(test_served_model_params):
 
-    served_model_params = ServedModelParams(model_name=test_model_name)
-    ServedKeywordsModel(served_model_params=served_model_params)
+#     ServedKeywordsModel(served_model_params=test_served_model_params)
 
 
-def test_served_keywords_model_load(test_model_name):
+# def test_served_keywords_model_load(test_served_model_params):
 
-    served_model_params = ServedModelParams(model_name=test_model_name)
-    served_keywords_model = ServedKeywordsModel(served_model_params=served_model_params)
+#     served_keywords_model = ServedKeywordsModel(served_model_params=test_served_model_params)
 
-    assert not served_keywords_model.is_ready()
+#     assert not served_keywords_model.is_ready()
 
+#     served_keywords_model.load()
+
+#     assert served_keywords_model.is_ready()
+
+
+@pytest.mark.parametrize("test_record_index", [0, 1, 2])
+def test_served_keywprds_model_predict(
+    test_served_model_params,
+    test_inputs,
+    test_inference_params,
+    test_predictions,
+    test_record_index,
+):
+
+    served_keywords_model = ServedKeywordsModel(
+        served_model_params=test_served_model_params
+    )
     served_keywords_model.load()
 
-    assert served_keywords_model.is_ready()
+    input = PredictRequestModel(
+        configuration=PredictConfiguration(**test_inference_params),
+        inputs=[PredictInputDocumentModel(document=test_inputs[test_record_index])],
+    )
+
+    actual_output = served_keywords_model.predict(input)
+
+    expected_output = PredictResponseModel(
+        outputs=[
+            PredictionOutputDocument(
+                predicted_document=[
+                    PredictionExtractedKeyword(keyword_token=kt, keyword_score=ks)
+                    for kt, ks in test_predictions[test_record_index]
+                ]
+            )
+        ]
+    )
+
+    assert actual_output == expected_output
