bf6e0a020eb6d024e531e3ba629fcd332c660e3a
Sebastian Scherer <scherersebastian@yahoo.de>
2023-06-28 20:35:33 +0000

    moved the model server integration test into the functiona suite as it requires a client <-> server setup we cant really support withouth docker compose. this frees up the integration level suite for the library in the CI, so we can at least test the new testing load testing subpackage e2e

--- libs/serving/onclusiveml/tests/integration/conftest.py
+++ libs/serving/onclusiveml/tests/integration/conftest.py
@@ -1,26 +1,17 @@
 # 3rd party libraries
 import pytest
 
-
-@pytest.fixture
-def test_api_version():
-
-    return "test-version"
-
-
-@pytest.fixture
-def test_port():
-
-    return 8000
-
-
-@pytest.fixture
-def test_model_name():
-
-    return "test-model"
+# Internal libraries
+from onclusiveml.serving.rest.serve import ServingBaseParams
 
 
 @pytest.fixture
 def test_locustfile():
 
     return "libs/serving/onclusiveml/tests/integration/rest/testing/test_locustfile.py"
+
+
+@pytest.fixture
+def test_serving_base_params_env_prefix():
+
+    return ServingBaseParams.__config__.env_prefix
