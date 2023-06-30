d743902c962c5ac45e35eec0d5bb39a9454ed47d
Sebastian Scherer <scherersebastian@yahoo.de>
2023-06-30 19:48:43 +0000

    added new model download service that can be run from the development build stage of the serve image. updated the dev version of the docker compose file. also had to update all libs poetry files to ensure: 1) dev dependencies are listed in the dev group, not the main one 2) pytest is consistently pinned to 7.3.2

---
+++ projects/keywords/serve/src/download_compiled_model.py
@@ -0,0 +1,35 @@
+# Standard Library
+import os
+
+# Internal libraries
+from onclusiveml.core.logging import get_default_logger
+from onclusiveml.tracking import TrackedModelVersion
+
+# Source
+from src.downloading_params import CompiledKeyWordModelSpecs, ServedModelParams
+
+
+if __name__ == "__main__":
+
+    logger = get_default_logger(__name__)
+    # model registry reference to the desired (compiled) model version
+    model_version_specs = CompiledKeyWordModelSpecs().dict()
+    # output directory specs
+    model_export_params = ServedModelParams()
+    # initialize client for specific model version
+    mv = TrackedModelVersion(**model_version_specs)
+
+    if not os.path.isdir(model_export_params.model_directory):
+        # if the target dir does not exist, download all model artifacts for the model version to
+        # local
+        mv.download_directory_from_model_version(
+            local_directory_path=model_export_params.model_directory,
+            neptune_attribute_path="model",
+        )
+    else:
+        logger.info(
+            f"The specified output directory {model_export_params.model_directory} already "
+            "exists. Model download skipped."
+        )
+    # shutdown client
+    mv.stop()
