12642b63f30ee60b2419ee379526f0c05963906c
Sebastian Scherer <scherersebastian@yahoo.de>
2023-07-03 09:51:08 +0000

    tweaked the model download export directory to ensure model versions get their own directories. updated the compose file for download-model, serve, integration and functional accordingly.

--- projects/keywords/serve/src/download_compiled_model.py
+++ projects/keywords/serve/src/download_compiled_model.py
@@ -13,11 +13,11 @@ if __name__ == "__main__":
 
     logger = get_default_logger(__name__)
     # model registry reference to the desired (compiled) model version
-    model_version_specs = TrackedCompiledModelSpecs().dict()
+    model_version_specs = TrackedCompiledModelSpecs()
     # output directory specs
     model_export_params = ServedModelParams()
     # initialize client for specific model version
-    mv = TrackedModelVersion(**model_version_specs)
+    mv = TrackedModelVersion(**model_version_specs.dict())
 
     if not os.path.isdir(model_export_params.model_directory):
         # if the target dir does not exist, download all model artifacts for the model version to
@@ -26,10 +26,18 @@ if __name__ == "__main__":
             local_directory_path=model_export_params.model_directory,
             neptune_attribute_path="model",
         )
+    elif not os.listdir(model_export_params.model_directory):
+        # if the target dir does exist but is empty, download all model artifacts for the model
+        # version to local
+        mv.download_directory_from_model_version(
+            local_directory_path=model_export_params.model_directory,
+            neptune_attribute_path="model",
+        )
     else:
         logger.info(
             f"The specified output directory {model_export_params.model_directory} already "
-            "exists. Model download skipped."
+            f"exists and is not empty: {os.listdir(model_export_params.model_directory)}. Model "
+            "download skipped."
         )
     # shutdown client
     mv.stop()
