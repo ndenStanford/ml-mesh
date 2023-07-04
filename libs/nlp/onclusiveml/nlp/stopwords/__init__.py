357ab0c50114779e010254da820b65edce4da3bd
sreza1 <shrezash98@gmail.com>
2023-07-04 15:45:39 +0000

    stopwords package

---
+++ libs/nlp/onclusiveml/nlp/stopwords/__init__.py
@@ -0,0 +1,5 @@
+"""Init."""
+from onclusiveml.nlp.stopwords.stopwords import (  # noqa: F401
+    load_stop_words_file,
+    stopwords,
+)
---
+++ libs/nlp/onclusiveml/nlp/stopwords/stopwords.py
@@ -0,0 +1,40 @@
+# Standard Library
+import json
+import os
+from functools import lru_cache
+from typing import List, Optional
+
+# Internal libraries
+# Internal Libraries
+from onclusiveml.nlp.stopwords.stopwords_exception import StopwordsFileException
+
+
+def _get_stopword_filepath(lang: str) -> str:
+    """Returns stopword filename for a language."""
+    return os.path.join("libs/nlp/onclusiveml/nlp/stopwords/data", f"{lang}.json")
+
+
+@lru_cache()
+def load_stop_words_file(lang: str) -> List[str]:
+    """Loads stop words from file."""
+    content = []
+    try:
+        with open(_get_stopword_filepath(lang)) as f:
+            content = json.loads(f.read())
+    except FileNotFoundError:
+        raise StopwordsFileException(language=lang)
+    return content
+    # return files
+
+
+def stopwords(
+    lang: str, content: Optional[List[str]] = None, lowercase: Optional[bool] = False
+) -> List[str]:
+    if content:
+        if len(content) > 0:
+            if lowercase:
+                content = list(map(str.lower, content))
+            stopwords = load_stop_words_file(lang=lang)
+            res = [words for words in content if words not in stopwords]
+            return res
+    return []
