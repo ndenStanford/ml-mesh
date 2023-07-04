291ce22c19b2dfd212c4db5402abe6984e015f48
Sebastian Scherer <scherersebastian@yahoo.de>
2023-07-04 18:38:31 +0000

    combined unit tests as suggested. also added doc strings as suggested

--- projects/keywords/serve/tests/unit/serving_params_test.py
+++ projects/keywords/serve/tests/unit/serving_params_test.py
@@ -2,12 +2,13 @@
 import builtins
 import io
 import json
+import os
 
 # 3rd party libraries
 import pytest
 
 # Source
-from src.serving_params import ServedModelArtifacts
+from src.serving_params import ServedModelArtifacts, ServedModelParams
 
 
 @pytest.mark.parametrize(
@@ -38,6 +39,9 @@ def test_served_model_artifacts(
     test_inference_params_test_file_expected,
     test_predictions_test_file_expected,
 ):
+    """Tests the constructor method of the ServedModelArtifacts class, mocking up the json.load
+    method. In particular, checks for the behaviour of the constructor arg `remove_model_prefix`."""
+
     def mock_load(json_file):
         return test_model_card
 
@@ -73,3 +77,42 @@ def test_served_model_artifacts(
         served_model_artifacts.predictions_test_file
         == test_predictions_test_file_expected  # noqa: W503
     )
+
+
+def test_keywords_served_model_params():
+    """Tests the initialization of the ServedModelParams class"""
+    ServedModelParams()
+
+
+def test_served_model_params_env_prefix():
+    """Tests the inherited environment variable prefix (from KeywordsServingBaseParams) of the
+    ServedModelParams class"""
+
+    assert ServedModelParams.__config__.env_prefix == "onclusiveml_serving_"
+
+
+@pytest.mark.parametrize(
+    "test_field_name, test_field_value_expected",
+    [
+        ("model_name", "test_model_name"),
+        ("model_directory", "test_dir"),
+    ],
+)
+def test_served_model_params_set_fields_via_env_vars(
+    test_field_name, test_field_value_expected
+):
+    """Tests the correct behaviour of the inherited environment variable prefix (from
+    KeywordsServingBaseParams) of the ServedModelParams class's fields by initializing an instance
+    after setting the corresponding environment variables in the local test scope."""
+
+    prefixed_field_env_var_ref = f"onclusiveml_serving_{test_field_name}"
+    os.environ[prefixed_field_env_var_ref] = test_field_value_expected
+
+    test_keywords_served_model_params = ServedModelParams()
+
+    test_attribute_values_actual = getattr(
+        test_keywords_served_model_params, test_field_name
+    )
+    assert test_attribute_values_actual == test_field_value_expected
+
+    del os.environ[prefixed_field_env_var_ref]
