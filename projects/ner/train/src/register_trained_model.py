d146d876aeaa1000966c07b45b41b949fd378d38
sreza1 <shrezash98@gmail.com>
2023-07-06 11:33:53 +0000

    add logging and update docker compose

--- projects/ner/train/src/register_trained_model.py
+++ projects/ner/train/src/register_trained_model.py
@@ -29,6 +29,7 @@ def main() -> None:
     model_version = TrackedModelVersion(**model_specs.dict())
     # --- initialize models
     # get pretrained model and tokenizer
+    logger.info("Initializing model and tokenizer")
     tokenizer = AutoTokenizer.from_pretrained(
         model_card.model_params.huggingface_model_reference
     )
@@ -36,6 +37,7 @@ def main() -> None:
         model_card.model_params.huggingface_model_reference, return_dict=False
     )
     # Create pipeline using ner model and tokenizer
+    logger.info("Creating huggingface pipeline")
     hf_pipeline = pipeline(
         task=model_card.model_params.huggingface_pipeline_task,
         model=model,
@@ -44,6 +46,7 @@ def main() -> None:
     # ner settings
     ner_settings = model_card.model_params.ner_settings.dict()
     # --- create prediction files
+    logger.info("Making predictions from example inputs")
     ner_predictions: List[List[Dict[str, Union[str, float, int]]]] = hf_pipeline(
         model_card.model_inputs.sample_documents
     )
@@ -54,6 +57,7 @@ def main() -> None:
             dictionary["score"] = float(dictionary["score"])
     # --- add assets to registered model version on neptune ai
     # testing assets - inputs, inference specs and outputs
+    logger.info("Pushing assets to neptune AI")
     for (test_file, test_file_attribute_path) in [
         (model_card.model_inputs.sample_documents, model_card.model_test_files.inputs),
         (ner_settings, model_card.model_test_files.inference_params),
@@ -62,6 +66,7 @@ def main() -> None:
         model_version.upload_config_to_model_version(
             config=test_file, neptune_attribute_path=test_file_attribute_path
         )
+    logger.info("Pushing model artifact and assets to s3")
     # model artifact
     hf_pipeline_local_dir = os.path.join(model_card.local_output_dir, "hf_pipeline")
     hf_pipeline.save_pretrained(hf_pipeline_local_dir)
