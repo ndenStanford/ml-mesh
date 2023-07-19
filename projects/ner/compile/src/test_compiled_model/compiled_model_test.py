a27db73961639c2376ada2d9ee45ef99981bb903
sreza1 <shrezash98@gmail.com>
2023-07-19 14:33:33 +0000

    change batch size in config to 6

--- projects/ner/compile/src/test_compiled_model/compiled_model_test.py
+++ projects/ner/compile/src/test_compiled_model/compiled_model_test.py
@@ -27,7 +27,12 @@ def to_dataframe(extract_entites: List[dict]) -> pd.DataFrame:
     return df_sorted
 
 
-# @pytest.mark.order(1)
+"""
+NOTE: handler makes changes to the output by merging inner and outer tags
+into one tag and so modify expected predictions here
+"""
+
+
 @pytest.mark.parametrize(
     "test_sample_index, expected_predictions",
     [
@@ -37,7 +42,7 @@ def to_dataframe(extract_entites: List[dict]) -> pd.DataFrame:
                 {
                     "entity_type": "ORG",
                     "entity_text": "Google",
-                    "score": "0.99824464",
+                    "score": "0.9981779",
                     "sentence_index": 0,
                     "start": 0,
                     "end": 6,
@@ -45,7 +50,7 @@ def to_dataframe(extract_entites: List[dict]) -> pd.DataFrame:
                 {
                     "entity_type": "LOC",
                     "entity_text": "Mountain View",
-                    "score": "0.9991042",
+                    "score": "0.99910104",
                     "sentence_index": 0,
                     "start": 16,
                     "end": 29,
@@ -53,7 +58,7 @@ def to_dataframe(extract_entites: List[dict]) -> pd.DataFrame:
                 {
                     "entity_type": "LOC",
                     "entity_text": "CA",
-                    "score": "0.99936754",
+                    "score": "0.99936694",
                     "sentence_index": 0,
                     "start": 31,
                     "end": 33,
@@ -66,7 +71,7 @@ def to_dataframe(extract_entites: List[dict]) -> pd.DataFrame:
                 {
                     "entity_type": "LOC",
                     "entity_text": "Gulf Stream",
-                    "score": "0.9891915",
+                    "score": "0.9890644",
                     "sentence_index": 0,
                     "start": 21,
                     "end": 32,
@@ -74,7 +79,7 @@ def to_dataframe(extract_entites: List[dict]) -> pd.DataFrame:
                 {
                     "entity_type": "LOC",
                     "entity_text": "Cape Cod",
-                    "score": "0.9872701",
+                    "score": "0.9871611",
                     "sentence_index": 0,
                     "start": 81,
                     "end": 89,
@@ -87,7 +92,7 @@ def to_dataframe(extract_entites: List[dict]) -> pd.DataFrame:
                 {
                     "entity_type": "LOC",
                     "entity_text": "Jupiter",
-                    "score": "0.99235547",
+                    "score": "0.99254674",
                     "sentence_index": 0,
                     "start": 105,
                     "end": 112,
@@ -100,7 +105,7 @@ def to_dataframe(extract_entites: List[dict]) -> pd.DataFrame:
                 {
                     "entity_type": "ORG",
                     "entity_text": "Loggerhead",
-                    "score": "0.6166753",
+                    "score": "0.62161815",
                     "sentence_index": 0,
                     "start": 10,
                     "end": 20,
@@ -108,7 +113,7 @@ def to_dataframe(extract_entites: List[dict]) -> pd.DataFrame:
                 {
                     "entity_type": "LOC",
                     "entity_text": "Marinelife Center",
-                    "score": "0.8741132",
+                    "score": "0.8719994",
                     "sentence_index": 0,
                     "start": 21,
                     "end": 38,
