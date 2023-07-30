6aaee772ecf0ffe4288d5bde8e2f52e917fe461c
Sebastian Scherer <sebastian.scherer@onclusive.com>
2023-07-30 07:54:57 +0000

    added new methods to unit test checking methods. also added init to resolve name duplication issue when running entire test suite

--- libs/compile/onclusiveml/tests/unit/conftest.py
+++ libs/compile/onclusiveml/tests/unit/conftest.py
@@ -44,6 +44,9 @@ class MockTokenizer(object):
 
     def __init__(self):
         self.model_max_length = MODEL_MAX_LENGTH
+        self.is_fast = True
+        self._tokenizer = None
+        self.unk_token_id = "UNK"
 
     def encode_plus(self, text: str):
 
@@ -70,6 +73,12 @@ class MockTokenizer(object):
     def clean_up_tokenization(self, text: str):
 
         return text.replace(" .", "").replace(" ,", ",")
+
+    def convert_ids_to_tokens(self, ids: List[int]):
+
+        return [
+            "",
+        ] * len(ids)
 
     def __call__(self, text: str, padding: str = None, **kwargs):
 
@@ -139,4 +148,5 @@ def all_delegated_method_references_with_sample_inputs():
         ("create_token_type_ids_from_sequences", ["some", "example", "text"]),
         ("convert_tokens_to_string", [0, 1, 2]),
         ("clean_up_tokenization", "some ,example text ."),
+        ("convert_ids_to_tokens", [1, 2, 3]),
     )
