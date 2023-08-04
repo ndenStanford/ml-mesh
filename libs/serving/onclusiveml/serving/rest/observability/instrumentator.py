26eb4e43b74b44caee6b4de4c84df9b507b170a0
vincent.park <vincent.park@onclusive.com>
2023-08-04 20:54:48 +0000

    Remove opentelemetry exporter

--- libs/serving/onclusiveml/serving/rest/observability/instrumentator.py
+++ libs/serving/onclusiveml/serving/rest/observability/instrumentator.py
@@ -1,6 +1,3 @@
-# Standard Library
-import os
-
 # 3rd party libraries
 from fastapi import FastAPI
 
@@ -9,10 +6,6 @@ from onclusiveml.serving.rest.observability.handlers import metrics
 from onclusiveml.serving.rest.observability.middlewares import (
     PrometheusMiddleware,
 )
-from onclusiveml.serving.rest.observability.opentelemetry import setting_otlp
-
-
-OTLP_GRPC_ENDPOINT = os.environ.get("OTLP_GRPC_ENDPOINT", "http://tempo:4317")
 
 
 class Instrumentator:
@@ -21,20 +14,15 @@ class Instrumentator:
         app: FastAPI,
         app_name: str = "FastAPI App",
         metrics_endpoint: str = "/metrics",
-        setup_otl_exporter: bool = False,
     ):
         self.app = app
         self.app_name = app_name
         self.metrics_endpoint = metrics_endpoint
-        self.setup_otl_exporter = setup_otl_exporter
 
     def setup(self) -> "Instrumentator":
         # Setting metrics middleware
         self.app.add_middleware(PrometheusMiddleware, app_name=self.app_name)
         self.app.add_route(self.metrics_endpoint, metrics)
-        # Setting OpenTelemetry exporter
-        if self.setup_otl_exporter:
-            setting_otlp(self.app, self.app_name, OTLP_GRPC_ENDPOINT)
         return self
 
     @staticmethod
