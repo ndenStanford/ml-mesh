4c41b95d899bbecff2c93984baa93aa9047ac505
vincent.park <vincent.park@onclusive.com>
2023-08-08 14:33:44 +0000

    Clean up noqa

--- libs/serving/onclusiveml/serving/rest/observability/__init__.py
+++ libs/serving/onclusiveml/serving/rest/observability/__init__.py
@@ -1,12 +1,12 @@
 from onclusiveml.serving.rest.observability.utils import get_path  # noqa: F401
 from onclusiveml.serving.rest.observability.metrics import (  # noqa: F401
-    INFO,  # noqa: F401
-    REQUESTS_IN_PROGRESS,  # noqa: F401
-    REQUESTS,  # noqa: F401
-    EXCEPTIONS,  # noqa: F401
-    REQUESTS_PROCESSING_TIME,  # noqa: F401
-    RESPONSES,  # noqa: F401
-)  # noqa: F401
+    INFO,
+    REQUESTS_IN_PROGRESS,
+    REQUESTS,
+    EXCEPTIONS,
+    REQUESTS_PROCESSING_TIME,
+    RESPONSES,
+)
 from onclusiveml.serving.rest.observability.instrumentator import (  # noqa: F401
-    Instrumentator,  # noqa: F401
-)  # noqa: F401
+    Instrumentator,
+)
