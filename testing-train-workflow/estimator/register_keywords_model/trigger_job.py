4a38a00e823cd97fe7aa9fbad2bb53b090ea675a
Sebastian Scherer <sebastian.scherer@onclusive.com>
2023-08-10 14:43:26 +0000

    stashing progress

--- testing-train-workflow/estimator/register_keywords_model/trigger_job.py
+++ testing-train-workflow/estimator/register_keywords_model/trigger_job.py
@@ -7,11 +7,12 @@ def main():
 
     estimator = Estimator(
         image_uri='063759612765.dkr.ecr.us-east-1.amazonaws.com/keywords-train:seb-test-sagemaker',
+        #image_uri='427cdea66e52',
         base_job_name="register-keywords-model",
         role='arn:aws:iam::063759612765:role/mesh-sagemaker-execution-role-dev',
         instance_count=1,
-        instance_type='ml.m4.xlarge', # ("local", "local_gpu") for local mode
-        #instance_type='local',
+        #instance_type='ml.m4.xlarge', # ("local", "local_gpu") for local mode
+        instance_type='local',
         environment = {'NEPTUNE_API_TOKEN': os.environ['NEPTUNE_API_TOKEN']}
     )
 
