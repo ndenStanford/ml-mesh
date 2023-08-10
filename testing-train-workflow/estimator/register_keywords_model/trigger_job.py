d80f072bbc839b5628a835161ba954ce9d54015a
Sebastian Scherer <sebastian.scherer@onclusive.com>
2023-08-10 17:26:23 +0000

    added sagemaker entrypoint to production layer of keywords train and tested successfully on local

--- testing-train-workflow/estimator/register_keywords_model/trigger_job.py
+++ testing-train-workflow/estimator/register_keywords_model/trigger_job.py
@@ -1,9 +1,15 @@
 from sagemaker.estimator import Estimator
-from dotenv import dotenv_values
 import os
 from datetime import datetime as dt
+import click
 
-def main():
+@click.command()
+@click.option(
+    '--instance-type',
+    default='ml.m4.xlarge',
+    help="Select 'local'/'local_gpu' for testing. Defaults to 'ml.m4.xlarge'."
+)
+def trigger_sagemaker_train(instance_type):
 
     estimator = Estimator(
         image_uri='063759612765.dkr.ecr.us-east-1.amazonaws.com/keywords-train:seb-test-sagemaker',
@@ -11,12 +17,11 @@ def main():
         base_job_name="register-keywords-model",
         role='arn:aws:iam::063759612765:role/mesh-sagemaker-execution-role-dev',
         instance_count=1,
-        #instance_type='ml.m4.xlarge', # ("local", "local_gpu") for local mode
-        instance_type='local',
+        instance_type=instance_type,
         environment = {'NEPTUNE_API_TOKEN': os.environ['NEPTUNE_API_TOKEN']}
     )
 
     estimator.fit(job_name=dt.now().strftime('%Y-%m-%d--%H-%M-%S'))
 
 if __name__ == "__main__":
-    main()
+    trigger_sagemaker_train()
