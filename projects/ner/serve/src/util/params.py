7286494777c6f6ff1060b0a2b4fc9b6d358d2bcd
Syed Reza <syed.reza@onclusive.com>
2023-08-22 12:06:52 +0000

    get ci back to original state

--- projects/ner/serve/src/util/params.py
+++ projects/ner/serve/src/util/params.py
@@ -9,6 +9,6 @@ from onclusiveml.tracking import TrackedModelSpecs
 class TrackedCompiledModelSpecs(TrackedModelSpecs):
     # we need an additional version tag since we are referencing an EXISTING model version, rather
     # than creating a new one
-    with_id: str = Field("NER-COMPILED-11", env="neptune_model_version_id")
+    with_id: str = Field("NER-COMPILED-12", env="neptune_model_version_id")
     # we only need to download from the base model, not upload
     mode: str = Field(Mode.READ_ONLY, env="neptune_client_mode")
