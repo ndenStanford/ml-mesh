4dd8b7ae788eaf27a10be0cc0499615f2c391d19
vishal-singh <vishal.singh@onclusive.com>
2023-09-06 11:29:20 +0000

    ML-196-write-data-library-with-feature-store-module

--- libs/data/onclusiveml/data/feature_store/__init__.py
+++ libs/data/onclusiveml/data/feature_store/__init__.py
@@ -1,4 +1,4 @@
 """Init."""
 
-from onclusiveml.data.feature_store.feature_store_handle import FeatureStoreHandle
-from onclusiveml.data.feature_store.redshift_source import RedshiftSourceCustom
+from onclusiveml.data.feature_store.feature_store_handle import FeatureStoreHandle # noqa: F401
+from onclusiveml.data.feature_store.redshift_source import RedshiftSourceCustom # noqa: F401
