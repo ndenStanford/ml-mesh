1271ce00c409513f196833ab855bf500ceeb662f
Rene-Jean Corneille <rene-jean.corneille@onclusive.com>
2023-09-10 10:01:28 +0000

    working entity linking rewrite

--- projects/entity-linking/serve/src/__main__.py
+++ projects/entity-linking/serve/src/__main__.py
@@ -1,19 +1,21 @@
 """Main."""
 
+# Internal libraries
+from onclusiveml.serving.rest.observability import Instrumentator
+from onclusiveml.serving.rest.serve import ModelServer
+
 # Source
-from src.model import EntityLinkingModel
+from src.serve.model import EntityLinkingServedModel
+from src.settings import get_settings
 
 
-class ServedModelParams(ServingBaseParams):
-    """Serve model parameters."""
-
-    model_name: str = "entity-linking"
-    model_directory: Union[str, Path] = "."
+settings = get_settings()
 
 
 if __name__ == "__main__":
     # launch server process(es)
     model_server = ModelServer(
-        configuration=ServedModelParams(), model=EntityLinkingModel()
+        configuration=settings, model=EntityLinkingServedModel(name=settings.model_name)
     )
+    Instrumentator.enable(model_server, app_name=settings.model_name)
     model_server.serve()
--- projects/entity-linking/serve/src/settings.py
+++ projects/entity-linking/serve/src/settings.py
@@ -2,21 +2,38 @@
 
 # Standard Library
 from functools import lru_cache
+from pathlib import Path
+from typing import Union
 
 # 3rd party libraries
 from pydantic import BaseSettings, SecretStr
 
+# Internal libraries
+from onclusiveml.core.base import OnclusiveBaseSettings
+from onclusiveml.serving.rest.serve.params import ServingParams
 
-class Settings(BaseSettings):
+
+class ServedModelSettings(ServingParams):
+    """Serve model parameters."""
+
+    model_name: str = "entity-linking"
+    model_directory: Union[str, Path] = "."
+
+
+class ApplicationSettings(OnclusiveBaseSettings):
     """App base settings."""
 
-    API_KEY_NAME: str = "x-api-key"
-    ENTITY_FISHING_ENDPOINT: str
-    INTERNAL_ML_ENDPOINT_API_KEY: SecretStr
-    ENTITY_RECOGNITION_ENDPOINT: str
+    api_key_name: str = "x-api-key"
+    entity_fishing_endpoint: str
+    internal_ml_api_key: SecretStr
+    entity_recognition_endpoint: str
+
+
+class GlobalSettings(ServedModelSettings, ApplicationSettings):
+    """Global server settings."""
 
 
 @lru_cache
 def get_settings() -> BaseSettings:
-    """Returns instanciated Settings class."""
-    return Settings()
+    """Returns instanciated global settings class."""
+    return GlobalSettings()
