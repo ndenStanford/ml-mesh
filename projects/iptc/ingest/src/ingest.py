1f219f1af97e1e59d71fb78e339e4810dafd2141
Ivan Okhotnikov <ivan.okhotnikov@onclusive.com>
2023-09-15 13:45:11 +0000

    add to dependabot, read from source, write to target

--- projects/iptc/ingest/src/ingest.py
+++ projects/iptc/ingest/src/ingest.py
@@ -1,31 +1,38 @@
 """Ingest component."""
-# Standard libraries
 
 # Standard Library
 import os
 
 # 3rd party libraries
 import boto3
+from pyarrow import csv
+from pyarrow import parquet as pq
 
 
-def ingest(source_bucket: str, target_bucket: str) -> None:
+def ingest(source_bucket_name: str, target_bucket_name: str) -> None:
     """Read the opoint data, write to the data lake bucket in .parquet.
 
     Args:
-        source_bucket (str): S3 bucket name with source, raw data
-        target_bucket (str): S3 bucket name with target, ingested data
+        source_bucket_name (str): S3 bucket name with source, raw data
+        target_bucket_name (str): S3 bucket name with target, ingested data
     """
-    # Retrieve the list of existing buckets
-    s3 = boto3.client("s3")
-    response = s3.list_buckets()
-    # Output the bucket names
-    print("Existing buckets:")
-    for bucket in response["Buckets"]:
-        print(f'  {bucket["Name"]}')
+    s3 = boto3.resource("s3")
+
+    for obj in s3.Bucket(source_bucket_name).objects.all():
+        current_object = obj.get()
+        root_dir, sub_dir, filename = obj.key.split("/")
+        df_pa = csv.read_csv(
+            current_object["Body"],
+            parse_options=csv.ParseOptions(invalid_row_handler=lambda x: "skip"),
+        )
+        pq.write_table(
+            df_pa,
+            f"s3://{target_bucket_name}/{root_dir}/{sub_dir}/{filename.replace('csv', 'parquet')}",
+        )
 
 
 if __name__ == "__main__":
     ingest(
-        source_bucket=os.environ["SOURCE_BUCKET"],
-        target_bucket=os.environ["TARGET_BUCKET"],
+        source_bucket_name=os.environ["SOURCE_BUCKET"],
+        target_bucket_name=os.environ["TARGET_BUCKET"],
     )
