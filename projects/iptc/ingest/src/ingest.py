7628609fc84f3e565307a5afd610d0e6af672906
Ivan Okhotnikov <ivan.okhotnikov@onclusive.com>
2023-09-18 17:21:58 +0000

    add beam

--- projects/iptc/ingest/src/ingest.py
+++ projects/iptc/ingest/src/ingest.py
@@ -4,9 +4,10 @@
 import os
 
 # 3rd party libraries
+import apache_beam as beam
 import boto3
-from pyarrow import csv
-from pyarrow import parquet as pq
+import pyarrow
+from apache_beam.io import ReadFromText, WriteToParquet
 
 
 def ingest(
@@ -21,21 +22,44 @@ def ingest(
     """
     s3 = boto3.resource("s3")
 
-    for obj in s3.Bucket(source_bucket_name).objects.all():
-        current_object = obj.get()
-        _, sub_dir, filename = obj.key.split("/")
-        df_pa = csv.read_csv(
-            current_object["Body"],
-            parse_options=csv.ParseOptions(invalid_row_handler=lambda x: "skip"),
-        )
-        pq.write_table(
-            table=df_pa,
-            where=f"s3://{target_bucket_name}/iptc/{sub_dir}/{filename.replace('csv', 'parquet')}"
-            if not test
-            else f"s3://{target_bucket_name}/test/{sub_dir}/{filename.replace('csv', 'parquet')}",
-        )
-        if test:
-            break
+    with beam.Pipeline() as p:
+        for obj in s3.Bucket(source_bucket_name).objects.all():
+            target_key = (
+                obj.key.replace("csv", "parquet").replace("raw", "test")
+                if test
+                else obj.key.replace("csv", "parquet").replace("raw", "iptc")
+            )
+            _ = (
+                p
+                | ReadFromText(f"s3://{source_bucket_name}/{obj.key}")
+                | WriteToParquet(
+                    f"s3://{target_bucket_name}/{target_key}",
+                    schema=pyarrow.schema(
+                        [
+                            ("topic_1", pyarrow.string()),
+                            ("topic_2", pyarrow.string()),
+                            ("title", pyarrow.string()),
+                            ("language", pyarrow.string()),
+                            ("content", pyarrow.string()),
+                        ]
+                    ),
+                )
+            )
+            if test:
+                break
+    # for obj in s3.Bucket(source_bucket_name).objects.all():
+    #     current_object = obj.get()
+    #     _, sub_dir, filename = obj.key.split("/")
+    #     df_pa = csv.read_csv(
+    #         current_object["Body"],
+    #         parse_options=csv.ParseOptions(invalid_row_handler=lambda x: "skip"),
+    #     )
+    #     pq.write_table(
+    #         table=df_pa,
+    #         where=f"s3://{target_bucket_name}/iptc/{sub_dir}/{filename.replace('csv', 'parquet')}"
+    #         if not test
+    #         else f"s3://{target_bucket_name}/test/{sub_dir}/{filename.replace('csv', 'parquet')}",
+    #     )
 
 
 if __name__ == "__main__":
