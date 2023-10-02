9b0fb12381cae0c0d7cbcfddbc85fdb5124bbf49
Sebastian Scherer <sebastian.scherer@onclusive.com>
2023-10-02 11:42:42 +0000

    updated unit tests

--- projects/keywords/compile/tests/unit/settings_test.py
+++ projects/keywords/compile/tests/unit/settings_test.py
@@ -4,20 +4,26 @@
 from src.settings import (
     CompiledKeywordsTrackedModelCard,
     CompiledTrackedModelSpecs,
-    IOSettings,
+    CompilePipelineExecutionSettings,
+    CompilePipelineIOSettings,
 )
 
 
 def test_io_settings():
-    """IO settings fixture."""
-    IOSettings()
+    """Tests pipeline IO settings."""
+    CompilePipelineIOSettings()
+
+
+def test_compile_pipeline_execution_settings():
+    """Tests pipeline execution settings."""
+    CompilePipelineExecutionSettings()
 
 
 def test_compiled_tracked_model_specs():
-    """Compiled tracked model specs fixture."""
+    """Tests Compiled tracked model specs."""
     CompiledTrackedModelSpecs()
 
 
 def test_compiled_keywords_tracked_model_card():
-    """Compiled keywords model card fixture."""
+    """Tests compiled keywords model card."""
     CompiledKeywordsTrackedModelCard()
