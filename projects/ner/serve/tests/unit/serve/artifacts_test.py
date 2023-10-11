4e8ac8d302458d4b1c2e29a27790875d4907b5fc
Rene-Jean Corneille <rene-jean.corneille@onclusive.com>
2023-10-11 18:02:33 +0000

    fix env variable get

--- projects/ner/serve/tests/unit/serve/artifacts_test.py
+++ projects/ner/serve/tests/unit/serve/artifacts_test.py
@@ -16,8 +16,8 @@ from src.serve.artifacts import ServedModelArtifacts
     [
         (
             False,
-            f"models/{os.environ('NEPTUNE_MODEL_VERSION_ID')}/models/model_card",
-            f"models/{os.environ('NEPTUNE_MODEL_VERSION_ID')}/model/some/other/dir",
+            f"models/{os.environ.get('NEPTUNE_MODEL_VERSION_ID')}/models/model_card",
+            f"models/{os.environ.get('NEPTUNE_MODEL_VERSION_ID')}/model/some/other/dir",
         ),  # TODO: add test case for remove_model_prefix=True
     ],
 )
