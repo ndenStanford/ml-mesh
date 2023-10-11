2b0aba0210f533f9ac9d179e9438530e401b9ce2
zheyuan.hu <zheyuan.hu@onclusive.com>
2023-10-11 00:42:34 +0000

    register pipelines to upload more assets

--- projects/summarization/train/src/register_trained_model.py
+++ projects/summarization/train/src/register_trained_model.py
@@ -43,6 +43,11 @@ def main() -> None:
     model_en = BartForConditionalGeneration.from_pretrained(
         model_card.model_params_en.huggingface_model_reference
     )
+    hf_pipeline_en = pipeline(
+        task=model_card.model_params_en.huggingface_pipeline_task,
+        model=model_en,
+        tokenizer=tokenizer_en,
+    )    
     
     logger.info("Initializing model and tokenizer for French and German")
     tokenizer_frde = AutoTokenizer.from_pretrained(
@@ -54,6 +59,12 @@ def main() -> None:
         model_card.model_params_frde.huggingface_model_reference
     )
     
+    hf_pipeline_frde = pipeline(
+        task=model_card.model_params_frde.huggingface_pipeline_task,
+        model=model_frde,
+        tokenizer=tokenizer_frde,
+    )    
+    
     logger.info("Initializing model and tokenizer for Spanish")
     tokenizer_es = AutoTokenizer.from_pretrained(
         model_card.model_params_es.huggingface_model_reference,
@@ -63,6 +74,12 @@ def main() -> None:
     model_es = BartForConditionalGeneration.from_pretrained(
         model_card.model_params_es.huggingface_model_reference
     )
+    
+    hf_pipeline_es = pipeline(
+        task=model_card.model_params_es.huggingface_pipeline_task,
+        model=model_es,
+        tokenizer=tokenizer_es,
+    )    
     
     logger.info("Initializing model and tokenizer for Catalan")
     tokenizer_ca = AutoTokenizer.from_pretrained(
@@ -74,6 +91,12 @@ def main() -> None:
         model_card.model_params_ca.huggingface_model_reference
     )
     
+    hf_pipeline_ca = pipeline(
+        task=model_card.model_params_ca.huggingface_pipeline_task,
+        model=model_ca,
+        tokenizer=tokenizer_ca,
+    )    
+    
     logger.info("Initializing model and tokenizer for Italian")
     tokenizer_it = AutoTokenizer.from_pretrained(
         model_card.model_params_it.huggingface_model_reference,
@@ -84,8 +107,17 @@ def main() -> None:
         model_card.model_params_it.huggingface_model_reference
     )
     
-    tokenizers = [tokenizer_en, tokenizer_frde, tokenizer_es, tokenizer_ca, tokenizer_it]
-    models = [model_en, model_frde, model_es, model_ca, model_it]
+    hf_pipeline_it = pipeline(
+        task=model_card.model_params_it.huggingface_pipeline_task,
+        model=model_it,
+        tokenizer=tokenizer_it,
+    )    
+    
+    pipelines = [hf_pipeline_en, 
+                 hf_pipeline_frde,
+                 hf_pipeline_es,
+                 hf_pipeline_ca,
+                 hf_pipeline_it,]
     
     # summarization settings
     sum_settings_en = model_card.model_params_en.sum_settings.dict()
@@ -94,7 +126,7 @@ def main() -> None:
     sum_settings_ca = model_card.model_params_ca.sum_settings.dict()
     sum_settings_it = model_card.model_params_it.sum_settings.dict()
     
-    sum_settings =  [sum_settings_en,
+    sum_settings =  [sum_settings_en, 
                      sum_settings_frde, 
                      sum_settings_es,
                      sum_settings_ca, 
@@ -107,23 +139,11 @@ def main() -> None:
     # Tokenize the sample documents
     sum_predictions = []
     for idx in range(len(sum_settings)):
-        inputs = tokenizers[idx](
-            model_card.model_inputs.sample_documents[idx],
-            return_tensors="pt",
-            truncation=True,
-            padding="max_length",
-            max_length=512
-        )
-    
-        # Generate summaries
-        summaries = models[idx].generate(
-            **inputs,
-            min_length=32,
-            max_length=128,
-            num_beams=1,
-            num_return_sequences=1)
-        # Decode the summaries
-        sum_predictions.append([tokenizers[idx].decode(t, skip_special_tokens=True) for t in summaries][0])
+        summaries = pipelines[idx](model_card.model_inputs.sample_documents[idx],
+                       min_length=32,
+                       max_length=128,
+                       num_beams=1)
+        sum_predictions.append(summaries[0]["summary_text"])
         
     # --- add assets to registered model version on neptune ai
     # testing assets - inputs, inference specs and outputs
@@ -139,40 +159,40 @@ def main() -> None:
 
     logger.info("Pushing model artifact and assets to s3")
     # model artifact
-    hf_model_local_dir_en = os.path.join(model_card.local_output_dir, "hf_model_en")
-    hf_model_local_dir_frde = os.path.join(model_card.local_output_dir, "hf_model_frde")
-    hf_model_local_dir_es = os.path.join(model_card.local_output_dir, "hf_model_es")
-    hf_model_local_dir_ca = os.path.join(model_card.local_output_dir, "hf_model_ca")
-    hf_model_local_dir_it = os.path.join(model_card.local_output_dir, "hf_model_it")
+    hf_pipeline_local_dir_en = os.path.join(model_card.local_output_dir, "hf_pipeline_en")
+    hf_pipeline_local_dir_frde = os.path.join(model_card.local_output_dir, "hf_pipeline_frde")
+    hf_pipeline_local_dir_es = os.path.join(model_card.local_output_dir, "hf_pipeline_es")
+    hf_pipeline_local_dir_ca = os.path.join(model_card.local_output_dir, "hf_pipeline_ca")
+    hf_pipeline_local_dir_it = os.path.join(model_card.local_output_dir, "hf_pipeline_it")
     
-    model_en.save_pretrained(hf_model_local_dir_en)
-    model_frde.save_pretrained(hf_model_local_dir_frde)
-    model_es.save_pretrained(hf_model_local_dir_es)
-    model_ca.save_pretrained(hf_model_local_dir_ca)
-    model_it.save_pretrained(hf_model_local_dir_it)
+    hf_pipeline_en.save_pretrained(hf_pipeline_local_dir_en)
+    hf_pipeline_frde.save_pretrained(hf_pipeline_local_dir_frde)
+    hf_pipeline_es.save_pretrained(hf_pipeline_local_dir_es)
+    hf_pipeline_ca.save_pretrained(hf_pipeline_local_dir_ca)
+    hf_pipeline_it.save_pretrained(hf_pipeline_local_dir_it)
 
     model_version.upload_directory_to_model_version(
-        local_directory_path=hf_model_local_dir_en,
+        local_directory_path=hf_pipeline_local_dir_en,
         neptune_attribute_path=model_card.model_artifact_attribute_path
         + model_card.en_model_subdirectory,
     )
     model_version.upload_directory_to_model_version(
-        local_directory_path=hf_model_local_dir_frde,
+        local_directory_path=hf_pipeline_local_dir_frde,
         neptune_attribute_path=model_card.model_artifact_attribute_path
         + model_card.frde_model_subdirectory,
     )
     model_version.upload_directory_to_model_version(
-        local_directory_path=hf_model_local_dir_es,
+        local_directory_path=hf_pipeline_local_dir_es,
         neptune_attribute_path=model_card.model_artifact_attribute_path
         + model_card.es_model_subdirectory,
     )
     model_version.upload_directory_to_model_version(
-        local_directory_path=hf_model_local_dir_ca,
+        local_directory_path=hf_pipeline_local_dir_ca,
         neptune_attribute_path=model_card.model_artifact_attribute_path
         + model_card.ca_model_subdirectory,
     )
     model_version.upload_directory_to_model_version(
-        local_directory_path=hf_model_local_dir_it,
+        local_directory_path=hf_pipeline_local_dir_it,
         neptune_attribute_path=model_card.model_artifact_attribute_path
         + model_card.it_model_subdirectory,
     )    
