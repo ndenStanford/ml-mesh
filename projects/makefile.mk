400a5dff5bd5ea44838b71cd1ca49bcdc26b46a7
Sebastian Scherer <sebastian.scherer@onclusive.com>
2023-10-13 16:23:27 +0000

    updated make targets so theres one for starting a components main task, and one for running its auxiliary tasks

--- projects/makefile.mk
+++ projects/makefile.mk
@@ -11,7 +11,10 @@ projects.install/%:
 projects.deploy/%: ## Deploy project component docker image to ECR.
 	docker compose -f ./projects/$(notdir projects/$@)/docker-compose.$(ENVIRONMENT).yaml push $(COMPONENT)
 
-projects.run/%: # Start development container of component
+projects.start/%: # Run main task of component in container
+	docker compose -f projects/$(notdir $@)/docker-compose.$(ENVIRONMENT).yaml --profile $(COMPONENT) up $(COMPONENT) --exit-code-from $(COMPONENT) --force-recreate
+
+projects.run/%: # Run auxiliary task of component in container
 	docker compose -f projects/$(notdir $@)/docker-compose.$(ENVIRONMENT).yaml --profile $(COMPONENT) up $(COMPONENT)-$(TASK) --exit-code-from $(COMPONENT)-$(TASK) --force-recreate
 
 projects.stop/%: # Start development container of component
