4c6cf9dbb1adec31d0a1e15cc063a345fad8c3c7
vishal <vishal.singh@onclusive.com>
2023-10-19 19:00:25 +0000

    fixed keywords serve integration tests according to the new API naming convention

--- projects/keywords/serve/tests/integration/serve/model_server_test.py
+++ projects/keywords/serve/tests/integration/serve/model_server_test.py
@@ -26,7 +26,7 @@ def test_get_model_server():
 @pytest.mark.order(5)
 def test_model_server_root(test_client):
     """Tests the root endpoint of a ModelServer (not running) instance."""
-    root_response = test_client.get("/v1/")
+    root_response = test_client.get("/keywords/v1/")
 
     assert root_response.status_code == 200
 
@@ -34,7 +34,7 @@ def test_model_server_root(test_client):
 @pytest.mark.order(6)
 def test_model_server_liveness(test_client):
     """Tests the liveness endpoint of a ModelServer (not running) instance."""
-    liveness_response = test_client.get("/v1/live")
+    liveness_response = test_client.get("/keywords/v1/live/")
 
     assert liveness_response.status_code == 200
     assert liveness_response.json() == LivenessProbeResponse().dict()
@@ -43,7 +43,7 @@ def test_model_server_liveness(test_client):
 @pytest.mark.order(6)
 def test_model_server_readiness(test_client):
     """Tests the readiness endpoint of a ModelServer (not running) instance."""
-    readiness_response = test_client.get("/v1/ready")
+    readiness_response = test_client.get("/keywords/v1/ready/")
 
     assert readiness_response.status_code == 200
     assert readiness_response.json() == ReadinessProbeResponse().dict()
