cd2252260c9c7f99c934c65d553bf2bfefdcdbfa
Sebastian Scherer <sebastian.scherer@onclusive.com>
2023-10-25 16:45:23 +0000

    making the entity-linking model available in module context

--- projects/entity-linking/serve/src/serve/__main__.py
+++ projects/entity-linking/serve/src/serve/__main__.py
@@ -6,9 +6,8 @@ from src.settings import get_settings
 
 
 settings = get_settings()
-
+model_server = get_model_server(settings=settings)
 
 if __name__ == "__main__":
     # launch server process(es)
-    model_server = get_model_server(settings=settings)
     model_server.serve()
