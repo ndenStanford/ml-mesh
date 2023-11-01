f2135741db26463d4bbb25a82ac9940cd97deba9
yuzhou.gao <yuzhou.gao@onclusive.com>
2023-11-01 19:18:57 +0000

    update setting

--- projects/topic-detection/serve/src/serve/_init.py
+++ projects/topic-detection/serve/src/serve/_init.py
@@ -1,16 +1,51 @@
 """Service initialization."""
 # isort: skip_file
 
+# Standard Library
+from typing import Optional
+
 # 3rd party libraries
 import requests
+from pydantic import BaseSettings
 
 # Internal libraries
 from onclusiveml.core.logging import get_default_logger
 
 # Source
-from src.serve.settings import Settings
+from src.serve.prompt_storage import Category_list, Prompt_dict
 
 logger = get_default_logger(__name__)
+
+
+class Settings(BaseSettings):
+    """API configuration."""
+
+    API_NAME: str = "Topic detection"
+    # API description
+    API_DESCRIPTION: str = ""
+    # API environment
+    ENVIRONMENT: str = "dev"
+    # Betterstack heartbeat key
+    BETTERSTACK_KEY: str = ""
+    # Debug level
+    DEBUG: bool = True
+    # API runtime
+    KUBERNETES_IN_POD: bool = False
+    # Logging level
+    LOGGING_LEVEL: str = "info"
+    # documentation endpoint
+    DOCS_URL: Optional[str] = "/topic-detection/docs"
+    OPENAPI_URL: Optional[str] = "/topic-detection/openapi.json"
+    # OpenAI api key
+    OPENAI_API_KEY: str = ""
+    # Prompt url
+    PROMPT_API: str = "http://prompt-backend:4000"
+    INTERNAL_ML_ENDPOINT_API_KEY: str = "1234"
+    # interested aspects/categories
+    CATEGORY_LIST = Category_list
+    # prompt for iteratively input; each time one category only
+    PROMPT_DICT = Prompt_dict
+
 
 settings = Settings()
 
