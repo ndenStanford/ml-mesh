45f37df42c26becde0153d9a0c268325467dc53c
jian.tong <jian.tong@onclusive.com>
2023-11-02 17:39:51 +0000

    remove redundant logic

--- projects/entity-linking/serve/src/serve/model.py
+++ projects/entity-linking/serve/src/serve/model.py
@@ -66,10 +66,7 @@ class EntityLinkingServedModel(ServedModel):
 
         text = re.sub("\n+", " ", text)
         lang = LanguageIso.from_language_iso(lang).value
-        if entities is None:
-            output = self._predict(text, lang, None)
-        else:
-            output = self._predict(text, lang, entities)
+        output = self._predict(text, lang, entities)
         return PredictResponseSchema.from_data(
             version=int(settings.api_version[1:]),
             namespace=settings.model_name,
