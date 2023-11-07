93b53662b242619d0cd9149c9842b23f7523893a
vishal <vishal.singh@onclusive.com>
2023-11-07 12:59:50 +0000

    removed depencency on env file fir unit tests

--- projects/topic/register/src/settings.py
+++ projects/topic/register/src/settings.py
@@ -6,16 +6,16 @@ from onclusiveml.data.feature_store import FeatureStoreParams
 class FeatureRegistrationParams(FeatureStoreParams):
     """Feature registration inputs."""
 
-    feast_config_bucket: str
-    config_file: str
-    local_config_dir: str
-    entity_name: str
-    entity_join_key: str
-    feature_view_name: str
-    redshift_database: str
-    redshift_schema: str
-    redshift_table: str
-    redshift_timestamp_field: str
+    feast_config_bucket: str = "kubeflow-feast-config-dev"
+    config_file: str = "feature_store.yaml"
+    local_config_dir: str = "local-config-dir"
+    entity_name: str = "topic"
+    entity_join_key: str = "iptc_id"
+    feature_view_name: str = "topic_feature_view"
+    redshift_database: str = "sources_dev"
+    redshift_schema: str = "feast"
+    redshift_table: str = "topic"
+    redshift_timestamp_field: str = "event_timestamp"
 
     class Config:
         env_file = "config/dev.env"
