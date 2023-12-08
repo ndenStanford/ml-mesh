5d535876812d9eae421d1a4e976c8c05a52b60fe
Sebastian Scherer <sebastian.scherer@onclusive.com>
2023-12-08 16:43:53 +0000

    updated nlp language detection logic and exception to work with class methods and be more verbose

--- libs/nlp/onclusiveml/nlp/language/lang_exception.py
+++ libs/nlp/onclusiveml/nlp/language/lang_exception.py
@@ -7,10 +7,16 @@ from onclusiveml.core.base.exception import OnclusiveException
 class LanguageFilterException(OnclusiveException):
     """Language filter exception."""
 
-    message_format = "The language, '{language}', is currently not supported."
+    message_format = (
+        "The language '{language_iso}', is currently not supported. Supported languages are: "
+        "{supported_language_isos}."
+    )
 
 
 class LanguageDetectionException(OnclusiveException):
     """Language detection exception."""
 
-    message_format = "The language, '{language}', cannot be found"
+    message_format = (
+        "The language reference '{language_iso_value}' could not be mapped. Supported references "
+        "are {supported_language_iso_values}. Supported languages are {supported_language_isos}."
+    )
