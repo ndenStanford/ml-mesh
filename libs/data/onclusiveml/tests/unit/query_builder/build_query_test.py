1f3e6b63f95806ea275d4c1bbf70efbfb0dedfda
Amaury de Guillebon <amaury.deguillebon@onclusive.com>
2023-12-14 19:35:32 +0000

    update on unit tests

--- libs/data/onclusiveml/tests/unit/query_builder/build_query_test.py
+++ libs/data/onclusiveml/tests/unit/query_builder/build_query_test.py
@@ -1,17 +1,7 @@
-"""Test."""
+"""Build query Test."""
 
 # 3rd party libraries
 import pytest  # noqa
-
-
-def test_el(test_el):
-    """Test EL API."""
-    assert test_el == "https://www.wikidata.org/wiki/Q312"
-
-
-def test_ner(test_ner):
-    """Test NER API."""
-    assert test_ner == ["Amazon", "AMZN"]
 
 
 def test_query_builder(test_query_builder, expected_query_builder):
@@ -25,13 +15,3 @@ def test_query_builder(test_query_builder, expected_query_builder):
     assert actual_test_el == expected_test_el
     assert actual_test_ner == expected_test_ner
     assert actual_test_regex == expected_test_regex
-
-
-def test_scoring(test_scoring):
-    """Test scoring."""
-    all_scores, texts, embeddings, cluster_labels = test_scoring
-    assert all_scores["silhouette"] > 0
-    assert all_scores["intra_cluster_similarity"] > 0
-    assert all_scores["w_penalization"] > 0
-    assert all_scores["homogeneity_score"] > 0
-    assert all_scores["relevance_score"] > 0
