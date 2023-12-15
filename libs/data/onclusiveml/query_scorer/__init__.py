d006e0732a2bd0020683133ac7dccca6d9b4faf5
jian.tong <jian.tong@onclusive.com>
2023-12-15 02:50:09 +0000

    query score

--- libs/data/onclusiveml/query_scorer/__init__.py
+++ libs/data/onclusiveml/query_scorer/__init__.py
@@ -10,4 +10,4 @@ from onclusiveml.query_builder.get_articles import (  # noqa: F401
 )
 from onclusiveml.query_builder.get_el import predict_entity_linking  # noqa: F401
 from onclusiveml.query_builder.get_ner import predict_ner  # noqa: F401
-from onclusiveml.query_builder.scoring import evaluate_query  # noqa: F401
+from onclusiveml.query_scorer.scoring import evaluate_query  # noqa: F401
--- libs/data/onclusiveml/query_scorer/settings.py
+++ libs/data/onclusiveml/query_scorer/settings.py
@@ -1,11 +1,10 @@
 """Settings."""
 
 # Standard Library
-from typing import Any, List
+from typing import Any
 
 # 3rd party libraries
 from elasticsearch import Elasticsearch
-from pydantic import Field, SecretStr
 
 # Internal libraries
 from onclusiveml.core.base import OnclusiveBaseSettings
@@ -40,62 +39,12 @@ class ScoringConfig(OnclusiveBaseSettings):
     penalty_scaling_power: float = 0.5
 
 
-class ApiSettings(OnclusiveBaseSettings):
-    """EL."""
-
-    api_key: SecretStr = Field(default="api_token", exclude=True)
-    predict_url: str
-
-
-class EntityLinkingProdSettings(ApiSettings):
-    """EL."""
-
-    predict_url = "https://internal.api.ml.prod.onclusive.com/entity-linking/v1/predict"
-
-    class Config:
-        env_prefix = "onclusiveml_data_entity_linking_prod_"
-
-
-class EntityLinkingStageSettings(ApiSettings):
-    """EL."""
-
-    predict_url = (
-        "https://internal.api.ml.stage.onclusive.com/entity-linking/v1/predict"
-    )
-
-    class Config:
-        env_prefix = "onclusiveml_data_entity_linking_stage_"
-
-
-class NERProdSettings(ApiSettings):
-    """NER Prod."""
-
-    predict_url = "https://internal.api.ml.prod.onclusive.com/ner/v1/predict"
-
-    class Config:
-        env_prefix = "onclusiveml_data_ner_prod_"
-
-
-class NERStageSettings(ApiSettings):
-    """NER Stage."""
-
-    predict_url = "https://internal.api.ml.stage.onclusive.com/ner/v1/predict"
-
-    class Config:
-        env_prefix = "onclusiveml_data_ner_stage_"
-
-
 class Settings(OnclusiveBaseSettings):
     """Settings."""
 
     es: Any
     clustering_config: ClusteringConfig
     scoring_config: ScoringConfig
-    entity_linking_prod: EntityLinkingProdSettings
-    entity_linking_stage: EntityLinkingStageSettings
-    NER_prod: NERProdSettings
-    NER_stage: NERStageSettings
-    es_index: List[str]
 
 
 def get_settings() -> Settings:
@@ -112,11 +61,6 @@ def get_settings() -> Settings:
     clustering_config = ClusteringConfig()
     scoring_config = ScoringConfig()
 
-    entity_linking_prod = EntityLinkingProdSettings()
-    entity_linking_stage = EntityLinkingStageSettings()
-    NER_prod = NERProdSettings()
-    NER_stage = NERStageSettings()
-
     es_index = ["crawler", "crawler-2023.11", "crawler-2023.10", "crawler-2023.09"]
 
     return Settings(
@@ -124,8 +68,4 @@ def get_settings() -> Settings:
         es_index=es_index,
         clustering_config=clustering_config,
         scoring_config=scoring_config,
-        entity_linking_prod=entity_linking_prod,
-        entity_linking_stage=entity_linking_stage,
-        NER_prod=NER_prod,
-        NER_stage=NER_stage,
     )
