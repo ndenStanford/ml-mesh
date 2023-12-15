cee16ad0810acd2012681e9a5c26c5d30290095c
jian.tong <jian.tong@onclusive.com>
2023-12-15 19:51:05 +0000

    add intergration test

--- libs/data/onclusiveml/query_scorer/__init__.py
+++ libs/data/onclusiveml/query_scorer/__init__.py
@@ -1,13 +1,8 @@
 """Init."""
 
-# 3rd party libraries
-from elasticsearch import Elasticsearch  # noqa
-
 # Internal libraries
 from onclusiveml.query_builder.build_query import build_query  # noqa: F401
-from onclusiveml.query_builder.get_articles import (  # noqa: F401
+from onclusiveml.query_scorer.get_articles import (  # noqa: F401
     get_query_results,
 )
-from onclusiveml.query_builder.get_el import predict_entity_linking  # noqa: F401
-from onclusiveml.query_builder.get_ner import predict_ner  # noqa: F401
 from onclusiveml.query_scorer.scoring import evaluate_query  # noqa: F401
--- libs/data/onclusiveml/tests/integration/query_scorer/conftest.py
+++ libs/data/onclusiveml/tests/integration/query_scorer/conftest.py
@@ -4,6 +4,7 @@
 import pytest  # noqa
 
 # Internal libraries
+from onclusiveml.query_builder import build_query
 from onclusiveml.query_scorer import evaluate_query as query_evaluation
 from onclusiveml.query_scorer.settings import get_settings
 
@@ -12,9 +13,19 @@ settings = get_settings()
 
 
 @pytest.fixture
+def query_test():
+    """Basic params."""
+    company: str = "Apple"
+    company_ticker: str = "AAPL"
+    keywords: List[str] = ["Iphone"]
+    return company, company_ticker, keywords
+
+
+@pytest.fixture
 def evaluate_query_test(build_query_test):
     """Get results."""
-    test_el, _, _ = build_query_test
+    company, company_ticker, keywords = query_test
+    test_el, _, _ = build_query(company, company_ticker, keywords, all_keywords=False)
     return test_el, settings.clustering_config, settings.scoring_config
 
 
