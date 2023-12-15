31d57b13eb139ae2402a8b9adc3a26342eece483
jian.tong <jian.tong@onclusive.com>
2023-12-15 18:15:29 +0000

    update lang to language

--- libs/data/onclusiveml/query_builder/__init__.py
+++ libs/data/onclusiveml/query_builder/__init__.py
@@ -1,8 +1,5 @@
 """Init."""
 
-# 3rd party libraries
-from elasticsearch import Elasticsearch  # noqa
-
 # Internal libraries
 from onclusiveml.query_builder.build_query import build_query  # noqa: F401
 from onclusiveml.query_builder.get_el import predict_entity_linking  # noqa: F401
