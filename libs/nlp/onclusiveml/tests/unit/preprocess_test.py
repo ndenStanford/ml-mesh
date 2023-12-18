3adb5cbc8345f7401e023f859810305e14566f96
manal el guerouani <manal.elguerouani@onclusive.com>
2023-12-18 17:26:10 +0000

    modify tests

--- libs/nlp/onclusiveml/tests/unit/preprocess_test.py
+++ libs/nlp/onclusiveml/tests/unit/preprocess_test.py
@@ -7,22 +7,13 @@ import pytest
 from onclusiveml.nlp import preprocess
 
 
-list_of_inputs_with_expected_output_for_html_removal = [
-    ("<p>This is <b>a sample</b> text.</p>", "This is a sample text."),
-    ("", ""),
-    ("This is a sample text.", "This is a sample text."),
-]
-
-list_of_inputs_with_expected_output_for_whitspace_removal = [
-    ("This    is   a   sample   text.", "This is a sample text."),
-    ("", ""),
-    ("This is a sample text.", "This is a sample text."),
-    (" This   is   a  sample  text.", " This is a sample text."),
-]
-
-
 @pytest.mark.parametrize(
-    "input_text, expected_output", list_of_inputs_with_expected_output_for_html_removal
+    "input_text, expected_output",
+    [
+        ("<p>This is <b>a sample</b> text.</p>", "This is a sample text."),
+        ("", ""),
+        ("This is a sample text.", "This is a sample text."),
+    ],
 )
 def test_remove_html(input_text, expected_output):
     """Test remove_html function with various inputs.
@@ -39,11 +30,17 @@ def test_remove_html(input_text, expected_output):
 
 
 @pytest.mark.parametrize(
-    "input_text, expected_output",
-    list_of_inputs_with_expected_output_for_whitspace_removal,
+    "input_text, input_regex, expected_output",
+    [
+        ("This    is   a   sample   text.", r"\s+", "This is a sample text."),
+        ("", r"\s+", ""),
+        ("This is a sample text.", r"\s+", "This is a sample text."),
+        ("    This   is   a  sample  text.", r"\s+", " This is a sample text."),
+        ("This___is___a___sample___text.", r"_+", "This is a sample text."),
+    ],
 )
-def test_remove_whitespace(input_text, expected_output):
-    """Test remove_whitespace function with various inputs.
+def test_remove_whitespace(input_text, input_regex, expected_output):
+    """Test remove_whitespace function with various inputs and regex.
 
     Args:
         input_text (str): Text with extra spaces
@@ -52,18 +49,5 @@ def test_remove_whitespace(input_text, expected_output):
     Returns:
         None
     """
-    res = preprocess.remove_whitespace(input_text)
-    assert res == expected_output
-
-
-def test_remove_whitespace_custom_regex():
-    """Test remove_whitespace function with a user-provided regex.
-
-    Returns:
-        None
-    """
-    input_text = "This___is___a___sample___text."
-    expected_output = "This is a sample text."
-    custom_regex = r"_+"
-    res = preprocess.remove_whitespace(input_text, regex=custom_regex)
+    res = preprocess.remove_whitespace(input_text, input_regex)
     assert res == expected_output
