ed566e610d4b88505c0b7fcfb8a9c575155dfbf9
zheyuan.hu <zheyuan.hu@onclusive.com>
2023-12-20 16:30:16 +0000

    rename "compiled" to "trained"

--- libs/models/onclusiveml/models/gch_summarization/trained_summarization.py
+++ libs/models/onclusiveml/models/gch_summarization/trained_summarization.py
@@ -1,4 +1,4 @@
-"""Compiled topic."""
+"""Trained Summarizaiton."""
 
 # Standard Library
 import os
@@ -24,50 +24,50 @@ class TrainedSummarization:
 
     def __init__(
         self,
-        compiled_summarization_pipeline_en: pipeline,
-        compiled_summarization_pipeline_frde: pipeline,
-        compiled_summarization_pipeline_es: pipeline,
-        compiled_summarization_pipeline_ca: pipeline,
-        compiled_summarization_pipeline_it: pipeline,
+        trained_summarization_pipeline_en: pipeline,
+        trained_summarization_pipeline_frde: pipeline,
+        trained_summarization_pipeline_es: pipeline,
+        trained_summarization_pipeline_ca: pipeline,
+        trained_summarization_pipeline_it: pipeline,
     ):
-        self.compiled_summarization_pipeline_en = compiled_summarization_pipeline_en
-        self.compiled_summarization_pipeline_frde = compiled_summarization_pipeline_frde
-        self.compiled_summarization_pipeline_es = compiled_summarization_pipeline_es
-        self.compiled_summarization_pipeline_ca = compiled_summarization_pipeline_ca
-        self.compiled_summarization_pipeline_it = compiled_summarization_pipeline_it
+        self.trained_summarization_pipeline_en = trained_summarization_pipeline_en
+        self.trained_summarization_pipeline_frde = trained_summarization_pipeline_frde
+        self.trained_summarization_pipeline_es = trained_summarization_pipeline_es
+        self.trained_summarization_pipeline_ca = trained_summarization_pipeline_ca
+        self.trained_summarization_pipeline_it = trained_summarization_pipeline_it
 
     @classmethod
     def from_pretrained(cls, directory: Union[Path, str]) -> "TrainedSummarization":
-        """Load compiled summarization object from specfied directory.
+        """Load trained summarization object from specfied directory.
 
         Args:
-            directory (Union[Path, str]): The directory path contained the pretrained compiled
-                sent pipeline
+            directory (Union[Path, str]): The directory path contained the pretrained trained
+                summatization pipeline
         Returns:
-            CompiledSummarization: The loaded pre-trained CompiledSummarization object
+            TrainedSummarization: The loaded pre-trained TrainedSummarization object
         """
-        compiled_summarization_pipeline_en = pipeline(
+        trained_summarization_pipeline_en = pipeline(
             task="summarization", model=os.path.join(directory, "english_summarization")
         )
-        compiled_summarization_pipeline_frde = pipeline(
+        trained_summarization_pipeline_frde = pipeline(
             task="summarization",
             model=os.path.join(directory, "french_german_summarization"),
         )
-        compiled_summarization_pipeline_es = pipeline(
+        trained_summarization_pipeline_es = pipeline(
             task="summarization", model=os.path.join(directory, "spanish_summarization")
         )
-        compiled_summarization_pipeline_ca = pipeline(
+        trained_summarization_pipeline_ca = pipeline(
             task="summarization", model=os.path.join(directory, "catalan_summarization")
         )
-        compiled_summarization_pipeline_it = pipeline(
+        trained_summarization_pipeline_it = pipeline(
             task="summarization", model=os.path.join(directory, "italian_summarization")
         )
         return cls(
-            compiled_summarization_pipeline_en=compiled_summarization_pipeline_en,
-            compiled_summarization_pipeline_frde=compiled_summarization_pipeline_frde,
-            compiled_summarization_pipeline_es=compiled_summarization_pipeline_es,
-            compiled_summarization_pipeline_ca=compiled_summarization_pipeline_ca,
-            compiled_summarization_pipeline_it=compiled_summarization_pipeline_it,
+            trained_summarization_pipeline_en=trained_summarization_pipeline_en,
+            trained_summarization_pipeline_frde=trained_summarization_pipeline_frde,
+            trained_summarization_pipeline_es=trained_summarization_pipeline_es,
+            trained_summarization_pipeline_ca=trained_summarization_pipeline_ca,
+            trained_summarization_pipeline_it=trained_summarization_pipeline_it,
         )
 
     def remove_html(self, text: str) -> str:
@@ -115,15 +115,15 @@ class TrainedSummarization:
             summary (str): summary
         """
         if language == "en":
-            pipeline = self.compiled_summarization_pipeline_en
+            pipeline = self.trained_summarization_pipeline_en
         elif language == "fr" or language == "de":
-            pipeline = self.compiled_summarization_pipeline_frde
+            pipeline = self.trained_summarization_pipeline_frde
         elif language == "es":
-            pipeline = self.compiled_summarization_pipeline_es
+            pipeline = self.trained_summarization_pipeline_es
         elif language == "ca":
-            pipeline = self.compiled_summarization_pipeline_ca
+            pipeline = self.trained_summarization_pipeline_ca
         elif language == "it":
-            pipeline = self.compiled_summarization_pipeline_it
+            pipeline = self.trained_summarization_pipeline_it
 
         output = pipeline(
             inputs,
