e7244f38d1824b0d253d0f225ef76f5a9f444c08
Rene-Jean Corneille <rene-jean.corneille@onclusive.com>
2024-01-31 23:42:56 +0000

    update test

--- projects/ner/backfill/tests/unit/backfill/pipeline_test.py
+++ projects/ner/backfill/tests/unit/backfill/pipeline_test.py
@@ -3,7 +3,6 @@
 # 3rd party libraries
 import pytest
 from apache_beam.testing.test_pipeline import TestPipeline
-from kafka.errors import NoBrokersAvailable
 
 # Source
 from src.backfill.pipeline import get_pipeline
@@ -12,7 +11,7 @@ from src.settings import get_settings
 
 # NOTE: until we have a reliable way to mock the kafka consumer
 # this is the behaviour consistency we can keep testing for.
-@pytest.mark.xfail(raises=NoBrokersAvailable)
+@pytest.mark.xfail(raises=RuntimeError)
 def test_pipeline():
     """Test enrichment pipeline."""
     with TestPipeline() as pipeline:
