37f729af17535f1371fcb49592470fb67f788edc
Amaury de Guillebon <amaury.deguillebon@onclusive.com>
2024-02-07 18:27:30 +0000

    update scoring

--- libs/data/onclusiveml/data/query_scorer/get_articles.py
+++ libs/data/onclusiveml/data/query_scorer/get_articles.py
@@ -27,3 +27,28 @@ def get_query_results(
         texts.append(results["hits"]["hits"][k]["_source"]["content"])
         list_scores.append(results["hits"]["hits"][k]["_score"])
     return (texts, list_scores)
+
+
+def remove_duplicates(doc_list: List[Optional[str]]) -> List[Optional[str]]:
+    """Returns duplicate documents from a list of documents.
+
+    Args:
+        doc_list (list): A list of documents.
+
+    Returns:
+        list: A list of unique documents.
+    """
+    processed_docs = [
+        doc.lower().strip() if doc is not None else None for doc in doc_list
+    ]
+
+    unique_docs = set()
+    duplicate_count = 0
+
+    for doc in processed_docs:
+        if doc in unique_docs:
+            duplicate_count += 1
+        else:
+            unique_docs.add(doc)
+
+    return list(unique_docs)
