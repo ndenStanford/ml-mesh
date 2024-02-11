4ba7b8941cc42a3990e89dfaf8af285f1a69b4eb
jian.tong <jian.tong@onclusive.com>
2024-02-11 21:46:18 +0000

    update folder name

--- projects/iptc/train/run_projects_in_environments.sh
+++ projects/iptc/train/run_projects_in_environments.sh
@@ -4,5 +4,5 @@
 declare -a environments=("IPTC-00" "IPTC-02" "IPTC-03")
 
 for env in "${environments[@]}"; do
-    make projects.start/"${PROJECT_NAME}" COMPONENT=train ENV_FILE="$env"
+    make projects.start/"${PROJECT_NAME}" COMPONENT=train MODEL_ID="$env"
 done
