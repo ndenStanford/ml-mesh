5ee99a89795b1c630c79472737d879e841dd5de4
jian.tong <jian.tong@onclusive.com>
2024-02-11 21:43:17 +0000

    update naming convention

--- projects/iptc/train/run_projects_in_environments.sh
+++ projects/iptc/train/run_projects_in_environments.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define your environments
-declare -a environments=("IPTC-00000" "IPTC-10000" "IPTC-20000")
+declare -a environments=("IPTC-00" "IPTC-02" "IPTC-03")
 
 for env in "${environments[@]}"; do
     make projects.start/"${PROJECT_NAME}" COMPONENT=train ENV_FILE="$env"
