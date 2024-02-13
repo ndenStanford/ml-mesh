4918cec50510733cbe9a3c7da65d86f39238a80c
jian.tong <jian.tong@onclusive.com>
2024-02-13 23:32:44 +0000

    update ci

--- projects/iptc/train/run_projects_in_environments.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Define your environments
-declare -a environments=("IPTC-00" "IPTC-02" "IPTC-03")
-
-for env in "${environments[@]}"; do
-    make projects.start/"${PROJECT_NAME}" COMPONENT=train MODEL_ID="$env"
-done
