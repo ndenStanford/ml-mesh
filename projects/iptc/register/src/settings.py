2f808d912509ac9f2358dca1c108766fe093b7e2
vishal.singh <vishal.singh@gmail.com>
2024-02-14 14:31:06 +0000

    Register three levels of IPTC features

--- projects/iptc/register/src/settings.py
+++ projects/iptc/register/src/settings.py
@@ -1,7 +1,10 @@
 """Feature registration inputs."""
 
 # Standard Library
-from typing import List, Tuple
+from typing import List, Optional, Tuple
+
+# 3rd party libraries
+from pydantic import BaseSettings
 
 # Internal libraries
 from onclusiveml.data.feature_store import FeatureStoreParams
@@ -11,18 +14,56 @@ class FeatureRegistrationParams(FeatureStoreParams):
     """Feature registration inputs."""
 
     feast_config_bucket: str
-    config_file: str
-    local_config_dir: str
-    entity_name: str
-    entity_join_key: str
-    feature_view_name: str
     redshift_database: str
-    redshift_schema: str
-    redshift_table: str
-    redshift_timestamp_field: str
-    fields: List[Tuple[str, str]]
+    redshift_table: Optional[str]
+    entity_name: Optional[str]
+    feature_view_name: Optional[str]
+    fields: Optional[List[Tuple[str, str]]]
+    entity_join_key: str
     register_features: bool = False
 
     class Config:
+        env_file = "config/dev.env"
+        env_file_encoding = "utf-8"
+
+
+class IptcFirstLevelFeatureRegistrationParams(BaseSettings):
+    """Feature registration inputs."""
+
+    entity_name: str
+    feature_view_name: str
+    redshift_table: str
+    fields: List[Tuple[str, str]]
+
+    class Config:
+        env_prefix = "first_level_"
+        env_file = "config/dev.env"
+        env_file_encoding = "utf-8"
+
+
+class IptcSecondLevelFeatureRegistrationParams(BaseSettings):
+    """Feature registration inputs."""
+
+    entity_name: str
+    feature_view_name: str
+    redshift_table: str
+    fields: List[Tuple[str, str]]
+
+    class Config:
+        env_prefix = "second_level_"
+        env_file = "config/dev.env"
+        env_file_encoding = "utf-8"
+
+
+class IptcThirdLevelFeatureRegistrationParams(BaseSettings):
+    """Feature registration inputs."""
+
+    entity_name: str
+    feature_view_name: str
+    redshift_table: str
+    fields: List[Tuple[str, str]]
+
+    class Config:
+        env_prefix = "third_level_"
         env_file = "config/dev.env"
         env_file_encoding = "utf-8"
