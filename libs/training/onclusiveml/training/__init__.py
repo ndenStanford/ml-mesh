92418aa85b7d97685c22d891e786673d2e8f521d
Nutch <nutch.den@onclusive.com>
2024-02-21 19:17:28 +0000

    pre commit cleanup

--- libs/training/onclusiveml/training/__init__.py
+++ libs/training/onclusiveml/training/__init__.py
@@ -2,7 +2,9 @@
 from onclusiveml.training.onclusive_model_trainer import (  # noqa: F401
     OnclusiveModelTrainer,
 )
-from libs.training.onclusiveml.training.bertopic.trainer import BertopicTrainer  # noqa: F401
+from libs.training.onclusiveml.training.bertopic.trainer import (  # noqa: F401
+    BertopicTrainer,
+)
 from onclusiveml.training.huggingface.trainer import (  # noqa: F401
     OnclusiveHuggingfaceModelTrainer,
 )
