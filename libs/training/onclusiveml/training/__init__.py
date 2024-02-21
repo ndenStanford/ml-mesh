c2c500884a5df4722a3dbfdbe91e21290377760b
ndenStanford <nden@alumni.stanford.edu>
2024-02-22 01:55:04 +0700

    refactoring code

--- libs/training/onclusiveml/training/__init__.py
+++ libs/training/onclusiveml/training/__init__.py
@@ -2,7 +2,7 @@
 from onclusiveml.training.onclusive_model_trainer import (  # noqa: F401
     OnclusiveModelTrainer,
 )
-from onclusiveml.training.bertopic_trainer import BertopicTrainer  # noqa: F401
+from libs.training.onclusiveml.training.bertopic.trainer import BertopicTrainer  # noqa: F401
 from onclusiveml.training.huggingface.trainer import (  # noqa: F401
     OnclusiveHuggingfaceModelTrainer,
 )
