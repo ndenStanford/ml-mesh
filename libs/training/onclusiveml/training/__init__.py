2d2202aa6bf508bf324f03ec23fcf9f186a39839
Nutch <nutch.den@onclusive.com>
2024-02-22 13:48:15 +0000

    update init

--- libs/training/onclusiveml/training/__init__.py
+++ libs/training/onclusiveml/training/__init__.py
@@ -2,7 +2,7 @@
 from onclusiveml.training.onclusive_model_trainer import (  # noqa: F401
     OnclusiveModelTrainer,
 )
-from libs.training.onclusiveml.training.bertopic.trainer import (  # noqa: F401
+from onclusiveml.training.bertopic.trainer import (  # noqa: F401
     BertopicTrainer,
 )
 from onclusiveml.training.huggingface.trainer import (  # noqa: F401
