e3fef14ed1c697ec08b1f312631284875a835a02
ec2-user <amaury.deguillebon@onclusive.com>
2024-03-13 19:08:22 +0000

    save up

---
+++ projects/evaluator/train/src/settings.py
@@ -0,0 +1,65 @@
+"""Settings."""
+
+# Standard Library
+import os
+from typing import List
+
+# Internal libraries
+from onclusiveml.tracking import (
+    TrackedModelCard,
+    TrackedModelSpecs,
+    TrackedParams,
+)
+
+
+# Settings classes
+class TrackedDocumentEvaluatorSpecs(TrackedModelSpecs):
+    """Params class for specifying the neptune project and model suite."""
+
+    project: str = "onclusive/document-evaluator"
+    model: str = "DOCUMENT-EVALUATOR"
+
+    class Config:
+        env_file = "config/dev.env"
+        env_file_encoding = "utf-8"
+
+
+class DocumentEvaluatorParams(TrackedParams):
+    """Ground truth specification for document evaluator model."""
+
+    # LightGBM parameters
+    lgb_params: dict = {"objective": "binary", "verbose": -1, "is_unbalance": True}
+
+    numerical_cols: List[str] = ["pagerank", "reach", "score"]
+    categorical_cols: List[str] = [
+        "lang",
+        "media_type",
+        "label",
+        "publication",
+        "country",
+        "is_copyrighted",
+        "type_of_summary",
+    ]
+    # File paths
+    data_file_path: str = "data/processed_data.csv"
+    config_file_path: str = "config/dev.env"
+    # Training settings
+    test_size: float = 0.2
+    random_state: int = 7
+    num_boost_rounds: int = 10000
+
+
+class TrackedDocumentEvaluatorModelCard(TrackedModelCard):
+    """The model card for the document evaluator ML project."""
+
+    model_type: str = "trained"
+    # --- custom fields
+    # model params
+    model_params: DocumentEvaluatorParams = DocumentEvaluatorParams()
+    # admin
+    local_output_dir: str = os.path.join(".", "document_evaluator_artifacts")
+    logging_level: str = "INFO"
+
+    class Config:
+        env_file = "config/dev.env"
+        env_file_encoding = "utf-8"
---
+++ projects/evaluator/train/src/train_model.py
@@ -0,0 +1,61 @@
+"""Register trained model."""
+
+# Standard Library
+import os
+
+# 3rd party libraries
+import lightgbm as lgb
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OrdinalEncoder
+
+# Source
+from src.settings import DocumentEvaluatorParams
+
+
+def main() -> None:
+    """Train and evaluate the model."""
+    current_directory = os.getcwd()
+    print("Current directory:", current_directory)
+    params = DocumentEvaluatorParams()
+
+    data_file_path = params.data_file_path
+    df = pd.read_csv(data_file_path).drop(columns=["Unnamed: 0"])
+
+    df["y"] = 1
+    df.loc[
+        df["message_type"] == "onclusive.delivery.event.content.validation.accepted",
+        "y",
+    ] = 1
+    df.loc[
+        df["message_type"] == "onclusive.delivery.event.content.validation.rejected",
+        "y",
+    ] = 0
+
+    numerical_cols = params.numerical_cols
+    categorical_cols = params.categorical_cols
+    X = df[numerical_cols + categorical_cols]
+    y = df[["y"]]
+    # Encode categorical features
+    enc = OrdinalEncoder()
+    enc.fit(X[categorical_cols])
+    X[categorical_cols] = enc.transform(X[categorical_cols])
+
+    test_size = params.test_size
+    random_state = params.random_state
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_size, random_state=random_state
+    )
+
+    d_train = lgb.Dataset(X_train, label=y_train, categorical_feature=categorical_cols)
+    d_test = lgb.Dataset(X_test, label=y_test, categorical_feature=categorical_cols)
+
+    lgb_params = params.lgb_params
+    num_boost_rounds = params.num_boost_rounds
+
+    model = lgb.train(lgb_params, d_train, num_boost_rounds, valid_sets=[d_test])
+    model.save_model("data/model")
+
+
+if __name__ == "__main__":
+    main()
