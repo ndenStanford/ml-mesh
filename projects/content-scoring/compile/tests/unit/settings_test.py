57b8e15e219f2183ee20eb3dbfad5bd2fb536f83
ec2-user <amaury.deguillebon@onclusive.com>
2024-03-20 16:33:19 +0000

    remove compile

--- projects/content-scoring/compile/tests/unit/settings_test.py
+++
@@ -1,9 +0,0 @@
-"""Settings tests."""
-
-# Source
-from src.settings import CompiledTrackedModelSpecs
-
-
-def test_compiled_tracked_model_specs():
-    """Test compiled tracked model specs."""
-    CompiledTrackedModelSpecs()
