05ee994b6faae75d4431c6ebf52b0882c1e64c5f
jian.tong <jian.tong@onclusive.com>
2024-03-29 18:14:20 +0000

    scale to all models

--- libs/models/onclusiveml/models/iptc/test_samples.py
+++ libs/models/onclusiveml/models/iptc/test_samples.py
@@ -1,20 +1,214 @@
 """Test samples."""
 TEST_SAMPLES = {
     "00000000": (
-        """Stocks reversed earlier losses to close higher despite rising oil prices
-                that followed the attack by Hamas on Israel over the weekend. Dovish comments by
-                Federal Reserve officials boosted the three major indexes. The Dow Jones Industrial
-                Average added nearly 200 points.""",
-        {"label": "economy, business and finance"},
+        """Music Listens: 28 Styles: Breaks Duration: 4:37 Size: 8.4Mb
+        Publication: today 16:21 Please, sign up or sign in to post comments.""",
+        {"label": "arts, culture, entertainment and media"},
+    ),
+    "01000000": (
+        """Music Listens: 1 Styles: Nu metal Duration: 2:09 Size: 2Mb
+        Publication: today 15:33 Please, sign up or sign in to post comments.""",
+        {"label": "arts and entertainment"},
     ),
     "02000000": (
-        """Depuis plusieurs années, les tribunaux sont sous les feux de la rampe dans leur traitement
-                des crimes à caractère sexuel, alors que plusieurs causes…""",
-        {"label": "judiciary"},
+        """San Juan County Sheriff's Office recently completed a rigorous
+        accreditation program that certifies they are operating under best practices.""",
+        {"label": "law enforcement"},
+    ),
+    "03000000": (
+        """Four people were seriously injured after a collision involving an SUV
+        and a car occurred on a highway south of Edmonton on Sunday night.""",
+        {"label": "accident and emergency incident"},
     ),
     "04000000": (
-        """Liberal Party members met with transport and freight stakeholders in Narrogin this month
-        to share their concern over the future of infrastructure project funding in the region.""",
+        """Liberal Party members met with transport and freight stakeholders in
+        Narrogin to share their concern over the future of infrastructure funding.""",
         {"label": "economic sector"},
+    ),
+    "05000000": (
+        """Redcar and Cleveland College has been named AIM Empowering Futures’
+        Access to Higher Education Centre of the Year.""",
+        {"label": "school"},
+    ),
+    "06000000": (
+        """Rising sea levels threaten to swamp the Maldives and the Indian Ocean
+        archipelago is already out of drinking water.""",
+        {"label": "natural resources"},
+    ),
+    "07000000": (
+        """The disease appears to be similar to kennel cough, spreading easily
+        in high-volume areas like boarding and doggie day care facilities.""",
+        {"label": "disease and condition"},
+    ),
+    "09000000": (
+        """If you are self-employed, you may be able to set up a tax-advantaged
+        solo 401(k) retirement savings plan.""",
+        {"label": "retirement"},
+    ),
+    "10000000": (
+        """Rabat, Morocco: Voli Low Cost. Cheap flight deals. Search and compare
+        low cost flights and tickets. Worldwide destinations.""",
+        {"label": "leisure"},
+    ),
+    "11000000": (
+        """比例代表制の長所は小政党でも国会に代表を送ることができ、
+        死票が少なくなる。短所は小党分立になりやすく、有権者と候補者の距離が遠くなる""",
+        {"label": "election"},
+    ),
+    "12000000": (
+        """In that day a fountain shall be opened for the house of David and
+        for the inhabitants of Jerusalem, for sin and for uncleanness.""",
+        {"label": "belief systems"},
+    ),
+    "13000000": (
+        """Sensory neurons can vary greatly in length. They can be less than a
+        millimeter long in small invertebrates, while in humans, they can be up to a meter long.""",
+        {"label": "natural science"},
+    ),
+    "14000000": (
+        """The related rates for women is actually cuatro. More dramatic than the
+        pattern toward afterwards marriage and parenthood.""",
+        {"label": "family"},
+    ),
+    "15000000": (
+        """Just like every other contact sport, sumo wrestling is not without its
+        risks. Here are some massive dangers faced by sumo wrestlers.""",
+        {"label": "competition discipline"},
+    ),
+    "16000000": (
+        """Yemen's Houthi rebel group on Sunday announced its intention to target
+        ships owned or operated by Israeli companies.""",
+        {"label": "armed conflict"},
+    ),
+    "17000000": (
+        """Tuesday is expected to be very similar to Monday. It will be a cold
+        morning with sunny skies and modest winds and a cool afternoon.""",
+        {"label": "weather forecast"},
+    ),
+    # Second level models
+    "20000002": (
+        """This show has singing, dancing, drama - all the ingredients of a super
+        musical. There's only one catch - this is not a musical!""",
+        {"label": "theatre"},
+    ),
+    "20000038": (
+        """Historical monuments will be protected by donating them to private
+        groups, says Minister.""",
+        {"label": "monument and heritage site"},
+    ),
+    "20000045": (
+        """We are 94.3 NOW!radio, where our on-air hosts and our listeners are
+        at the very center of everything. Call or Text us.""",
+        {"label": "radio"},
+    ),
+    "20000053": (
+        """Tens of thousands of supporters from Pakistan’s main religious
+        political party rallied in Lahore against Israel’s bombing of Palestinians.""",
+        {"label": "bombings"},
+    ),
+    "20000065": (
+        """Thousands across the UK took part in more than 100 local ‘Day of
+        Action’ rallies. TRT World spoke to pro-Palestinian protestors.""",
+        {"label": "demonstration"},
+    ),
+    "20000082": (
+        """A jewelry shop employee uses a fork spear to fight off three robbers,
+        and two were arrested.""",
+        {"label": "theft"},
+    ),
+    "20000106": (
+        """With most litigation being resolved before trial, the importance of
+        high-quality expert written evidence cannot be overstated.""",
+        {"label": "court"},
+    ),
+    "20000121": (
+        """The SEC reviews non-disclosure agreements to ensure whistleblowers
+        are not restricted from communicating with the agency.""",
+        {"label": "civil law"},
+    ),
+    "20000129": (
+        """One man is in jail after he threatened two people with a knife,
+        prompting a SWAT standoff at a Coeur d'Alene apartment.""",
+        {"label": "punishment (criminal)"},
+    ),
+    "20000170": (
+        """Riot Platforms (NASDAQ: RIOT) November weekly 11.50 straddle is
+        priced for a move of 15% into the expected release of quarter results.""",
+        {"label": "business finance"},
+    ),
+    "20000209": (
+        """A heating expert has shared myths that could be adding to your
+        British Gas, OVO, EDF energy bills.""",
+        {"label": "energy and resource"},
+    ),
+    "20000385": (
+        """Shift4 Payments (NYSE: FOUR) has filed for up to 3,510,362 share
+        offering by selling stockholders.""",
+        {"label": "securities"},
+    ),
+    "20000446": (
+        """Hay fever is not an autoimmune disorder. It is an allergic reaction
+        to allergens such as pollen, dust mites, or pet dander.""",
+        {"label": "illness"},
+    ),
+    "20000538": (
+        """Throughout the years, Call of Duty has been exceptional at times.
+        This is IGN's Top 10 Call of Duty Games.""",
+        {"label": "game"},
+    ),
+    "20000565": (
+        """Exploring the world of artisanal coffee, enthusiasts turn to locally
+        sourced beans and sustainable brewing techniques.""",
+        {"label": "Food and Drink"},
+    ),
+    "20000574": (
+        """The government has introduced a requirement for voters to show photo
+        ID when voting at a polling station.""",
+        {"label": "voting"},
+    ),
+    "20000587": (
+        """Recent protests spotlight the urgent need for addressing police
+        brutality. Activists demand reforms to ensure accountability.""",
+        {"label": "Human Rights"},
+    ),
+    "20000593": (
+        """The next meeting of one nation and one poll committee is set to
+        take place on October 25, according to sources.""",
+        {"label": "legislative body"},
+    ),
+    "20000621": (
+        """The US condemns North Korea's missile launch, stating it destabilizes
+        regional security and violates multiple UN Security Council resolutions.""",
+        {"label": "nuclear policy"},
+    ),
+    "20000638": (
+        """National leaders of China and Japan met in San Francisco, discussing
+        the 45th anniversary of the peace and friendship treaty between their nations.""",
+        {"label": "diplomacy"},
+    ),
+    "20000649": (
+        """The Governance and Democratic Participation Programme aims to provide
+        a record of achievements and lessons learned over its duration.""",
+        {"label": "political system"},
+    ),
+    "20000717": (
+        """Astronomers estimate there are about 200 billion trillion stars in the
+        observable universe. Why isn’t space filled with light?""",
+        {"label": "astronomy"},
+    ),
+    "20000735": (
+        """The process for enrolling patients in clinical trials is undergoing
+        significant change, expanding beyond traditional methods.""",
+        {"label": "medical research"},
+    ),
+    "20000742": (
+        """Child psychologists have their own set of goals for their kids,
+        prioritizing psychological well-being over material possessions.""",
+        {"label": "psychology"},
+    ),
+    "20000756": (
+        """SpaceX launched a Falcon Heavy rocket carrying an X-37B military space
+        plane used by the U.S. Space Force from Kennedy Space Center.""",
+        {"label": "aerospace engineering"},
     ),
 }
