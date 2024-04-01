dd51b0f9421438691db275d820189817282fa92e
jian.tong <jian.tong@onclusive.com>
2024-04-01 17:08:12 +0000

    update more models

--- libs/models/onclusiveml/models/iptc/test_samples.py
+++ libs/models/onclusiveml/models/iptc/test_samples.py
@@ -131,14 +131,21 @@ TEST_SAMPLES = {
         prompting a SWAT standoff at a Coeur d'Alene apartment.""",
         {"label": "punishment (criminal)"},
     ),
+    "20000148": (
+        """中国地震台网正式测定：10月28日11时44分在美国阿拉斯加州
+        （北纬59.05度，西经136.40度）发生5.4级地震，震源深度10千米。""",
+        {"label": "natural disasters"},
+    ),
     "20000170": (
         """Riot Platforms (NASDAQ: RIOT) November weekly 11.50 straddle is
-        priced for a move of 15% into the expected release of quarter results.""",
+        priced for a move of 15% into the expected release of quarter results
+        today after the bell.""",
         {"label": "business finance"},
     ),
     "20000209": (
-        """A heating expert has shared myths that could be adding to your
-        British Gas, OVO, EDF energy bills.""",
+        """A heating expert has shared the myths that could be adding to your
+        British Gas, OVO, EDF, E. • UK • One News Page[UK]: Saturday,
+        18 November 2023""",
         {"label": "energy and resource"},
     ),
     "20000385": (
@@ -146,69 +153,152 @@ TEST_SAMPLES = {
         offering by selling stockholders.""",
         {"label": "securities"},
     ),
+    "20000430": (
+        """[새한일보=신화통신] 황허(黃河) 샤오랑디(小浪底)댐 출수구(出水口)를 28일 드론으로
+        내려다봤다. 초가을 허난(河南)성 황허습지 국가급자연보호구 지위안(濟源)
+        구간은 맑은 물과 우거진 초목으로 그림같이 아름다운 풍경을 자아낸다. 2023.8.28""",
+        {"label": "water"},
+    ),
+    "20000441": (
+        """The habitat is the physical environment in which a species lives and to
+        which it is adapted. A niche refers to the role of a species in its ecosystem.
+        So a habitat is the actual place where an organism/species lives, and a niche
+        is its role in that habitat.""",
+        {"label": "ecosystem"},
+    ),
     "20000446": (
-        """Hay fever is not an autoimmune disorder. It is an allergic reaction
-        to allergens such as pollen, dust mites, or pet dander.""",
+        """Flexi Says: No, hay fever is not an autoimmune disorder. It is an allergic
+        reaction to allergens such as pollen, dust mites, or pet dander""",
         {"label": "illness"},
     ),
+    "20000464": (
+        """新型コロナワクチンを接種後に亡くなった兵庫県内の6人がワクチンとの因果関係を否定できない
+        として、厚労...""",
+        {"label": "preventative medicine"},
+    ),
     "20000538": (
-        """Throughout the years, Call of Duty has been exceptional at times.
-        This is IGN's Top 10 Call of Duty Games.""",
+        """Throughout the years, Call of Duty has been exceptional at times. From the
+        bombastic highs of Call of Duty 4: Modern Warfare, to the intense stories of
+        the Black Ops trilogy, and more, this is IGN's Top 10 Call of Duty Games.""",
         {"label": "game"},
     ),
     "20000565": (
-        """Exploring the world of artisanal coffee, enthusiasts turn to locally
-        sourced beans and sustainable brewing techniques.""",
+        """Exploring the world of artisanal coffee, enthusiasts are turning to locally
+        sourced beans and sustainable brewing techniques. This movement not only
+        highlights unique flavors but also supports small-scale farmers and promotes
+        environmental responsibility.""",
         {"label": "Food and Drink"},
     ),
     "20000574": (
-        """The government has introduced a requirement for voters to show photo
-        ID when voting at a polling station.""",
+        """The government has introduced a requirement for voters to show photo ID when
+        voting at a polling station at some elections. Click here for more information.""",
         {"label": "voting"},
     ),
     "20000587": (
-        """Recent protests spotlight the urgent need for addressing police
-        brutality. Activists demand reforms to ensure accountability.""",
+        """Recent protests spotlight the urgent need for addressing police brutality.
+        Activists demand reforms to ensure accountability and protect citizens' rights,
+        emphasizing the importance of dialogue and legal changes to foster a more just
+        society..""",
         {"label": "Human Rights"},
     ),
     "20000593": (
-        """The next meeting of one nation and one poll committee is set to
-        take place on October 25, according to sources.""",
+        """The next meeting of one nation and one poll committee is set to take place
+        on October 25, according to sources.""",
         {"label": "legislative body"},
     ),
     "20000621": (
-        """The US condemns North Korea's missile launch, stating it destabilizes
-        regional security and violates multiple UN Security Council resolutions.""",
+        """미국은 17일(현지시간) 북한이 동해상으로 단거리 탄도미사일을 발사해 역내 안보를 불안정하게
+        만들었다고 규탄했다. 미국 국무부 대변인은 이날 연합뉴스의 입장 문의에 보낸 답변에서
+        "미국은 북한의 12월 17일 탄도미사일 발사를 규탄한다"고 밝혔다. 대변인은 "이번 발사는
+        올해 북한의 다른 탄도미사일 발사들과 같이 유엔 안전보장이사회 결의 다수를 위반했다"면서
+        "북한의 이웃에 위협이 되며 역내 안보를 저해한다"고 지적했다.""",
         {"label": "nuclear policy"},
     ),
     "20000638": (
-        """National leaders of China and Japan met in San Francisco, discussing
-        the 45th anniversary of the peace and friendship treaty between their nations.""",
+        """<匯港通訊> 國家主席習近平在美國三藩市與日本首相岸田文雄會面。央視報道,習近平指出,今年是中日和平友好條約縍結
+        45周年,條約以法律形式確立了中日和平,友好合作方向。 習近平又指,兩國關係雖然經過風雨,但總體保持發展勢頭。""",
         {"label": "diplomacy"},
     ),
     "20000649": (
-        """The Governance and Democratic Participation Programme aims to provide
-        a record of achievements and lessons learned over its duration.""",
+        """The Governance and Democratic Participation Programme (GDPP) Legacy document aims
+        to provide a record of the achievements, challenges and lessons learned by the multi-year
+        democratic governance programme (June 2017- December 2022)""",
         {"label": "political system"},
     ),
+    "20000657": (
+        """奈良市の東大寺で7日、本尊の国宝・盧舎那仏（大仏）のほこりを払う「お身拭い」があった。夏の恒例行事。
+        僧侶や信者ら約170人が参加。大仏の魂を抜く法要や読経の後、ほこりを払い落とす。大仏の高さは約15メートル。天井からつるしたゴンドラを使用。""",
+        {"label": "Buddhism"},
+    ),
+    "20000697": (
+        """गणेश चतुर्थी के अवसर पर हम आपको बता रहे हैं उस मंदिर के बारे में जहां आज भी भगवान
+        गणेश की सिर कटी मूर्ति की पूजा होती है। यह मंदिर है केदारनाथ यात्रा मार्ग पर स्थित
+        मुनकटिया गणेश मंदिर। इस मंदिर में आज भी भगवान गणेश की सिर कटी मूर्ति विराजमान है।""",
+        {"label": "temple"},
+    ),
+    "20000705": (
+        """People who claim hadiths are fake & came 200 years after the Prophet ﷺ -
+        assim al hakeem""",
+        {"label": "Qur'an"},
+    ),
     "20000717": (
-        """Astronomers estimate there are about 200 billion trillion stars in the
-        observable universe. Why isn’t space filled with light?""",
+        """Astronomers estimate that there are about 200 billion trillion stars in
+        the observable universe. And many of those stars are as bright or even brighter
+        than our sun. So, why isn’t space filled with dazzling light?""",
         {"label": "astronomy"},
     ),
     "20000735": (
         """The process for enrolling patients in clinical trials is undergoing
-        significant change, expanding beyond traditional methods.""",
+        significant change. With the rise in digital technology, recruitment methods
+        have greatly expanded beyond traditional physician referrals to include online
+        platforms and social media channels. """,
         {"label": "medical research"},
     ),
     "20000742": (
-        """Child psychologists have their own set of goals for their kids,
-        prioritizing psychological well-being over material possessions.""",
+        """Parents have a lot of hopes and dreams for their children's futures —
+        and child psychologists have their own set of goals for their kids. Child
+        psychology experts aren't prioritizing material possessions or high-status jobs.""",
         {"label": "psychology"},
     ),
     "20000756": (
         """SpaceX launched a Falcon Heavy rocket carrying an X-37B military space
-        plane used by the U.S. Space Force from Kennedy Space Center.""",
+        plane used by the U.S. Space Force from Kennedy Space Center in Florida on
+        Thursday. The launch was delayed for weeks due to technical issues.""",
         {"label": "aerospace engineering"},
+    ),
+    "20000775": (
+        """Within this still turbulent market, buying a car can be even more
+        challenging for minority consumers due to discriminatory practices that have
+        plagued the car industry historically and continue to affect buyers.""",
+        {"label": "racism"},
+    ),
+    "20000780": (
+        """Les jeunes de moins de 25 ans ont de plus en plus envie de se dire oui
+        pour la vie. Les professionnels du mariage ont relevé une augmentation de
+        4% des célébrations chez les jeunes de moins de 25 ans.""",
+        {"label": "marriage"},
+    ),
+    "20000799": (
+        """Definition: (adjective) Dirty and wretched, as from poverty or lack of
+        care. Synonyms: flyblown, sordid. Usage: They lived like beasts in great squalid
+        labor-ghettos, festering in misery and degradation.""",
+        {"label": "poverty"},
+    ),
+    "20000802": (
+        """Explore the science behind addiction, its causes, and the treatment options
+        available for various addictions, including gambling, and drugs.""",
+        {"label": "addiction"},
+    ),
+    "20000808": (
+        """18HornyBoys has all the hottest and best twinks, who are horny and naked
+        performing sucking and fucking. We have all the best amateur and professional
+        videos, captured from the best tube sites on the internet. If you have
+        recommendations, please email us.""",
+        {"label": "pornography"},
+    ),
+    "20000822": (
+        """ウマニティは、中央競馬と地方競馬の出走馬の最新情報や血統分析、調教診断、騎手の乗り替わり情報、
+        馬場のコンディション速報、重賞レースのデータ分析などの最新ニュースを配信しています。""",
+        {"label": "horse racing"},
     ),
 }
