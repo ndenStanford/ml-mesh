a7d0a61e430925c02e182c951c04f7e8840d19a5
jian.tong <jian.tong@onclusive.com>
2024-04-03 04:32:54 +0000

    fix format

--- projects/iptc/compile/src/test_compiled_model/compiled_model_test.py
+++ projects/iptc/compile/src/test_compiled_model/compiled_model_test.py
@@ -28,7 +28,7 @@ def test_compiled_model_regression(  # type: ignore[no-untyped-def]
     total_sample_size = len(test_files["inputs"])
     model_specs = UncompiledTrackedModelSpecs()
     model_id = extract_model_id(model_specs.project)
-    class_dict_dict = class_dict(id_to_topic(model_id))
+    class_dict_dict = class_dict[id_to_topic[model_id]]
     for test_sample_index in range(total_sample_size):
 
         compiled_pred = compiled_iptc(test_files["inputs"][test_sample_index])
