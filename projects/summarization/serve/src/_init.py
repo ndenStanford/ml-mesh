e9906aaba4d496452aa4288300711306816f5d36
Rene-Jean Corneille <rene-jean.corneille@onclusive.com>
2024-04-30 12:30:02 +0000

    use prompt backend v2

--- projects/summarization/serve/src/_init.py
+++ projects/summarization/serve/src/_init.py
@@ -16,19 +16,3 @@ logger = get_default_logger(__name__)
 def init() -> None:
     """App initialization."""
     logger.info("Setting up prompts...")
-    _setup_prompts()
-
-
-def _setup_prompts() -> None:
-    """Setup prompts."""
-    headers = {"x-api-key": settings.INTERNAL_ML_ENDPOINT_API_KEY}
-
-    for input_lang, target_lang_dict in settings.PROMPT_DICT.items():
-        for target_lang, prompt_dict in target_lang_dict.items():
-            alias = prompt_dict["alias"]
-            template = prompt_dict["template"]
-            requests.post(
-                f"{settings.PROMPT_API}/api/v1/prompts?template={template}&alias={alias}",  # noqa: E501
-                headers=headers,
-            )
-    return
