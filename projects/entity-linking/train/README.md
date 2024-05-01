8f0e4169ba6c1f759635c5c1fde0f9e36642d763
Nutch <nutch.den@onclusive.com>
2024-05-01 09:19:42 +0000

    start training component

---
+++ projects/entity-linking/train/README.md
@@ -0,0 +1,11 @@
+# Overview
+
+For details on how to run and maintain the `sentiment` project `train` component, please refer
+to the
+- [the project README](../README.md) and
+- [the project train doc.](../../docs/02_train.md)
+
+using
+
+- `PROJECT_NAME=sentiment`, and
+- `COMPONENT=train`
---
+++ projects/entity-linking/train/src/settings.py
@@ -0,0 +1,63 @@
+"""Settings."""
+
+# Standard Library
+import os
+
+# Internal libraries
+from onclusiveml.tracking import (
+    TrackedModelCard,
+    TrackedModelSpecs,
+    TrackedParams,
+)
+
+
+# --- settings classes
+class TrackedEntityLinkingModelSpecs(TrackedModelSpecs):
+    """Tracked entity-linking model settings."""
+
+    project: str = "onclusive/entity-linking"
+    model = "EL-TRAINED"
+
+    class Config:
+        env_file = "config/dev.env"
+        env_file_encoding = "utf-8"
+
+
+class EntityLinkingSettings(TrackedParams):
+    """Entity linking settings."""
+
+    class Config:
+        env_file = "config/dev.env"
+        env_file_encoding = "utf-8"
+
+
+class EntityLinkingModelParams(TrackedParams):
+    """Entity linking Model parameters."""
+
+    repo: str = "wannaphong/BELA"
+    checkpoint_name: str = "wiki"
+    embeddings_filename: str = "embeddings.pt"
+    index_filename: str = "index.txt"
+
+    entity_linking_settings: EntityLinkingSettings = EntityLinkingSettings()
+
+    class Config:
+        env_file = "config/dev.env"
+        env_file_encoding = "utf-8"
+
+
+class TrackedEntityLinkingBaseModelCard(TrackedModelCard):
+    """The model card for the base model of the EL ML project."""
+
+    model_type: str = "trained"
+    # --- custom fields
+    # model params
+    model_params: EntityLinkingModelParams = EntityLinkingModelParams()
+    # model_inputs: Inputs = Inputs()
+    # admin
+    local_output_dir: str = os.path.join(".", "sent_model_artifacts")
+    logging_level: str = "INFO"
+
+    class Config:
+        env_file = "config/dev.env"
+        env_file_encoding = "utf-8"
