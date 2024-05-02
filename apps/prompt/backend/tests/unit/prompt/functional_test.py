51c9904086967556cf4bf91d43b0049caf66a333
Rene-Jean Corneille <rene-jean.corneille@onclusive.com>
2024-05-02 11:02:44 +0000

    fix failing unit test

--- apps/prompt/backend/tests/unit/prompt/functional_test.py
+++ apps/prompt/backend/tests/unit/prompt/functional_test.py
@@ -7,6 +7,7 @@ from unittest.mock import patch
 import pytest
 import redis
 from langchain.chains import ConversationChain
+from langchain_core.runnables.base import RunnableSequence
 from redis_cache import RedisCache
 
 # Source
@@ -24,7 +25,7 @@ from src.prompt.tables import PromptTemplate
 )
 @patch.object(PromptTemplate, "get")
 @patch.object(LanguageModel, "get")
-@patch.object(ConversationChain, "predict")
+@patch.object(RunnableSequence, "invoke")
 @patch.object(RedisCache, "__call__")
 @patch.object(redis.connection.ConnectionPool, "get_connection")
 @patch("botocore.session.Session")
@@ -42,18 +43,19 @@ def test_generate_from_prompt_template(
     provider,
 ):
     """Test generate from prompt template."""
+    prompt = PromptTemplate(alias=prompt_alias, template=template, project=project)
     mock_redis_get_connection.return_value.retry.call_with_retry.return_value = dict()
     mock_conversation_chain_predict.return_value = dict()
     mock_model_get.return_value = LanguageModel(alias=model_alias, provider=provider)
-    mock_prompt_get.return_value = PromptTemplate(
-        alias=prompt_alias, template=template, project=project
-    )
+    mock_prompt_get.return_value = prompt
 
     _ = F.generate_from_prompt_template(
         prompt_alias, model_alias, **{"input": {"text": ""}}
     )
 
-    mock_conversation_chain_predict.assert_called_with(input="Human: ")
+    mock_conversation_chain_predict.assert_called_with(
+        {"text": "", "format_instructions": prompt.format_instructions}
+    )
 
 
 @pytest.mark.parametrize(
