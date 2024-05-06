b2d32e20e680f38f170dd53f6b3f86cff85f171b
jian.tong <jian.tong@onclusive.com>
2024-05-06 16:05:23 +0000

    update word tokenizer

--- libs/nlp/onclusiveml/nlp/tokenizers/word/factory.py
+++ libs/nlp/onclusiveml/nlp/tokenizers/word/factory.py
@@ -3,9 +3,17 @@
 # Internal libraries
 from onclusiveml.nlp.tokenizers.consts import NLTK_SUPPORTED_LANGS
 from onclusiveml.nlp.tokenizers.factory import TokenizerFactory
-from onclusiveml.nlp.tokenizers.word.nltk_tokenizer import NLTKWordTokenizer
+from onclusiveml.nlp.tokenizers.word.nltk_tokenizer import (
+    JiebaWordTokenizer,
+    KonlpyWordTokenizer,
+    MecabWordTokenizer,
+    NLTKWordTokenizer,
+)
 
 
 word_factory = TokenizerFactory()
 for lang in NLTK_SUPPORTED_LANGS:
     word_factory.register_language(lang, NLTKWordTokenizer())
+word_factory.register_language("ko", KonlpyWordTokenizer())
+word_factory.register_language("ja", MecabWordTokenizer())
+word_factory.register_language("zh", JiebaWordTokenizer())
--- libs/nlp/onclusiveml/nlp/tokenizers/word/nltk_tokenizer.py
+++ libs/nlp/onclusiveml/nlp/tokenizers/word/nltk_tokenizer.py
@@ -5,7 +5,10 @@ import re
 from typing import Any, Dict, List
 
 # 3rd party libraries
+import jieba
 import nltk
+from konlpy.tag import Okt
+from konoha import WordTokenizer
 
 
 nltk.download("punkt")
@@ -38,6 +41,74 @@ class NLTKWordTokenizer(BaseWordTokenizer):
         for word in words_first:
             w = self.regex.split(word)
             words += w
+
+        ret = {"words": words}
+
+        return ret
+
+
+class JiebaWordTokenizer(BaseWordTokenizer):
+    """Tokenizing words in a given text."""
+
+    def tokenize(self, content: str) -> Dict[str, List[Any]]:
+        """Tokenizes the input content into words.
+
+        Uses both nltk word tokenize and regex using list of unique characters
+
+        Args:
+            content (str): Text to be tokenized into words
+            language (str, optional): Language of the text (default English)
+
+        Returns:
+            dict: Dictionary containing tokenized words
+        """
+        words = jieba.lcut(content)
+
+        ret = {"words": words}
+
+        return ret
+
+
+class KonlpyWordTokenizer(BaseWordTokenizer):
+    """Tokenizing words in a given text."""
+
+    def tokenize(self, content: str) -> Dict[str, List[Any]]:
+        """Tokenizes the input content into words.
+
+        Uses both nltk word tokenize and regex using list of unique characters
+
+        Args:
+            content (str): Text to be tokenized into words
+            language (str, optional): Language of the text (default English)
+
+        Returns:
+            dict: Dictionary containing tokenized words
+        """
+        tokenizer_okt = Okt()
+        words = tokenizer_okt.morphs(content)
+
+        ret = {"words": words}
+
+        return ret
+
+
+class MeCabWordTokenizer(BaseWordTokenizer):
+    """Tokenizing words in a given text."""
+
+    def tokenize(self, content: str) -> Dict[str, List[Any]]:
+        """Tokenizes the input content into words.
+
+        Uses both nltk word tokenize and regex using list of unique characters
+
+        Args:
+            content (str): Text to be tokenized into words
+            language (str, optional): Language of the text (default English)
+
+        Returns:
+            dict: Dictionary containing tokenized words
+        """
+        tokenizer = WordTokenizer("MeCab")
+        words = tokenizer.tokenize(content)
 
         ret = {"words": words}
 
