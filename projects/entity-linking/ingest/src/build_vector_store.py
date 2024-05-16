a5edf86bd0a65f94c347763391a53ea2d3ed2c9a
Ivan Okhotnikov <ivan.okhotnikov@onclusive.com>
2024-05-16 08:51:44 +0000

    add docker file, compose ingest, debug launch entry

--- projects/entity-linking/ingest/src/build_vector_store.py
+++ projects/entity-linking/ingest/src/build_vector_store.py
@@ -19,10 +19,33 @@ from redis.exceptions import ResponseError
 from src.settings import get_settings
 
 
+class WikiEmbeddings(Dataset):
+    """Index and embeddings dataset."""
+
+    def __init__(self, embeddings_file: str, index_file: str) -> None:
+        self.embeddings_file = embeddings_file
+        self.index_file = index_file
+        self.embeddings = torch.load(self.embeddings_file)
+        self.index = self._get_index()
+
+    def _get_index(self) -> List:
+        with open(self.index_file, "r") as f:
+            index = [line.strip() for line in f]
+        return index
+
+    def __len__(self) -> int:
+        return len(self.index)
+
+    def __getitem__(self, idx: int) -> tuple:
+        return self.index[idx], self.embeddings[idx].numpy().tobytes()
+
+
 def create_redis_index(
     vector_dimensions: int, client: Redis, settings: BaseSettings
 ) -> None:
     """Create Redis index.
+
+    https://redis-py.readthedocs.io/en/stable/examples/search_vector_similarity_examples.html.
 
     Args:
         vector_dimensions (int): dimensions of a vector embedding
@@ -50,29 +73,28 @@ def create_redis_index(
         client.ft(index_name=settings.INDEX_NAME).create_index(fields=schema)
 
 
-class WikiEmbeddings(Dataset):
-    """Index and embeddings dataset."""
+def write_to_redis_index(client: Redis, loader: DataLoader) -> None:
+    """Write the data from loader to the redis index.
 
-    def __init__(self, settings: BaseSettings) -> None:
-        self.settings = settings
-        self.embeddings = torch.load(self.settings.EMBEDDINGS_FILE)
-        self.index = self._get_index()
-
-    def _get_index(self) -> List:
-        with open(self.settings.INDEX_FILE, "r") as f:
-            index = [line.strip() for line in f]
-        return index
-
-    def __len__(self) -> int:
-        return len(self.index)
-
-    def __getitem__(self, idx: int) -> tuple:
-        return self.index[idx], self.embeddings[idx].numpy().tobytes()
+    Args:
+        client (redis.client.Redis): Redis client
+        loader (torch.utils.data.DataLoader): Dataset in pytorch DataLoader
+    """
+    pipe = client.pipeline()
+    for idx, obj in loader:
+        pipe.hset(name=idx[0], mapping={"embedding": obj[0]})
+    pipe.execute()
 
 
-if __name__ == "__main__":
-    settings = get_settings()
-    wiki_embeddings = WikiEmbeddings(settings=settings)
+def build_vector_store(settings: BaseSettings) -> None:
+    """Build vector store.
+
+    Args:
+        settings (pydantic.BaseSettings): Pydantic settings
+    """
+    wiki_embeddings = WikiEmbeddings(
+        embeddings_file=settings.EMBEDDINGS_FILE, index_file=settings.INDEX_FILE
+    )
     loader = DataLoader(dataset=wiki_embeddings)
     client = from_url(url=settings.REDIS_CONNECTION_STRING)
     create_redis_index(
@@ -80,7 +102,9 @@ if __name__ == "__main__":
         client=client,
         settings=settings,
     )
-    pipe = client.pipeline()
-    for idx, obj in loader:
-        pipe.hset(name=idx[0], mapping={"embedding": obj[0]})
-        pipe.execute()
+    write_to_redis_index(client=client, loader=loader)
+
+
+if __name__ == "__main__":
+    settings = get_settings()
+    build_vector_store(settings)
