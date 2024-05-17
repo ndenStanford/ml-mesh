2819d96e47e1992fcc46205fe556a7d1ae207199
Ivan Okhotnikov <ivan.okhotnikov@onclusive.com>
2024-05-17 06:36:48 +0000

    add querying example

---
+++ projects/entity-linking/ingest/src/example_querying.py
@@ -0,0 +1,48 @@
+# type: ignore
+"""Querying exmaple."""
+
+# 3rd party libraries
+import numpy as np
+from pydantic import BaseSettings
+from redis import from_url
+from redis.commands.search.query import Query
+
+# Source
+from src.build_vector_store import WikiEmbeddings
+from src.settings import get_settings
+
+
+def run_querying_example(settings: BaseSettings) -> None:
+    """Runs querying example.
+
+    https://redis-py.readthedocs.io/en/stable/examples/search_vector_similarity_examples.html.
+
+    Args:
+        settings (pydantic.BaseSettings): Pydantic settings
+    """
+    try:
+        client = from_url(url=settings.REDIS_CONNECTION_STRING)
+    except ValueError as e:
+        print(f"REDIS_CONNECTION_STRING is not valid: {e}")
+        raise
+    wiki_embeddings = WikiEmbeddings(
+        embeddings_file=settings.EMBEDDINGS_FILE, index_file=settings.INDEX_FILE
+    )
+    query = (
+        Query("*=>[KNN 5 @embedding $query_vector as score]")
+        .sort_by("score")
+        .return_fields("id", "score")
+        .paging(0, 2)
+        .dialect(2)
+    )
+    query_params = {
+        "query_vector": np.random.rand(wiki_embeddings.embeddings.shape[1])
+        .astype(np.float32)
+        .tobytes()
+    }
+    client.ft(settings.INDEX_NAME).search(query, query_params).docs
+
+
+if __name__ == "__main__":
+    settings = get_settings()
+    run_querying_example(settings)
