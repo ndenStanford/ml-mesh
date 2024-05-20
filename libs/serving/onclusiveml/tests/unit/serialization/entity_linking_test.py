c4711596506879404ec8e925926e088b33af8dd7
Amaury de Guillebon <amaury.deguillebon@onclusive.com>
2024-05-20 21:35:44 +0000

    library test update

--- libs/serving/onclusiveml/tests/unit/serialization/entity_linking_test.py
+++ libs/serving/onclusiveml/tests/unit/serialization/entity_linking_test.py
@@ -26,7 +26,12 @@ def test_request_schema_attributes(content):
 
     assert attributes.content == content
     assert isinstance(attributes.content, str)
-    assert dict(attributes) == {"content": content, "entities": None}
+    assert dict(attributes) == {
+        "content": content,
+        "entities": None,
+        "mention_offsets": None,
+        "mention_lengths": None,
+    }
 
 
 def test_request_schema_attributes_extra():
@@ -53,44 +58,44 @@ def test_request_schema_parameters_extra():
 
 @pytest.mark.parametrize(
     "entity_type, entity_text, score, sentence_index, wiki_link",
-    [("ORG", "Company name", 0.9, 0, "link")],
+    [("Company name", 0.9, 0, "link", 0.459234)],
 )
 def test_response_attribute_schema(
-    entity_type, entity_text, score, sentence_index, wiki_link
+    entity_text, score, sentence_index, wiki_link, wiki_score
 ):
     """Test response schema parameters."""
     attributes = PredictResponseAttributeSchemaV1(
         entities=[
             PredictResponseEntity(
-                entity_type=entity_type,
                 entity_text=entity_text,
                 score=score,
                 sentence_index=sentence_index,
                 wiki_link=wiki_link,
+                wiki_score=wiki_score,
             )
         ]
     )
 
-    assert attributes.entities[0].entity_type == entity_type
     assert attributes.entities[0].entity_text == entity_text
     assert attributes.entities[0].score == score
     assert attributes.entities[0].sentence_index == sentence_index
     assert attributes.entities[0].wiki_link == wiki_link
+    assert attributes.entities[0].wiki_score == wiki_score
 
-    assert isinstance(attributes.entities[0].entity_type, str)
     assert isinstance(attributes.entities[0].entity_text, str)
     assert isinstance(attributes.entities[0].score, float)
     assert isinstance(attributes.entities[0].sentence_index, int)
     assert isinstance(attributes.entities[0].wiki_link, str)
+    assert isinstance(attributes.entities[0].wiki_score, float)
 
     assert dict(attributes) == {
         "entities": [
             {
-                "entity_type": entity_type,
                 "entity_text": entity_text,
                 "score": score,
                 "sentence_index": sentence_index,
                 "wiki_link": wiki_link,
+                "wiki_score": wiki_score,
             }
         ]
     }
