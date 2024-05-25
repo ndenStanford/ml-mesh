c9a0560b92a85d6b5f2edca50619675ed77e1e5b
Nutch <nutch.den@onclusive.com>
2024-05-25 14:32:04 +0000

    add test

--- projects/gch-summarization/serve/tests/functional/serve/server_test.py
+++ projects/gch-summarization/serve/tests/functional/serve/server_test.py
@@ -87,3 +87,36 @@ def test_model_server_prediction(payload, expected_response):
     assert response.status_code == 200
     # TODO: assert score close to expected
     assert response.json() == expected_response
+
+
+@pytest.mark.parametrize(
+    "payload",
+    [
+        (
+            {
+                "data": {
+                    "namespace": "gch-summarization",
+                    "attributes": {
+                        "content": "天空新闻宣布了英国新任总理任命的一系列特别节目总理。\
+                        天空新闻的政治节目将在重要的一周之前扩大英国政坛即将宣布新首相。\
+                        天空新闻键时政类节目将回归，为观众带来深度讨论与剖析“ \
+                        来自唐宁街和威斯敏斯特的所有最新新闻现场报道。\
+                        天空电视台负责人新闻，约翰·莱利：“这是英国政坛重要的一周，\
+                        新首相将处理一个充满紧急决策的托盘。"
+                    },
+                    "parameters": {
+                        "language": "zh",
+                    },
+                }
+            }
+        )
+    ],
+)
+def test_model_server_prediction_invalid_language(payload):
+    """Tests predict endpoint of the gch summarization ModelServer."""
+    response = requests.post(
+        "http://serve:8000/gch-summarization/v1/predict",
+        json=payload,
+    )
+
+    assert response.status_code == 422
