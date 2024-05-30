bde0c1d1061ada00bdbd0357579365ec356b8d2f
Amaury de Guillebon <amaury.deguillebon@onclusive.com>
2024-05-30 20:33:52 +0000

    changing model name

--- projects/entity-linking/serve/src/settings.py
+++ projects/entity-linking/serve/src/settings.py
@@ -24,7 +24,7 @@ class TrackedTrainedModelSpecs(TrackedModelSpecs):
 
     # we need an additional version tag since we are referencing an EXISTING model version, rather
     # than creating a new one
-    with_id: str = Field("EL-TRAINED-22", env="neptune_model_version_id")
+    with_id: str = Field("EL-TRAINED-", env="neptune_model_version_id")
     # we only need to download from the base model, not upload
     mode: str = Field(Mode.READ_ONLY, env="neptune_client_mode")
 
