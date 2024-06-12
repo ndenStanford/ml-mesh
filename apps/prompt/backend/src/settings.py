77be182b3fd1a51b533a00f2a22c4dc174ca9f42
Nutch <nutch.den@onclusive.com>
2024-06-12 14:56:17 +0000

    add default prompt

--- apps/prompt/backend/src/settings.py
+++ apps/prompt/backend/src/settings.py
@@ -47,7 +47,7 @@ class Settings(OnclusiveBaseSettings):
     AWS_PROFILE: Optional[str] = None
     AWS_ACCESS_KEY_ID: Optional[str] = None
     AWS_SECRET_ACCESS_KEY: Optional[str] = None
-    DEFAULT_MODELS: dict = {}
+    DEFAULT_MODELS: dict = {"default": "anthropic.claude-3-haiku-20240307-v1:0"}
     CORS_ORIGIN: List[str] = ["*"]
 
     # Github configuration
