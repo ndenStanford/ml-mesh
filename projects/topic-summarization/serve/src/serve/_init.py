06552b3c54d3655d7f9bff72bae83d30b03f1fec
Syed Reza <syed.reza@onclusive.com>
2024-06-13 16:19:47 +0000

    bring back init

---
+++ projects/topic-summarization/serve/src/serve/_init.py
@@ -0,0 +1,31 @@
+# Standard Library
+from typing import Type
+
+# 3rd party libraries
+from botocore.exceptions import ClientError
+from dyntastic import Dyntastic
+
+# Internal libraries
+from onclusiveml.core.logging import get_default_logger
+
+# Source
+from src.serve.tables import TopicSummaryDynamoDB
+from src.settings import get_settings
+
+logger = get_default_logger(__name__)
+settings = get_settings()
+
+
+def init() -> None:
+    """App initialization."""
+    logger.info("Creating tables...")
+    if settings.ENVIRONMENT == "dev":
+        _create_table(TopicSummaryDynamoDB)
+
+
+def _create_table(table: Type[Dyntastic]) -> None:
+    """Create Tables."""
+    try:
+        table.create_table()
+    except ClientError as e:
+        logger.info("Table creation failed with error: {}".format(e))
