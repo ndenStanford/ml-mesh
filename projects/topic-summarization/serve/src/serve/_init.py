f30d732d2d9c6fed85aa864809fafc477bf96c7c
Syed Reza <syed.reza@onclusive.com>
2024-06-13 16:29:30 +0000

    skip isort for init

--- projects/topic-summarization/serve/src/serve/_init.py
+++ projects/topic-summarization/serve/src/serve/_init.py
@@ -1,3 +1,4 @@
+# isort: skip_file
 # Standard Library
 from typing import Type
 
@@ -11,6 +12,7 @@ from onclusiveml.core.logging import get_default_logger
 # Source
 from src.serve.tables import TopicSummaryDynamoDB
 from src.settings import get_settings
+
 
 logger = get_default_logger(__name__)
 settings = get_settings()
