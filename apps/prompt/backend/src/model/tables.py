2c1d9473bac231e73c98007c3d1d3fd017d86933
vishal.singh <vishal.singh@onclusive.com>
2024-06-20 10:52:56 +0000

    added header paramters to override model_params

--- apps/prompt/backend/src/model/tables.py
+++ apps/prompt/backend/src/model/tables.py
@@ -1,11 +1,12 @@
 """Language model dynamoDB tables."""
 
 # Standard Library
-from typing import Optional
+from typing import Any, Dict, Optional
 
 # 3rd party libraries
 import boto3
 from dyntastic import Dyntastic
+from fastapi import Header
 from langchain_community.chat_models import BedrockChat, ChatOpenAI
 
 # Internal libraries
@@ -15,6 +16,7 @@ from onclusiveml.llms.typing import LangchainT
 # Source
 from src.model.constants import (
     BedrockModelParameters,
+    ChatModel,
     ChatModelProdiver,
     OpenaiModelParameters,
 )
@@ -34,17 +36,23 @@ class LanguageModel(Dyntastic, LangchainConvertibleMixin):
 
     alias: str
     provider: str
+    model_params: Dict[str, Any] = Header(None)
 
     def as_langchain(self) -> Optional[LangchainT]:
         """Return model as langchain chat model."""
         if self.provider == ChatModelProdiver.OPENAI:
-            model_params = OpenaiModelParameters()
+            if self.model_params is None:
+                self.model_params = OpenaiModelParameters()
             return ChatOpenAI(
                 model=self.alias,
                 temperature=model_params.temperature,
                 max_tokens=model_params.max_tokens,
             )
         if self.provider == ChatModelProdiver.BEDROCK:
+            print(self.model_params)
+            if self.model_params is None:
+                self.model_params = BedrockModelParameters().dict()
+                print(self.model_params)
             boto3.setup_default_session(
                 profile_name=settings.AWS_PROFILE,
                 aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
@@ -55,11 +63,10 @@ class LanguageModel(Dyntastic, LangchainConvertibleMixin):
                 region_name=settings.AWS_DEFAULT_REGION,
                 endpoint_url="https://bedrock-runtime.us-east-1.amazonaws.com",
             )
+            chat_models = dict(map(lambda item: (item.value, item.name), ChatModel))
             return BedrockChat(
                 client=bedrock,
                 model_id=self.alias,
-                model_kwargs=BedrockModelParameters().dict()[
-                    f"params_{self.alias}".lower()
-                ],
+                model_kwargs=self.model_params[f"{chat_models[self.alias]}".lower()],
             )
         return None
