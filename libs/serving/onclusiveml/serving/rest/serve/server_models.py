93da457e7775eac6641d992c7d39f080005b44c6
Rene-Jean Corneille <rene-jean.corneille@onclusive.com>
2024-06-24 11:32:36 +0000

    fix tests

--- libs/serving/onclusiveml/serving/rest/serve/server_models.py
+++ libs/serving/onclusiveml/serving/rest/serve/server_models.py
@@ -4,7 +4,7 @@
 from typing import Any, Dict, List, Optional
 
 # 3rd party libraries
-from pydantic import validator
+from pydantic import model_validator
 
 # Internal libraries
 from onclusiveml.core.base import OnclusiveBaseModel
@@ -33,7 +33,7 @@ class ModelServerURLs(OnclusiveBaseModel):
     redoc: Optional[str] = ""
     openapi: Optional[str] = ""
 
-    @validator
+    @model_validator(mode="before")
     def check_non_root_urls(cls, values: Dict) -> Dict:
         """Checks that each url indeed starts with the root url."""
         root_url = values["root"]
