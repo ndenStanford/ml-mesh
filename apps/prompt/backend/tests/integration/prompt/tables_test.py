40360eee8dff7a737551c153d109a0505d42a5dd
vishal.singh <vishal.singh@onclusive.com>
2024-06-27 10:00:15 +0000

    ML-1288-added-put-endpoint-test

--- apps/prompt/backend/tests/integration/prompt/tables_test.py
+++ apps/prompt/backend/tests/integration/prompt/tables_test.py
@@ -6,6 +6,7 @@ import os
 # 3rd party libraries
 import pytest
 from dyntastic.exceptions import DoesNotExist
+from fastapi import status
 
 # Source
 from src.extensions.github import github
@@ -28,6 +29,39 @@ def test_save(alias, template, project, app):
     prompt.save()
 
     assert PromptTemplate.get(alias).json() == prompt.json()
+    assert os.path.join(project, alias) in github.ls(project)
+
+
+@pytest.mark.parametrize(
+    "alias, original_template, new_template, project",
+    [
+        ("prompt-12", "input: {old_text}", "input: {new_text}", "integration-test-1"),
+    ],
+)
+@pytest.mark.order(15)
+def test_update_prompt(alias, original_template, new_template, project, app):
+    """Test update method."""
+    with pytest.raises(DoesNotExist):
+        _ = PromptTemplate.get(alias)
+
+    prompt = PromptTemplate(alias=alias, template=original_template, project=project)
+    prompt.save()
+
+    assert PromptTemplate.get(alias).json() == prompt.json()
+
+    response = app.put(
+        f"/api/v2/prompts/{alias}",
+        headers={"x-api-key": "1234"},
+        json={"template": new_template},
+    )
+
+    assert response.status_code == status.HTTP_201_CREATED
+
+    updated_prompt = PromptTemplate.get(alias)
+    assert updated_prompt.template == new_template
+
+    assert response.json() == updated_prompt.json()
+
     assert os.path.join(project, alias) in github.ls(project)
 
 
