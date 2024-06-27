f246984037dcae5c7e6ec142bbca85fec2b5e473
zheyuan.hu <zheyuan.hu@onclusive.com>
2024-06-27 18:46:51 +0000

    update ci.yaml, settings, .env file for train and compile

--- projects/ner/compile/src/settings.py
+++ projects/ner/compile/src/settings.py
@@ -39,7 +39,7 @@ class UncompiledTrackedModelSpecs(TrackedModelSpecs):
     model: str = "NERMULTI-TRAINED"
     # we need an additional version tag since we are referencing an EXISTING model version, rather
     # than creating a new one
-    with_id: str = "NERMULTI-TRAINED-7"
+    with_id: str = "NERMULTI-TRAINED-13"
     # we only need to download from the base model, not upload
     mode: str = Field(Mode.READ_ONLY)
 
