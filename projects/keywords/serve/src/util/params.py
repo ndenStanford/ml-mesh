663449c94fc6fa24c0ae09547f66bef6b561a1b4
Rene-Jean Corneille <rene-jean.corneille@onclusive.com>
2024-07-02 12:50:26 +0000

    update keywords model id

--- projects/keywords/serve/src/util/params.py
+++ projects/keywords/serve/src/util/params.py
@@ -13,6 +13,6 @@ class TrackedCompiledModelSpecs(TrackedModelSettings):
 
     # we need an additional version tag since we are referencing an EXISTING model version, rather
     # than creating a new one
-    with_id: str = Field("KEYWORDS-COMPILED-88", env="neptune_model_version_id")
+    with_id: str = Field("KEYWORDS-COMPILED-231", env="neptune_model_version_id")
     # we only need to download from the base model, not upload
     mode: str = Field(Mode.READ_ONLY, env="neptune_client_mode")
