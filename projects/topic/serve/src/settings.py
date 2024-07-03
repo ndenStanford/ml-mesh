32042128dd8236b98660044c082e3533283e03f4
Rene-Jean Corneille <rene-jean.corneille@onclusive.com>
2024-07-03 08:34:08 +0000

    fix variable name

--- projects/topic/serve/src/settings.py
+++ projects/topic/serve/src/settings.py
@@ -5,10 +5,6 @@ from functools import lru_cache
 from pathlib import Path
 from typing import List, Union
 
-# 3rd party libraries
-from neptune.types.mode import Mode
-from pydantic import Field
-
 # Internal libraries
 from onclusiveml.core.base import OnclusiveBaseSettings
 from onclusiveml.nlp.language.constants import LanguageIso
@@ -36,16 +32,6 @@ SUPPORTED_LANGUAGES = [
 ]
 
 
-class TrackedTrainedModelSpecs(TrackedModelSettings):
-    """Tracked compiled model settings."""
-
-    # we need an additional version tag since we are referencing an EXISTING model version, rather
-    # than creating a new one
-    with_id: str = Field("TOPIC-TRAINED-63", env="neptune_model_version_id")
-    # we only need to download from the base model, not upload
-    mode: str = Field(Mode.READ_ONLY, env="neptune_client_mode")
-
-
 class ServerModelSettings(ServingParams):
     """Prediction model settings."""
 
@@ -61,10 +47,10 @@ class TopicSettings(TrackingSettings):
 
 class GlobalSettings(
     ServerModelSettings,
-    TrackedTrainedModelSpecs,
     TrackedGithubActionsSpecs,
     TrackedImageSpecs,
     TopicSettings,
+    TrackedModelSettings,
 ):
     """Global server settings."""
 
