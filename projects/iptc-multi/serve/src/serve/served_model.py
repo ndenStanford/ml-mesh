2e9912ecd3ce7275069aad3f5134ee3f367d3746
vincent.park <vincent.park@onclusive.com>
2024-07-10 17:42:02 +0000

    Keep track of failed models

--- projects/iptc-multi/serve/src/serve/served_model.py
+++ projects/iptc-multi/serve/src/serve/served_model.py
@@ -4,7 +4,7 @@
 import random
 import re
 import time
-from typing import Any, Dict, List, Type
+from typing import Any, Dict, List, Set, Type
 
 # Internal libraries
 from onclusiveml.core.base import OnclusiveBaseModel
@@ -48,6 +48,7 @@ class ServedIPTCMultiModel(ServedModel):
     def __init__(self) -> None:
         super().__init__(name="iptc-multi")
         self.last_checked: Dict[str, float] = {}
+        self.last_failed: Set[str] = set()
         self.sample_inference_content = settings.sample_inference_content
         self.historically_high_inferenced_models = (
             settings.historically_high_inferenced_models
@@ -78,12 +79,14 @@ class ServedIPTCMultiModel(ServedModel):
                     logger.error(
                         f"Error while inferencing the IPTC model {model_id}: empty attributes"
                     )
+                    self.last_failed[model_id] = True
                     all_models_live = False
                     break
             except Exception as e:
                 logger.error(
                     f"Error while inferencing the IPTC model {model_id}: {str(e)}"
                 )
+                self.last_failed[model_id] = True
                 all_models_live = False
                 break
         return all_models_live
@@ -125,6 +128,9 @@ class ServedIPTCMultiModel(ServedModel):
             bool: True if the model should be checked based on the current probability influenced by
             historical frequency and time decay; False otherwise.
         """
+        if self.last_failed.get(model_id):
+            self.last_failed[model_id] = None
+            return True
         probability = self._calculate_probability_with_decay(model_id)
         should_check = random.random() < probability
         if should_check:
