f411d2ef765c060b8f5337d9ab7749a1ff30d877
Rene-Jean Corneille <rene-jean.corneille@onclusive.com>
2024-07-29 15:00:38 +0000

    add base module

---
+++ libs/fileio/onclusiveml/fileio/base/__init__.py
@@ -0,0 +1,8 @@
+"""Init."""
+
+# Internal libraries
+from onclusiveml.fileio.base.filesystem import BaseFileSystem
+from onclusiveml.fileio.base.pathmodule import BasePathModule
+
+
+__all__ = ["BaseFileSystem", "BasePathModule"]
