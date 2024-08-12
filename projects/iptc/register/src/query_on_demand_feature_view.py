72e63563c0cab792b3521cffc8d3a8be49f02909
Nutch <nutch.den@onclusive.com>
2024-08-12 11:37:40 +0000

    add test files

--- projects/iptc/register/src/query_on_demand_feature_view.py
+++ projects/iptc/register/src/query_on_demand_feature_view.py
@@ -1,8 +1,18 @@
 # Internal libraries
 from onclusiveml.data.feature_store import FeastRepoBuilder
+import time
 
 # Source
 from src.settings import FeatureRegistrationParams
+
+# from src.utils import *
+
+# from src.class_dict import CANDIDATE_DICT_FIRST
+
+# title="Tesla, Inc. is an American multinational automotive and clean energy company."
+# article="""Tesla, Inc. is an American multinational automotive and clean energy company. Headquartered in Austin, Texas, it designs, manufactures and sells battery electric vehicles, stationary battery energy storage devices from home to grid-scale, solar panels and solar shingles, and related products and services."""
+# topic=generate_label_llm(title, article, CANDIDATE_DICT_FIRST)
+# topic
 
 
 feature_registration_params = FeatureRegistrationParams()
@@ -18,6 +28,8 @@ feature_view = [
 features = [f"{feature_view.name}:{feature.name}" for feature in feature_view.features]
 
 features += ["iptc_first_level_on_demand_feature_view:topic_1_llm"]
+
+start = time.time()
 entity_df = """SELECT iptc_id, CURRENT_TIMESTAMP AS event_timestamp FROM "feast"."iptc_first_level"
 LIMIT 10"""
 
@@ -25,4 +37,37 @@ dataset_df = fs_handle.fs.get_historical_features(
     entity_df=entity_df, features=features
 )
 df = dataset_df.to_df()
+
+print('='*20)
+print('10 samples')
+print(time.time() - start)
+print(df.head())
+
+
+start = time.time()
+entity_df = """SELECT iptc_id, CURRENT_TIMESTAMP AS event_timestamp FROM "feast"."iptc_first_level"
+LIMIT 100"""
+
+dataset_df = fs_handle.fs.get_historical_features(
+    entity_df=entity_df, features=features
+)
+df = dataset_df.to_df()
+
+print('='*20)
+print('100 samples')
+print(time.time() - start)
+print(df.head())
+
+start = time.time()
+entity_df = """SELECT iptc_id, CURRENT_TIMESTAMP AS event_timestamp FROM "feast"."iptc_first_level"
+LIMIT 1000"""
+
+dataset_df = fs_handle.fs.get_historical_features(
+    entity_df=entity_df, features=features
+)
+df = dataset_df.to_df()
+
+print('='*20)
+print('1000 samples')
+print(time.time() - start)
 print(df.head())
--- projects/iptc/register/src/utils.py
+++ projects/iptc/register/src/utils.py
@@ -8,7 +8,7 @@ from typing import Dict
 # 3rd party libraries
 import pandas as pd
 import requests
-from class_dict import CANDIDATE_DICT_FIRST
+from src.class_dict import CANDIDATE_DICT_FIRST
 
 
 class PromptBackendAPISettings:  # OnclusiveBaseSettings is not serializable.
@@ -22,19 +22,20 @@ class PromptBackendAPISettings:  # OnclusiveBaseSettings is not serializable.
     IPTC_RESPONSE_SCHEMA: Dict[str, str] = {
         "iptc category": "Answer the IPTC category",
     }
-    DEFAULT_MODEL: str = "anthropic.claude-3-5-sonnet-20240620-v1:0"
+    DEFAULT_MODEL: str = "gpt-4o-mini"
 
 
 settings = PromptBackendAPISettings()
 
 
-def generate_label_llm(title, article, candidates):
+def generate_label_llm(row):
     """Invoke LLM to generate IPTC ."""
     input_dict = {
-        "input": {"title": title, "article": article, "candidates": candidates},
+        "input": {"title": row['title'], "article": row['content'], "candidates": CANDIDATE_DICT_FIRST},
         "output": settings.IPTC_RESPONSE_SCHEMA,
     }
     headers = {"x-api-key": settings.INTERNAL_ML_ENDPOINT_API_KEY}
+
     q = requests.post(
         "{}/api/v2/prompts/{}/generate/model/{}".format(
             settings.PROMPT_API, settings.CLAUDE_IPTC_ALIAS, settings.DEFAULT_MODEL
@@ -50,11 +51,50 @@ def iptc_first_level_on_demand_feature_view(features_df: pd.DataFrame) -> pd.Dat
     """On-demand feature view transformation."""
     
     df = pd.DataFrame()
-    topic_label = [
-        generate_label_llm(title, content, CANDIDATE_DICT_FIRST)
-        for title, content in zip(
-            features_df["title"].values, features_df["content"].values
-        )
-    ]
-    df["topic_1_llm"] = pd.Series(topic_label).astype(pd.StringDtype())
+    df["topic_1_llm"] = features_df.apply(generate_label_llm,axis=1).astype(pd.StringDtype())
     return df
+
+# def generate_label_llm(title, article, candidates):
+#     """Invoke LLM to generate IPTC ."""
+#     input_dict = {
+#         "input": {"title": title, "article": article, "candidates": candidates},
+#         "output": settings.IPTC_RESPONSE_SCHEMA,
+#     }
+#     headers = {"x-api-key": settings.INTERNAL_ML_ENDPOINT_API_KEY}
+
+#     q = requests.post(
+#         "{}/api/v2/prompts/{}/generate/model/{}".format(
+#             settings.PROMPT_API, settings.CLAUDE_IPTC_ALIAS, settings.DEFAULT_MODEL
+#         ),
+#         headers=headers,
+#         json=input_dict,
+#     )
+#     output_content = json.loads(q.content)
+#     return output_content["iptc category"]
+
+
+# def iptc_first_level_on_demand_feature_view(features_df: pd.DataFrame) -> pd.DataFrame:
+#     """On-demand feature view transformation."""
+    
+#     df = pd.DataFrame()
+#     topic_label = [
+#         generate_label_llm(title, content, CANDIDATE_DICT_FIRST)
+#         for title, content in zip(
+#             features_df["title"].values, features_df["content"].values
+#         )
+#     ]
+#     df["topic_1_llm"] = pd.Series(topic_label).astype(pd.StringDtype())
+#     return df
+
+# def iptc_llm_feature_view_2(features_df: pd.DataFrame) -> pd.DataFrame:
+#     """On-demand feature view transformation."""
+    
+#     df = pd.DataFrame()
+#     topic_label = [
+#         generate_label_llm(title, content, CANDIDATE_DICT_FIRST)
+#         for title, content in zip(
+#             features_df["title"].values, features_df["content"].values
+#         )
+#     ]
+#     df["topic_1_llm"] = pd.Series(topic_label).astype(pd.StringDtype())
+#     return df
