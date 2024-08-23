1c835d7cf0bec06649bb30c4a751ad4ab9f3e799
Nutch <nutch.den@onclusive.com>
2024-08-23 18:38:52 +0000

    first level util

--- projects/iptc/register/src/class_dict.py
+++ projects/iptc/register/src/class_dict.py
@@ -1,70 +1,357 @@
-CANDIDATE_DICT_FIRST = [
-        {
+CANDIDATE_DICT_FIRST = {
+    "root": {
+        "0": {
             "name": "arts, culture, entertainment and media",
             "description": "All forms of arts, entertainment, cultural heritage and media",
         },
-        {
+        "1": {
             "name": "conflict, war and peace",
             "description": "Acts of socially or politically motivated protest or violence, military activities, geopolitical conflicts, as well as resolution efforts",  # noqa: E501
         },
-        {
+        "2": {
             "name": "crime, law and justice",
             "description": "The establishment and/or statement of the rules of behaviour in society, the enforcement of these rules, breaches of the rules, the punishment of offenders and the organisations and bodies involved in these activities",  # noqa: E501
         },
-        {
+        "3": {
             "name": "disaster, accident and emergency incident",
             "description": "Man made or natural event resulting in loss of life or injury to living creatures and/or damage to inanimate objects or property",  # noqa: E501
         },
-        {
+        "4": {
             "name": "economy, business and finance",
             "description": "All matters concerning the planning, production and exchange of wealth.",  # noqa: E501
         },
-        {
+        "5": {
             "name": "education",
             "description": "All aspects of furthering knowledge, formally or informally",
         },
-        {
+        "6": {
             "name": "environment",
             "description": "All aspects of protection, damage, and condition of the ecosystem of the planet earth and its surroundings.",  # noqa: E501
         },
-        {
+        "7": {
             "name": "health",
             "description": "All aspects of physical and mental well-being",
         },
-        {
+        "8": {
             "name": "human interest",
             "description": "Item that discusses individuals, groups, animals, plants or other objects in an emotional way",  # noqa: E501
         },
-        {
+        "9": {
             "name": "labour",
             "description": "Social aspects, organisations, rules and conditions affecting the employment of human effort for the generation of wealth or provision of services and the economic support of the unemployed.",  # noqa: E501
         },
-        {
+        "10": {
             "name": "lifestyle and leisure",
             "description": "Activities undertaken for pleasure, relaxation or recreation outside paid employment, including eating and travel.",  # noqa: E501
         },
-        {
+        "11": {
             "name": "politics",
             "description": "Local, regional, national and international exercise of power, or struggle for power, and the relationships between governing bodies and states.",  # noqa: E501
         },
-        {
+        "12": {
             "name": "religion",
             "description": "Belief systems, institutions and people who provide moral guidance to followers",  # noqa: E501
         },
-        {
+        "13": {
             "name": "science and technology",
             "description": "All aspects pertaining to human understanding of, as well as methodical study and research of natural, formal and social sciences, such as astronomy, linguistics or economics",  # noqa: E501
         },
-        {
+        "14": {
             "name": "society",
             "description": "The concerns, issues, affairs and institutions relevant to human social interactions, problems and welfare, such as poverty, human rights and family planning",  # noqa: E501
         },
-        {
+        "15": {
             "name": "sport",
             "description": "Competitive activity or skill that involves physical and/or mental effort and organisations and bodies involved in these activities",  # noqa: E501
         },
-        {
+        "16": {
             "name": "weather",
             "description": "The study, prediction and reporting of meteorological phenomena",
         },
-    ]
+    }
+}
+
+CANDIDATE_DICT_SECOND = {
+    "arts, culture, entertainment and media": {
+        "0": {
+            "name": "arts and entertainment",
+            "description": "All forms of arts and entertainment",
+        },
+        "1": {
+            "name": "culture",
+            "description": "The ideas, customs, arts, traditions of a particular group of persons",
+        },
+        "2": {"name": "mass media", "description": "Media addressing a large audience"},
+    },
+    "conflict, war and peace": {
+        "0": {
+            "name": "act of terror",
+            "description": "Act of violence, often resulting in casualties, designed to raise fear and anxiety in a population",
+        },
+        "1": {
+            "name": "armed conflict",
+            "description": "Disputes between opposing groups involving the use of weapons, but not necessarily formally declared wars",
+        },
+        "2": {
+            "name": "civil unrest",
+            "description": "Rallies, strikes, demonstrations, riots or other disturbances by a population, often in protest to a government or other organization's policies or actions",
+        },
+        "3": {
+            "name": "coup d'etat",
+            "description": "The overthrow of an established government by an organised military or political group",
+        },
+        "4": {
+            "name": "massacre",
+            "description": "The mass killing of people, usually civilians, during wartime hostilities",
+        },
+        "5": {
+            "name": "peace process",
+            "description": "Organized actions to end a war or conflict, usually involving peace negotiations and agreements",
+        },
+        "6": {
+            "name": "post-war reconstruction",
+            "description": "Actions to rebuild a society, economy or political system in an area affected by war",
+        },
+    },
+    "crime, law and justice": {
+        "0": {
+            "name": "crime",
+            "description": "Violations of laws by individuals, companies or organisations",
+        },
+        "1": {"name": "judiciary", "description": "The system of courts of law"},
+        "2": {"name": "law", "description": "The codification of rules of behaviour"},
+        "3": {
+            "name": "law enforcement",
+            "description": "Agencies that attempt to prevent disobedience to established laws or bring to justice those who disobey those laws",
+        },
+    },
+    "disaster, accident and emergency incident": {
+        "0": {
+            "name": "accident and emergency incident",
+            "description": "A sudden, unexpected event that causes unwanted consequences or requires immediate action",
+        },
+        "1": {
+            "name": "disaster",
+            "description": "A sudden, unplanned event that causes great damage or loss of life, such as an accident or a natural catastrophe",
+        },
+        "2": {
+            "name": "emergency response",
+            "description": "The planning and efforts made by people and organizations to help victims of a sudden, unplanned event, natural disaster or crisis",
+        },
+    },
+    "economy, business and finance": {
+        "0": {
+            "name": "business information",
+            "description": "Information about individual business entities, including companies, corporations, charities",
+        },
+        "1": {
+            "name": "economy",
+            "description": "Production, consumption, distribution and trade activities affecting regions or countries as a whole",
+        },
+        "2": {
+            "name": "market and exchange",
+            "description": "Market for buying and selling stocks, currencies, commodities and other goods",
+        },
+    },
+    "education": {
+        "0": {
+            "name": "school",
+            "description": "A building or institution in which education is provided",
+        }
+    },
+    "environment": {
+        "0": {
+            "name": "climate change",
+            "description": "Significant change in measures of climate (such as temperature, precipitation, or wind) lasting for an extended period",
+        },
+        "1": {
+            "name": "conservation",
+            "description": "Preservation of wilderness areas, flora and fauna, including species extinction",
+        },
+        "2": {
+            "name": "environmental pollution",
+            "description": "Corruption of air, water, land etc. by harmful substances",
+        },
+        "3": {
+            "name": "natural resources",
+            "description": "Environmental issues related to the exploitation of natural resources",
+        },
+        "4": {"name": "nature", "description": "The natural world in its entirety"},
+    },
+    "health": {
+        "0": {
+            "name": "disease and condition",
+            "description": "Any health conditions affecting humans",
+        },
+        "1": {
+            "name": "government health care",
+            "description": "Health care provided by governments at any level",
+        },
+        "2": {
+            "name": "health facility",
+            "description": "Facilities used for any kind of health care",
+        },
+        "3": {
+            "name": "health insurance",
+            "description": "Insurance covering medical costs",
+        },
+        "4": {
+            "name": "health treatment and procedure",
+            "description": "Remedies, therapies, interventions, medications, testing and treatments to prevent, diagnose, manage or improve a health condition",
+        },
+        "5": {
+            "name": "medical profession",
+            "description": "Profession requiring formal training in the study, diagnosis, treatment, care and prevention of disease, medical condition or injury",
+        },
+    },
+    "labour": {
+        "0": {
+            "name": "employment",
+            "description": "The state of having work, usually paid, for a company, organisation, or individual",
+        },
+        "1": {
+            "name": "labour relations",
+            "description": "The relationship between workers and employers",
+        },
+        "2": {"name": "retirement", "description": "The years after work"},
+        "3": {
+            "name": "unemployment",
+            "description": "The state of being available to work but not having a job",
+        },
+    },
+    "lifestyle and leisure": {
+        "0": {
+            "name": "leisure",
+            "description": "Activities carried out in ones' spare time and not in a competitive way.",
+        },
+        "1": {
+            "name": "lifestyle",
+            "description": "The way in which a person lives, including their style and possessions",
+        },
+    },
+    "politics": {
+        "0": {
+            "name": "election",
+            "description": "The selection of representatives by the casting of votes",
+        },
+        "1": {
+            "name": "fundamental rights",
+            "description": "The political, social and economic rights to which all creatures are entitled, usually upheld by law.",
+        },
+        "2": {"name": "government", "description": "The system for ruling a country"},
+        "3": {
+            "name": "government policy",
+            "description": "An overall plan or course of action set out by a government intended to influence and guide decisions and actions towards a desired outcome.",
+        },
+        "4": {
+            "name": "international relations",
+            "description": "Non-violent relations between nations through negotiation, treaty, or personal meetings",
+        },
+        "5": {
+            "name": "political process",
+            "description": "The art or science of participating in the affairs of government, a state of political party",
+        },
+    },
+    "religion": {
+        "0": {
+            "name": "belief systems",
+            "description": "A set of beliefs prescribed by an institution or text often focusing on the worship of a higher power and the outlining of moral guidelines",
+        },
+        "1": {
+            "name": "religious facility",
+            "description": "Any facility where a group carries out its religious rites",
+        },
+        "2": {
+            "name": "religious text",
+            "description": "Texts regarded as holy or important by a religion",
+        },
+    },
+    "science and technology": {
+        "0": {
+            "name": "biomedical science",
+            "description": "The application of biology-based science to medical fields such as research, health monitoring or medical treatment",
+        },
+        "1": {
+            "name": "mathematics",
+            "description": "The study of structure, space, change and number in abstract, often using symbolic logic and language, and including subjects such as geometry, algebra or trigonometry",
+        },
+        "2": {
+            "name": "natural science",
+            "description": "The sciences that deal with matter, energy and the physical world, including physics, biology, chemistry and astronomy",
+        },
+        "3": {
+            "name": "scientific research",
+            "description": "The scientific and methodical investigation of events, procedures and interactions to explain why they occur, or to find solutions for problems",
+        },
+        "4": {
+            "name": "social sciences",
+            "description": "The study of human society in such aspects as linguistics, anthropology, economics or sociology",
+        },
+        "5": {
+            "name": "technology and engineering",
+            "description": "The study and practice of industrial or applied sciences such as physics, hydrodynamics or thermodynamics",
+        },
+        "6": {
+            "name": "mechanical engineering",
+            "description": "The study and application of the design, construction and operation of mechanical systems",
+        },
+    },
+    "society": {
+        "0": {
+            "name": "communities",
+            "description": "A group of individuals actively sharing a common value or interest",
+        },
+        "1": {
+            "name": "demographics",
+            "description": "The study of human populations and their characteristics, for example statistics or trends around aging populations in a particular geographic region",
+        },
+        "2": {
+            "name": "discrimination",
+            "description": "Unfair treatment of, or policies or practices against, individuals or groups of people on the basis of real or perceived membership in a group, such as race, sexual orientation, political or religious beliefs, age or height",
+        },
+        "3": {
+            "name": "family",
+            "description": "A group of people related genetically or by a legal bond, or who consider themselves part of a familial unit regardless of genetic or legal status",
+        },
+        "4": {
+            "name": "mankind",
+            "description": "Human beings taken as a whole, or described as members of particular groups such as teenagers, women, or people with disabilities",
+        },
+        "5": {
+            "name": "social condition",
+            "description": "The circumstances or state of affairs affecting a person's life, welfare and relations with others in a society",
+        },
+        "6": {
+            "name": "social problem",
+            "description": "Issues related to human rights, human welfare and other areas of societal concern",
+        },
+        "7": {
+            "name": "values",
+            "description": "A person's or group's principles or standards of behaviour, which guide their way of living and choices made",
+        },
+        "8": {
+            "name": "welfare",
+            "description": "Help for those in need of food, housing, health and other services",
+        },
+    },
+    "sport": {
+        "0": {
+            "name": "competition discipline",
+            "description": "Different types of sport which can be executed in competitions",
+        },
+        "1": {
+            "name": "drug use in sport",
+            "description": "Drug use associated with sport activities, including doping, abuse, testing and permitted medical uses",
+        },
+    },
+    "weather": {
+        "0": {
+            "name": "weather forecast",
+            "description": "The long- or short-term meteorological prediction and reporting of the upcoming weather for a given region",
+        },
+        "1": {
+            "name": "weather warning",
+            "description": "Alerts issued to the public about severe or notable weather in their area",
+        },
+    },
+}
