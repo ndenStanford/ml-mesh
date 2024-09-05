c54d8d53bcdd5967c3d70ff6f45120fa983c6958
Nutch <nutch.den@onclusive.com>
2024-09-05 14:45:18 +0000

    update lib for 4-level

--- libs/feature_store/onclusiveml/feature_store/on_demand/iptc/__init__.py
+++ libs/feature_store/onclusiveml/feature_store/on_demand/iptc/__init__.py
@@ -4,6 +4,7 @@ from onclusiveml.feature_store.on_demand.iptc.class_dict import (  # noqa: F401
     CANDIDATE_DICT_FIRST,
     CANDIDATE_DICT_SECOND,
     CANDIDATE_DICT_THIRD,
+    CANDIDATE_DICT_FOURTH
 )
 from onclusiveml.feature_store.on_demand.iptc.name_mapping_dict import (  # noqa: F401
     NAME_MAPPING_DICT_FIRST,
