e725387a6e27f850e6148efb9298431d5ae46e8a
Nutch <nutch.den@onclusive.com>
2024-09-07 04:56:56 +0000

    testing fourth level

--- projects/iptc/register/src/query_on_demand_feature_view.py
+++ projects/iptc/register/src/query_on_demand_feature_view.py
@@ -12,6 +12,32 @@ feature_registration_params = FeatureRegistrationParams()
 feast_repo_builder = FeastRepoBuilder(feature_registration_params)
 fs_handle = feast_repo_builder.fs_handle
 
+
+feature_view = [
+    feature_view
+    for feature_view in fs_handle.list_feature_views()
+    if feature_view.name == "iptc_third_level_feature_view"
+][0]
+
+features = [f"{feature_view.name}:{feature.name}" for feature in feature_view.features]
+
+features += ["iptc_fourth_level_on_demand_feature_view:topic_4_llm"]
+
+start = time.time()
+entity_df = """SELECT iptc_id, CURRENT_TIMESTAMP AS event_timestamp FROM "external"."iptc_third_level"
+LIMIT 10"""
+
+dataset_df = fs_handle.fs.get_historical_features(
+    entity_df=entity_df, features=features
+)
+df = dataset_df.to_df()
+
+print("=" * 20)
+print("10 samples")
+print(time.time() - start)
+print(df.head())
+
+
 feature_view = [
     feature_view
     for feature_view in fs_handle.list_feature_views()
