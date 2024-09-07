e5ae6232379fe3deb7b9a72b268097e6be6a5896
Nutch <nutch.den@onclusive.com>
2024-09-07 06:39:41 +0000

    4th level tested

--- projects/iptc/register/src/query_on_demand_feature_view.py
+++ projects/iptc/register/src/query_on_demand_feature_view.py
@@ -16,31 +16,6 @@ fs_handle = feast_repo_builder.fs_handle
 feature_view = [
     feature_view
     for feature_view in fs_handle.list_feature_views()
-    if feature_view.name == "iptc_third_level_feature_view"
-][0]
-
-features = [f"{feature_view.name}:{feature.name}" for feature in feature_view.features]
-
-features += ["iptc_fourth_level_on_demand_feature_view:topic_4_llm"]
-
-start = time.time()
-entity_df = """SELECT iptc_id, CURRENT_TIMESTAMP AS event_timestamp FROM "external"."iptc_third_level"
-LIMIT 10"""
-
-dataset_df = fs_handle.fs.get_historical_features(
-    entity_df=entity_df, features=features
-)
-df = dataset_df.to_df()
-
-print("=" * 20)
-print("10 samples")
-print(time.time() - start)
-print(df.head())
-
-
-feature_view = [
-    feature_view
-    for feature_view in fs_handle.list_feature_views()
     if feature_view.name == "iptc_first_level_feature_view"
 ][0]
 
@@ -97,6 +72,31 @@ feature_view = [
 features = [f"{feature_view.name}:{feature.name}" for feature in feature_view.features]
 
 features += ["iptc_third_level_on_demand_feature_view:topic_3_llm"]
+
+start = time.time()
+entity_df = """SELECT iptc_id, CURRENT_TIMESTAMP AS event_timestamp FROM "external"."iptc_third_level"
+LIMIT 10"""
+
+dataset_df = fs_handle.fs.get_historical_features(
+    entity_df=entity_df, features=features
+)
+df = dataset_df.to_df()
+
+print("=" * 20)
+print("10 samples")
+print(time.time() - start)
+print(df.head())
+
+
+feature_view = [
+    feature_view
+    for feature_view in fs_handle.list_feature_views()
+    if feature_view.name == "iptc_third_level_feature_view"
+][0]
+
+features = [f"{feature_view.name}:{feature.name}" for feature in feature_view.features]
+
+features += ["iptc_fourth_level_on_demand_feature_view:topic_4_llm"]
 
 start = time.time()
 entity_df = """SELECT iptc_id, CURRENT_TIMESTAMP AS event_timestamp FROM "external"."iptc_third_level"
