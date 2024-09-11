e71bf719983d6d50d71e08d39d27cdc85bd80c0d
Rene-Jean Corneille <rene-jean.corneille@onclusive.com>
2024-09-11 06:00:41 +0000

    update input parameter

--- projects/iptc/compile/src/compile.py
+++ projects/iptc/compile/src/compile.py
@@ -55,7 +55,10 @@ def main(settings: OnclusiveBaseSettings) -> None:
         **cast(settings, PipelineCompilationSettings).model_dump(),
     )
 
-    compiled_iptc = CompiledIPTC(compiled_iptc_pipeline, project=settings.project)
+    compiled_iptc = CompiledIPTC(
+        project=settings.project,
+        compiled_iptc_pipeline=compiled_iptc_pipeline,
+    )
     # export compiled iptc model for next workflow component: test
     compiled_iptc.save_pretrained(
         settings.model_directory(CompileWorkflowTasks.COMPILE)
