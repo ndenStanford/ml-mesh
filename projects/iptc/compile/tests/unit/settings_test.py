d5c0616c8c857f6e5d88f38ee8c63c5ff1e6b47d
Rene-Jean Corneille <rene-jean.corneille@onclusive.com>
2024-09-13 18:32:35 +0000

    updae tests

--- projects/iptc/compile/tests/unit/settings_test.py
+++ projects/iptc/compile/tests/unit/settings_test.py
@@ -1,23 +1,9 @@
 """Settings test."""
 
 # Source
-from src.settings import (
-    CompiledIPTCTrackedModelCard,
-    CompiledTrackedModelSettings,
-    IOSettings,
-)
+from src.settings import IOSettings
 
 
 def test_io_settings():
     """IO settings fixture."""
     IOSettings()
-
-
-def test_compiled_tracked_model_specs():
-    """Compiled tracked model settings."""
-    CompiledTrackedModelSettings()
-
-
-def test_compiled_iptc_tracked_model_card():
-    """Compiled IPTC tracked model card."""
-    CompiledIPTCTrackedModelCard()
