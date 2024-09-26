2deb8b5bfbb3fa47fa2da74607fa407ed5909c8e
Ivan Okhotnikov <ivan.okhotnikov@onclusive.com>
2024-09-26 15:12:48 +0000

    commit back ner compile

---
+++ projects/ner/serve/src/settings.py
@@ -0,0 +1,132 @@
+"""Settings."""
+
+# Standard Library
+from functools import lru_cache
+from pathlib import Path
+from typing import List, Union
+
+# 3rd party libraries
+from neptune.types.mode import Mode
+
+# Internal libraries
+from onclusiveml.core.base import OnclusiveBaseSettings
+from onclusiveml.nlp.language.constants import LanguageIso
+from onclusiveml.serving.params import ServingBaseParams
+from onclusiveml.serving.rest.serve.params import ServingParams
+from onclusiveml.tracking import (
+    TrackedGithubActionsSpecs,
+    TrackedImageSpecs,
+    TrackedModelSettings,
+    TrackingSettings,
+)
+
+
+NER_SUPPORTED_LANGUAGE = [
+    LanguageIso.EN,  # English
+    LanguageIso.PT,  # Portuguese
+    LanguageIso.ES,  # Spanish
+    LanguageIso.AR,  # Arabic
+    LanguageIso.KO,  # Korean
+    LanguageIso.TH,  # Thai
+    LanguageIso.JA,  # Japanese
+    LanguageIso.TL,  # Tagalog
+    LanguageIso.TR,  # Turkish
+    LanguageIso.FR,  # French
+    LanguageIso.RU,  # Russian
+    LanguageIso.IT,  # Italian
+    LanguageIso.ID,  # Indonesian
+    LanguageIso.PL,  # Polish
+    LanguageIso.HI,  # Hindi
+    LanguageIso.NL,  # Dutch
+    LanguageIso.HT,  # Haitian
+    LanguageIso.UR,  # Urdu
+    LanguageIso.DE,  # German
+    LanguageIso.FA,  # Persian
+    LanguageIso.CA,  # Catalan
+    LanguageIso.SV,  # Swedish
+    LanguageIso.FI,  # Finnish
+    LanguageIso.ET,  # Estonian
+    LanguageIso.EL,  # Greek
+    LanguageIso.CS,  # Czech
+    LanguageIso.EU,  # Basque
+    LanguageIso.TA,  # Tamil
+    LanguageIso.HE,  # Hebrew
+    LanguageIso.ZH,  # Chinese
+    LanguageIso.NO,  # Norwegian
+    LanguageIso.DA,  # Danish
+    LanguageIso.CY,  # Welsh
+    LanguageIso.LV,  # Latvian
+    LanguageIso.HU,  # Hungarian
+    LanguageIso.RO,  # Romanian
+    LanguageIso.LT,  # Lithuanian
+    LanguageIso.VI,  # Vietnamese
+    LanguageIso.UK,  # Ukrainian
+    LanguageIso.NE,  # Nepali
+    LanguageIso.SR,  # Serbian
+    LanguageIso.BN,  # Bengali
+    LanguageIso.SL,  # Slovenian
+    LanguageIso.IS,  # Icelandic
+    LanguageIso.ML,  # Malayalam
+    LanguageIso.BG,  # Bulgarian
+    LanguageIso.MR,  # Marathi
+    LanguageIso.SI,  # Sinhala
+    LanguageIso.OR,  # Oriya
+    LanguageIso.LO,  # Lao
+    LanguageIso.TE,  # Telugu
+    LanguageIso.KN,  # Kannada
+    LanguageIso.KU,  # Kurdish
+    LanguageIso.PS,  # Pashto
+    LanguageIso.GU,  # Gujarati
+    LanguageIso.AM,  # Amharic
+    LanguageIso.SD,  # Sindhi
+    LanguageIso.PA,  # Punjabi
+    LanguageIso.MY,  # Burmese
+    LanguageIso.KM,  # Khmer
+    LanguageIso.HY,  # Armenian
+    LanguageIso.KA,  # Georgian
+    LanguageIso.DV,  # Dhivehi
+    LanguageIso.UG,  # Uighur
+]
+
+
+class NERSettings(TrackingSettings):
+    """NER settings."""
+
+    supported_languages: List[LanguageIso] = NER_SUPPORTED_LANGUAGE
+
+
+class ServerModelSettings(ServingParams):
+    """Serve model parameters."""
+
+    model_name: str = "ner"
+    model_directory: Union[str, Path] = "."
+
+
+class CompiledNERTrackedModelSettings(TrackedModelSettings):
+    """Tracked compiled model settings."""
+
+    with_id: str
+    mode: str = Mode.READ_ONLY
+
+
+class DownloadSettings(ServingBaseParams):
+    """Download settings."""
+
+    wait_seconds_after_download: int = 0
+
+
+class GlobalSettings(
+    ServerModelSettings,
+    NERSettings,
+    DownloadSettings,
+    TrackedImageSpecs,
+    TrackedGithubActionsSpecs,
+    CompiledNERTrackedModelSettings,
+):
+    """Global server settings."""
+
+
+@lru_cache
+def get_settings() -> OnclusiveBaseSettings:
+    """Returns instanciated global settings class."""
+    return GlobalSettings()
---
+++ projects/ner/serve/tests/functional/serve/server_test.py
@@ -0,0 +1,210 @@
+"""Server functional tests."""
+
+# 3rd party libraries
+import pytest
+import requests
+
+# Internal libraries
+from onclusiveml.serving.rest.serve import (
+    LivenessProbeResponse,
+    ReadinessProbeResponse,
+)
+
+
+def test_model_server_root():
+    """Tests the root endpoint of a ModelServer (not running) instance."""
+    root_response = requests.get("http://serve:8000/ner/v1/")
+
+    assert root_response.status_code == 200
+
+
+def test_model_server_liveness():
+    """Tests the liveness endpoint of a ModelServer (not running) instance."""
+    liveness_response = requests.get("http://serve:8000/ner/v1/live")
+
+    assert liveness_response.status_code == 200
+    assert liveness_response.json() == LivenessProbeResponse().dict()
+
+
+def test_model_server_readiness():
+    """Tests the readiness endpoint of a ModelServer (not running) instance."""
+    readiness_response = requests.get("http://serve:8000/ner/v1/ready")
+
+    assert readiness_response.status_code == 200
+    assert readiness_response.json() == ReadinessProbeResponse().dict()
+
+
+def test_model_server_bio():
+    """Tests the readiness endpoint of a ModelServer (not running) instance."""
+    readiness_response = requests.get("http://serve:8000/ner/v1/bio")
+
+    assert readiness_response.status_code == 200
+    assert readiness_response.json()["data"]["attributes"].get("model_card") is not None
+
+
+@pytest.mark.parametrize(
+    "payload, expected_response",
+    [
+        # Test case for English (no entity)
+        (
+            {
+                "data": {
+                    "identifier": None,
+                    "namespace": "ner",
+                    "attributes": {
+                        "content": "House prices were unchanged last month, defying predictions of another drop, but they are unlikely to have troughed just yet."  # noqa
+                    },
+                    "parameters": {"language": "en"},
+                }
+            },
+            {
+                "version": 1,
+                "data": {
+                    "identifier": None,
+                    "namespace": "ner",
+                    "attributes": {"entities": []},
+                },
+            },
+        ),
+        # Test case for Korean (with entities)
+        (
+            {
+                "data": {
+                    "identifier": None,
+                    "namespace": "ner",
+                    "attributes": {
+                        "content": "에어비앤비, 하와이 임시 거처 제공마우이 신속대응팀 등 비영리 단체와 지속 협력"
+                    },
+                    "parameters": {"language": "ko"},
+                }
+            },
+            {
+                "version": 1,
+                "data": {
+                    "identifier": None,
+                    "namespace": "ner",
+                    "attributes": {
+                        "entities": [
+                            {
+                                "entity_text": "에어비앤비",
+                                "entity_type": "ORG",
+                                "score": 0.9980051755905152,
+                                "sentence_index": 0,
+                                "start": 0,
+                                "end": 5,
+                            },
+                            {
+                                "entity_text": "제공마우이 신속대응",
+                                "entity_type": "ORG",
+                                "score": 0.7580916749106513,
+                                "sentence_index": 0,
+                                "start": 17,
+                                "end": 27,
+                            },
+                        ]
+                    },
+                },
+            },
+        ),
+        # Test case for English (with entities)
+        (
+            {
+                "data": {
+                    "identifier": None,
+                    "namespace": "ner",
+                    "attributes": {
+                        "content": "Amazon steps up AI race with up to 4 billion deal to invest in Anthropic."  # noqa
+                    },
+                    "parameters": {"language": "en"},
+                }
+            },
+            {
+                "version": 1,
+                "data": {
+                    "identifier": None,
+                    "namespace": "ner",
+                    "attributes": {
+                        "entities": [
+                            {
+                                "entity_text": "Amazon",
+                                "entity_type": "LOC",
+                                "score": 0.9364089369773865,
+                                "sentence_index": 0,
+                                "start": 0,
+                                "end": 6,
+                            },
+                            {
+                                "entity_text": "AI",
+                                "entity_type": "MISC",
+                                "score": 0.9874637126922607,
+                                "sentence_index": 0,
+                                "start": 16,
+                                "end": 18,
+                            },
+                            {
+                                "entity_text": "Anthropic",
+                                "entity_type": "MISC",
+                                "score": 0.8394989768664042,
+                                "sentence_index": 0,
+                                "start": 63,
+                                "end": 72,
+                            },
+                        ]
+                    },
+                },
+            },
+        ),
+    ],
+)
+def test_model_server_prediction(payload, expected_response):
+    """Tests the readiness endpoint of a ModelServer (not running) instance."""
+    response = requests.post(
+        "http://serve:8000/ner/v1/predict",
+        json=payload,
+    )
+
+    assert response.status_code == 200
+    # TODO: assert score close to expected
+    assert response.json() == expected_response
+
+
+@pytest.mark.parametrize(
+    "payload,expected_error_detail",
+    [
+        (
+            {
+                "data": {
+                    "identifier": None,
+                    "namespace": "ner",
+                    "attributes": {
+                        "content": "Irrelevant content because of invalid message value (nonsense)."
+                    },
+                    "parameters": {"language": "invalid_language"},
+                }
+            },
+            "The language reference 'invalid_language' could not be mapped, or the language could not be inferred from the content.",  # noqa: E501
+        ),
+        (
+            {
+                "data": {
+                    "identifier": None,
+                    "namespace": "ner",
+                    "attributes": {
+                        "content": "Second example of irrelevant content because of invalid message value (empty string)."  # noqa: E501
+                    },
+                    "parameters": {"language": ""},
+                }
+            },
+            "The language reference '' could not be mapped, or the language could not be inferred from the content.",  # noqa: E501
+        ),
+    ],
+)
+def test_model_server_prediction_invalid_language(payload, expected_error_detail):
+    """Tests the language validation of the predict endpoint of a running ModelServer instance."""
+    response = requests.post(
+        "http://serve:8000/ner/v1/predict",
+        json=payload,
+    )
+
+    assert response.status_code == 204
+    assert response.text == ""
