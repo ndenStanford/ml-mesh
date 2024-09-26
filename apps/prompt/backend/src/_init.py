bbec48cf4180e8bed1f5135a9d41462a2856129c
Nutch <nutch.den@onclusive.com>
2024-09-26 12:40:38 +0000

    add log

--- apps/prompt/backend/src/_init.py
+++ apps/prompt/backend/src/_init.py
@@ -32,6 +32,7 @@ def init() -> None:
     logger.info("Creating tables...")
     _create_tables([LanguageModel, PromptTemplate, Project])
     _initialize_table(LanguageModel, DEFAULT_MODELS)
+    logger.info("Finish tables creation...")
     if SystemInfo.in_docker():
         _syncronize_prompts()
 
@@ -55,13 +56,20 @@ def _initialize_table(table: Type[Dyntastic], values: List[dict]) -> None:
 
 def _syncronize_prompts():
     """Save prompts from registry in dynamoDB if non-exisant."""
+    logger.info("Start prompt syncronize...")
     files = github.ls("")
+    print(files)
     for file in files:
         project_alias, *prompt_alias = file.split("/")
+        print(f"project_alias: {str(project_alias)}")
+        print(f"prompt_alias: {str(prompt_alias)}")
         project = Project.safe_get(project_alias)
         if project is None:
             Project(alias=project_alias).sync()
         if project_alias != ".github" and len(prompt_alias) > 0:
+            print(f"alias: {str(prompt_alias[0])}")
+            print(f"template: {str(github.read(file))}")
+            print(f"project_alias: {str(project_alias)}")
             PromptTemplate(
                 alias=prompt_alias[0], template=github.read(file), project=project_alias
             ).sync()
