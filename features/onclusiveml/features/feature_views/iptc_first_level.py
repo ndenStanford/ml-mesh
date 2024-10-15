34d2708352ed336f0c3d67242f5f6ad36434d556
Nutch <nutch.den@onclusive.com>
2024-10-15 11:44:01 +0000

    pre commit fix

--- features/onclusiveml/features/feature_views/iptc_first_level.py
+++ features/onclusiveml/features/feature_views/iptc_first_level.py
@@ -2,7 +2,7 @@
 
 from datetime import timedelta
 
-from feast import Entity, FeatureView, Field, types, OnDemandFeatureView,
+from feast import Entity, FeatureView, Field, types, OnDemandFeatureView
 from onclusiveml.features.contrib.redshift import OnclusiveRedshiftSource
 from feast.transformation.pandas_transformation import PandasTransformation
 from onclusiveml.features.contrib.on_demand.iptc.utils import iptc_first_level_on_demand_feature_view
