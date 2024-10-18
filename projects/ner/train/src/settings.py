cc4cea977c9dc73d1dff26f2dbf06d84031ec0aa
ali elhabchi <ali.elhabchi@onclusive.com>
2024-10-18 16:11:24 +0000

    add korean and japanase realted settings

--- projects/ner/train/src/settings.py
+++ projects/ner/train/src/settings.py
@@ -3,11 +3,13 @@
 # Standard Library
 import os
 from functools import lru_cache
+from typing import List
 
 # 3rd party libraries
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import SettingsConfigDict
 
 # Internal libraries
+from onclusiveml.core.base import OnclusiveBaseSettings
 from onclusiveml.tracking import (
     TrackedModelCard,
     TrackedModelSettings,
@@ -27,7 +29,22 @@ class TrackedNERModelSpecs(TrackedModelSettings):
 class Inputs(TrackingSettings):
     """Inputs."""
 
-    sample_document: str
+    sample_documents: List[List[str]] = [
+        [
+            "Google HQ is in Mountain View, CA",
+            "Hitchhiking onto the Gulf Stream, adult sea turtles often end up as far north as "
+            + "Cape Cod in their migratory travels.",
+            "Nesting season started March 1, so female loggerhead, green and leatherback sea "
+            + "turtles will be visiting Jupiter beaches to lay their eggs.",
+            "Check out Loggerhead Marinelife Center for a close look at sea turtles.",
+        ],
+        [
+            "Google 本社はカリフォルニア州マウンテンビューにあります",
+            "メキシコ湾流でヒッチハイクをする大人のウミガメは、回遊の旅の途中で北のケープコッドまで到達することがよくあります。",
+            "런던과 샌프란시스코에 가족이 있어요",
+            "바다거북을 가까이서 관찰하려면 Loggerhead Marinelife Center를 확인하세요.",
+        ],
+    ]
 
 
 class NERSettings(TrackingSettings):
@@ -40,12 +57,23 @@ class NERModelParams(TrackingSettings):
     ner_settings: NERSettings = NERSettings()
 
 
-class NERModelParams(NERModelParams):
+class NERModelParamsBase(NERModelParams):
     """Dslim NER model."""
 
     huggingface_pipeline_task: str = "token-classification"
     huggingface_model_reference: str = "dslim/bert-base-NER"
     model_class: str = "BertForTokenClassification"
+    model_config = SettingsConfigDict(protected_namespaces=("settings_",))
+
+
+class NERModelParamsKJ(NERModelParams):
+    """Korean/Japanese NER model."""
+
+    huggingface_pipeline_task_kj: str = "token-classification"
+    huggingface_model_reference_kj: str = (
+        "Davlan/distilbert-base-multilingual-cased-ner-hrl"
+    )
+    model_class: str = "DistilBertForTokenClassification"
     model_config = SettingsConfigDict(protected_namespaces=("settings_",))
 
 
@@ -55,12 +83,15 @@ class TrackedNERBaseModelCard(TrackedModelCard):
     model_type: str = "trained"
     # --- custom fields
     # model params
-    ner_model_params: NERModelParams = NERModelParams()
-    # ner_model_params_kj: NERModelParamsKJ = NERModelParamsKJ()
+    ner_model_params_base: NERModelParamsBase = NERModelParamsBase()
+    ner_model_params_kj: NERModelParamsKJ = NERModelParamsKJ()
     model_inputs: Inputs = Inputs()
     # admin
     local_output_dir: str = os.path.join(".", "ner_model_artifacts")
     logging_level: str = "INFO"
+
+    kj_model_subdirectory: str = "/korean_japanese_ner"
+    base_model_subdirectory: str = "/base_ner"
 
     model_config = SettingsConfigDict(
         protected_namespaces=("settings_",),
@@ -79,6 +110,6 @@ class GlobalSettings(TrackedNERModelSpecs, TrackedNERBaseModelCard):
 
 
 @lru_cache
-def get_settings() -> BaseSettings:
+def get_settings() -> OnclusiveBaseSettings:
     """Returns instanciated global settings class."""
     return GlobalSettings()
