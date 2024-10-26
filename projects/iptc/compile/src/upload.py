36aa4279a4dd2970fe415c0714562c5a9474f297
Syed Reza <syed.reza@onclusive.com>
2024-10-26 16:56:35 +0000

    test by removing mode

--- projects/iptc/compile/src/upload.py
+++ projects/iptc/compile/src/upload.py
@@ -63,7 +63,7 @@ def main(settings: OnclusiveBaseSettings) -> None:
     # get read-only base model version
     trained_model_version = TrackedModelVersion(
         with_id=settings.with_id,
-        mode=settings.mode,
+        # mode=settings.mode,
         api_token=settings.api_token.get_secret_value(),
         project=settings.project,
     )
