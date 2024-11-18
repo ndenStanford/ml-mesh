3f0c169059eab9aa183ed08db9d7e42aa716d36f
yuzhou.gao <yuzhou.gao@onclusive.com>
2024-11-18 14:48:36 +0000

    fix feature_store

--- libs/feature_store/onclusiveml/tests/integration/feast_test.py
+++ libs/feature_store/onclusiveml/tests/integration/feast_test.py
@@ -11,9 +11,9 @@ def test_get_historical_features(settings, entity_df):
     dataset = fs.get_historical_features(
         entity_df=entity_df,
         features=[
-            "iptc_first_level:topic_1",
-            "iptc_first_level:content",
-            "iptc_first_level:title",
+            "iptc__first_level:topic_1",
+            "iptc__first_level:content",
+            "iptc__first_level:title",
         ],
     )
 
@@ -28,10 +28,10 @@ def test_get_historical_features_on_demand(settings, entity_df):
     dataset = fs.get_historical_features(
         entity_df=entity_df,
         features=[
-            "iptc_first_level:topic_1",
-            "iptc_first_level:content",
-            "iptc_first_level:title",
-            "iptc_first_level_on_demand_feature_view:topic_1_llm",
+            "iptc__first_level:topic_1",
+            "iptc__first_level:content",
+            "iptc__first_level:title",
+            "iptc__first_level_on_demand_feature_view:topic_1_llm",
         ],
     )
 
@@ -44,7 +44,7 @@ def test_get_training_dataset(settings):
     fs = FeastFeatureStore.from_settings(settings)
 
     dataset = fs.get_training_dataset(
-        name="iptc_first_level",
+        name="iptc__first_level",
         join_key_columns=[],
         feature_name_columns=["topic_1", "content", "language"],
         timestamp_field="created_at",
