a3f16b0aa7fb5eca00349ee28863076aefba3432
yuzhou.gao <yuzhou.gao@onclusive.com>
2024-11-21 02:18:26 +0000

    update new sentiment model & logic

--- libs/models/onclusiveml/tests/integration/sentiment/conftest.py
+++ libs/models/onclusiveml/tests/integration/sentiment/conftest.py
@@ -15,7 +15,7 @@ def test_hf_pipeline():
     """Huggingface pipeline."""
     return pipeline(
         task="sentiment-analysis",
-        model="cardiffnlp/twitter-xlm-roberta-base-sentiment",
+        model="yangheng/deberta-v3-base-absa-v1.1",
     )
 
 
