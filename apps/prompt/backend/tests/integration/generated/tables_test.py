23dc5a74613344631c562ecbebe0d8a2bf9b118b
vishal.singh <vishal.singh@onclusive.com>
2024-11-28 14:30:54 +0000

    fixed integration tests

--- apps/prompt/backend/tests/integration/generated/tables_test.py
+++ apps/prompt/backend/tests/integration/generated/tables_test.py
@@ -11,11 +11,15 @@ from src.generated.tables import Generated
     "data",
     [
         {
-            "id": "e2478dc2-08ec-42bc-928a-c9e59c3fafe",
-            "generation": "test1",
-            "method": "src.prompt.functional.generate_from_prompt_template",
-            "args": ["english-summarization", "gpt-4o"],
-            "kwargs": {"input": {"number": 5, "text": "What is the capital of US?"}},
+            "id": "e2977cc2-08ec-42bc-928a-c9e59c3fafe",
+            "generation": {"generated": "generation-1"},
+            "status": "PENDING",
+            "model": "gpt-4o",
+            "prompt": "english-summarization",
+            "error": None,
+            "model_parameters": {
+                "temperature": "0.6",
+            },
             "timestamp": "2024-11-19T12:00:00",
         }
     ],
@@ -26,19 +30,20 @@ def test_save(data, app):
     generated = Generated(
         id=data["id"],
         generation=data["generation"],
-        method=data["generation"],
-        args=data["args"],
-        kwargs=data["kwargs"],
+        status=data["status"],
+        model=data["model"],
+        prompt=data["prompt"],
+        error=data["error"],
+        model_parameters=data["model_parameters"],
         timestamp=data["timestamp"],
     )
     generated.save()
-    Generated.safe_get(data["id"])
+    Generated.get(data["id"])
 
 
-@pytest.mark.parametrize("id", ["e2478dc2-08ec-42bc-928a-c9e59c3fafe"])
+@pytest.mark.parametrize("id", ["e2977cc2-08ec-42bc-928a-c9e59c3fafe"])
 @pytest.mark.order(19)
 def test_delete(id, app):
     """Test delete."""
     generated = Generated.get(id)
     generated.delete()
-    assert Generated.safe_get(id) is None
